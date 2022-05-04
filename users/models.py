ba8718c6ddaf7ecc57723c2a30f762db8d478f7a
Alexander <dev@pavlovteam.ru>
2022-05-04 05:33:51 +0300

    added class Admin (AbstractBaseUser)

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+
+class Admin(AbstractBaseUser):
+    username = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    password = models.CharField(max_length=100)
+    USERNAME_FIELD = 'username'
