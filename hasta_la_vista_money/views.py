4f2186353ba9279546311a782039c0d221f96c34
Alexander <dev@pavlovteam.ru>
2022-05-04 22:35:39 +0300

    added the button with name button_register

--- hasta_la_vista_money/views.py
+++ hasta_la_vista_money/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.views import LoginView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import TemplateView
+from django.utils.translation import gettext, gettext_lazy
 
 from users.forms import Admin
-# from django.utils.translation import gettext, gettext_lazy
 
 
 class Index(SuccessMessageMixin, LoginView):
@@ -11,7 +11,8 @@ class Index(SuccessMessageMixin, LoginView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Hasta La Vista, Money :D'
+        context['button_text'] = gettext('Войти')
+        context['button_register'] = gettext('Регистрация')
         return context
 
 
