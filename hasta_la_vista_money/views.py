343518e10968160adb87c3795d7e29e23547f80a
Alexander <dev@pavlovteam.ru>
2022-05-05 23:56:44 +0300

    added permission

--- hasta_la_vista_money/views.py
+++ hasta_la_vista_money/views.py
@@ -1,5 +1,7 @@
+from django.contrib import messages
 from django.contrib.auth.views import LoginView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 from django.utils.translation import gettext, gettext_lazy
 
@@ -18,8 +20,15 @@ class Index(SuccessMessageMixin, LoginView):
 
 class IndexHastaLaVista(TemplateView):
     template_name = 'hasta_la_vista_money/index.html'
+    error_message = gettext('У вас нет прав на просмотр данной страницы! '
+                            'Авторизуйтесь!')
+    no_permission_url = 'index'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Hasta La Vista, Money :D'
         return context
+
+    def handle_no_permission(self):
+        messages.error(self.request, self.error_message)
+        return redirect(self.no_permission_url)
