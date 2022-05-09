3d9f107a6a82ec312035aa276b1890b820103036
Alexander <dev@pavlovteam.ru>
2022-05-09 15:32:36 +0300

    refactoring

--- bot/telegrambot.py
+++ bot/telegrambot.py
@@ -3,6 +3,7 @@ import logging
 
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 from aiogram.utils.executor import start_webhook
 
 import asyncio
@@ -17,27 +18,28 @@ loop = asyncio.get_event_loop()
 bot = Bot(API_TOKEN)
 dp = Dispatcher(bot, loop=loop)
 
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
-# webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/api-telegram-hook/{API_TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-# webserver settings
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
+# HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+#
+# # webhook settings
+# WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+# WEBHOOK_PATH = f'/api-telegram-hook/{API_TOKEN}'
+# WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+#
+# # webserver settings
+# WEBAPP_HOST = '0.0.0.0'
+# WEBAPP_PORT = os.getenv('PORT', default=8000)
 
 
 # Сообщение для администратора, что бот запущен
 async def on_startup(dispatcher):
     await bot.send_message(chat_id=admin_id, text='Бот запущен!\n')
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+    # await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
 
 async def on_shutdown(dispatcher):
     await bot.send_message(chat_id=admin_id, text='Бот остановлен!\n')
-    await bot.delete_webhook()
+    # await bot.delete_webhook()
 
 
 @dp.message_handler()
@@ -47,12 +49,8 @@ async def echo(message: types.Message):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=False,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+    executor.start_polling(dispatcher=dp,
+                           skip_updates=False,
+                           on_startup=on_startup,
+                           on_shutdown=on_shutdown,
+                           )
