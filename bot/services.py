75ef42c4964dd38b76e175a151fedf7bac27ce11
Alexander <dev@pavlovteam.ru>
2022-05-10 22:46:51 +0300

    refactoring

--- bot/services.py
+++ bot/services.py
@@ -2,13 +2,9 @@ import datetime
 
 
 # Выделяем дату из json
-def convert_date(date_time):
-    return datetime.datetime.fromtimestamp(date_time).strftime("%Y.%m.%d")
-
-
-# Выделяем время из json
-def convert_time(date_time):
-    return datetime.datetime.fromtimestamp(date_time).strftime("%H:%M:%S")
+def convert_date_time(date_time):
+    return datetime.datetime.fromtimestamp(date_time).strftime("%d-%m-%Y "
+                                                               "%H:%M:%S")
 
 
 def get_result_price(price):
