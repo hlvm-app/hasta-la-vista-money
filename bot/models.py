eab2192e2da112d6aa8bbfe349faccf6d70185c1
Alexander <dev@pavlovteam.ru>
2022-05-10 22:54:26 +0300

    rename class to Receipt

--- bot/models.py
+++ bot/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Receipts(models.Model):
+class Receipt(models.Model):
     receipt_date = models.CharField(max_length=20)
     name_seller = models.CharField(max_length=150)
     product_information = models.CharField(max_length=1000)
