e1766932339edd4384aaeccbb82565343a881962
Alexander <dev@pavlovteam.ru>
2022-05-15 13:41:21 +0300

    change Procfile

--- Procfile
+++ Procfile
@@ -1,5 +1,5 @@
 $ python manage.py collectstatic --noinput
 $ django-admin compilemessages
-web: gunicorn hasta_la_vista_money.wsgi
+web: gunicorn hasta_la_vista_money.wsgi:application
 worker: python manage.py startbot
 
--- hasta_la_vista_money/settings.py
+++ hasta_la_vista_money/settings.py
@@ -20,7 +20,7 @@ import os
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
