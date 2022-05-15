9499972f3e7c715bac81fc17a3c355cfca079711
Alexander <dev@pavlovteam.ru>
2022-05-15 13:53:38 +0300

    transfer Receipt class to receipts package

--- receipts/models.py
+++ receipts/models.py
@@ -1,1 +1,13 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+class Receipt(models.Model):
+    receipt_date = models.CharField(max_length=20)
+    name_seller = models.CharField(max_length=150)
+    product_information = ArrayField(ArrayField(
+            models.CharField(max_length=1000), default=list
+        )
+    )
+    total_sum = models.CharField(max_length=20)
 
