debe456e1886559f178a568de361710e0b1178e9
Alexander <dev@pavlovteam.ru>
2022-05-19 21:37:33 +0300

    refactor

--- receipts/views.py
+++ receipts/views.py
@@ -15,11 +15,4 @@ class ReceiptView(ListView):
     login_url = 'login'
     redirect_field_name = 'login'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = gettext('Страница с чеками')
-        return context
 
-    def get_queryset(self):
-        seller = self.model.objects.all()
-        return seller.model.name_seller
