b5d62203aff085686807922c0d1c6fcaf762a009
Alexander <dev@pavlovteam.ru>
2022-05-26 21:54:54 +0300

    added filter class

--- receipts/forms.py
+++ receipts/forms.py
@@ -1,3 +1,6 @@
+import django_filters
+from django_filters import rest_framework as filters
+from django.db import models
 from django.forms import ModelForm
 from django.utils.translation import gettext
 
@@ -11,3 +14,12 @@ class ReceiptForm(ModelForm):
         labels = {
             'name_seller': gettext('Продавец')
         }
+
+
+class ReceiptsFilter(filters.FilterSet):
+    names_seller = Receipt.objects.values_list('id', 'name_seller', named=True).all()
+    name_seller = filters.ChoiceFilter(label='Seller', choices=names_seller)
+
+    class Meta:
+        model = Receipt
+        fields = ('name_seller', )
