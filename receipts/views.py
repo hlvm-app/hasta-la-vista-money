98f3132a2bf9f6c444992ba7a453ff6dc484f02f
Alexander <dev@pavlovteam.ru>
2022-05-26 21:54:46 +0300

    refactoring

--- receipts/views.py
+++ receipts/views.py
@@ -1,17 +1,19 @@
-from django.utils.translation import gettext
-from django.views.generic import ListView
+from django.shortcuts import render
+from django.utils.translation import gettext_lazy
+from django_filters.views import FilterView
+from django_filters import rest_framework as filters
 
+from receipts.forms import ReceiptsFilter
 from receipts.models import Receipt
 
 
-class ReceiptView(ListView):
-    model = Receipt
+class ReceiptView(FilterView):
+    queryset = Receipt.objects.all()
     template_name = 'receipts/receipts.html'
     context_object_name = 'receipts'
+    filterset_class = ReceiptsFilter
+    filter_backends = (filters.DjangoFilterBackend,)
 
-    error_message = gettext('У вас нет прав на просмотр данной страницы! '
-                            'Авторизуйтесь!')
+    error_message = gettext_lazy('У вас нет прав на просмотр данной страницы! '
+                                 'Авторизуйтесь!')
     login_url = 'login'
-    redirect_field_name = 'login'
-
-
