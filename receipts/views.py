5d4bc85b7d57da9d9428052c58c0cdb5df9db0be
Alexander <dev@pavlovteam.ru>
2022-05-29 17:47:42 +0300

    del some import

--- receipts/views.py
+++ receipts/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
 from django.utils.translation import gettext_lazy
-from django.views.generic import ListView
 from django_filters.views import FilterView
 from django_filters import rest_framework as filters
 
