2ee7f30969dd1d73bf18e080b51ef63c27ede053
Alexander <dev@pavlovteam.ru>
2022-05-30 23:44:02 +0300

    added field with date

--- receipts/forms.py
+++ receipts/forms.py
@@ -1,5 +1,4 @@
 import django_filters
-from django_filters import rest_framework as filters
 from django.utils.translation import gettext_lazy
 
 from receipts.models import Receipt
@@ -8,9 +7,10 @@ from receipts.models import Receipt
 class ReceiptsFilter(django_filters.FilterSet):
     names_seller = Receipt.objects.values_list('name_seller',
                                                'name_seller').distinct()
-    name_seller = filters.ChoiceFilter(label=gettext_lazy('Продавец'),
-                                       choices=names_seller)
+    name_seller = django_filters.ChoiceFilter(label=gettext_lazy('Продавец'),
+                                              choices=names_seller)
+    receipt_date = django_filters.DateRangeFilter(label=gettext_lazy('Период'))
 
     class Meta:
         model = Receipt
-        fields = ('name_seller', )
+        fields = ('name_seller', 'receipt_date')
