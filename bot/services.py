c228b550d8915fb1a11009aaa5151e3f13837f56
Alexander <dev@pavlovteam.ru>
2022-05-30 21:07:13 +0300

    change format date

--- bot/services.py
+++ bot/services.py
@@ -3,8 +3,8 @@ import datetime
 
 # Выделяем дату из json
 def convert_date_time(date_time):
-    return datetime.datetime.fromtimestamp(date_time).strftime("%d-%m-%Y "
-                                                               "%H:%M:%S")
+    return datetime.datetime.fromtimestamp(date_time).strftime("%Y-%m-%d "
+                                                               "%H:%M")
 
 
 def get_result_price(price):
