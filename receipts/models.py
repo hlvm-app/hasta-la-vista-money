812fdba91ddbe63aec4e4c5ef924e02374726c99
Alexander <dev@pavlovteam.ru>
2022-05-30 21:06:26 +0300

    change type column

--- receipts/models.py
+++ receipts/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from django.utils import timezone
 
 
 class Receipt(models.Model):
-    receipt_date = models.CharField(max_length=20)
+    receipt_date = models.DateTimeField()
     name_seller = models.CharField(max_length=150)
     product_information = ArrayField(ArrayField(
             models.CharField(max_length=1000), default=list
