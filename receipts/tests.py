c0451f3d433fe737e7ea4c44704ad94ceab0dc68
Alexander <dev@pavlovteam.ru>
2022-06-01 06:05:50 +0300

    added tests and fixtures

--- receipts/tests.py
+++ receipts/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse_lazy
 
-# Create your tests here.
+from receipts.models import Receipt
+from users.models import User
+
+
+class TestReceipt(TestCase):
+
+    fixtures = ['receipts.yaml', 'users.yaml']
+
+    def setUp(self) -> None:
+        self.user = User.objects.get(pk=1)
+        self.receipt1 = Receipt.objects.get(pk=4)
+        self.receipt2 = Receipt.objects.get(pk=5)
+
+    def test_receipt_list(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse_lazy('receipts:list'))
+        self.assertEqual(response.status_code, 200)
+        receipts_list = list(response.context['receipts'])
+        self.assertQuerysetEqual(receipts_list, [self.receipt1, self.receipt2])
