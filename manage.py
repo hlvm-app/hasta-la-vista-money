2d3651dfc7390d0535d2693d049ae28fe603a44f
Alexander <dev@pavlovteam.ru>
2022-06-01 06:44:26 +0300

    lint

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hasta_la_vista_money.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'hasta_la_vista_money.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
