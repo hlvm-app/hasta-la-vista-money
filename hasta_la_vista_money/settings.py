686bcc29b81f2de45c936a9a6c8e37608e0c8684
Alexander <dev@pavlovteam.ru>
2022-06-01 22:24:46 +0300

    change env secret-key

--- hasta_la_vista_money/settings.py
+++ hasta_la_vista_money/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'true').lower() in {'yes', '1', 'true'}
