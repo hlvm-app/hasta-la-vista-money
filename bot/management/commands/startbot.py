c6d9fb1a44128959e84de8a8d7e2f72744d2c987
Alexander <dev@pavlovteam.ru>
2022-06-05 20:50:05 +0300

    adding CommandHandler + refactoring mime_type

--- bot/management/commands/startbot.py
+++ bot/management/commands/startbot.py
@@ -1,9 +1,11 @@
 from django.core.management import BaseCommand
 import os
 
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
 from bot.telegrambot import get_receipt
+from bot.adding_receipt import add_receipt
 
 TOKEN = os.environ.get('TOKEN_TELEGRAM_BOT')
 
@@ -14,8 +16,17 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         updater = Updater(token=TOKEN, use_context=True)
         dispatcher = updater.dispatcher
+        button = ['/Add']
+        markup = ReplyKeyboardMarkup.from_column(button)
 
-        dispatcher.add_handler(MessageHandler(Filters.all, get_receipt))
+        dispatcher.add_handler(
+            MessageHandler(
+                Filters.document.mime_type('application/json'), get_receipt
+            )
+        )
+        dispatcher.add_handler(
+            CommandHandler(command='Start', callback=add_receipt)
+        )
 
         updater.start_polling()
         updater.idle()
