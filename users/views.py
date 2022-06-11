39e2394c26bd4df1d5b81514f758d9de8587082c
Alexander <dev@pavlovteam.ru>
2022-06-11 15:09:16 +0300

    move class LoginUser to app users

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.views import LoginView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy, gettext
 from django.views.generic import CreateView, ListView
 
-from users.forms import RegisterUserForm
+from users.forms import RegisterUserForm, UserLoginForm
 from users.models import User
 
 
@@ -11,6 +12,19 @@ class ListUsers(ListView):
     model = User
     template_name = 'users/users.html'
     context_object_name = 'users'
+
+
+class LoginUser(SuccessMessageMixin, LoginView):
+    model = User
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
+    success_message = gettext_lazy('Вход успешно выполнен')
+    next_page = '/applications'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['button_text'] = gettext('Войти')
+        return context
 
 
 class CreateUser(SuccessMessageMixin, CreateView):
