ca42558adfceaef91eae3aac06c311de64b4b262
Alexander <dev@pavlovteam.ru>
2022-06-26 20:34:56 +0300

    remove files

--- bot/helpers.py
+++
@@ -1,6 +0,0 @@
-def respond(bot, chat_id, message):
-    return bot.send_message(chat_id=chat_id, **message)
-
-
-def receipt_adding_accepted(bot, chat_id, message):
-    return bot.send_message(chat_id=chat_id, **message)
