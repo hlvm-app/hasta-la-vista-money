607d2aa4a9fd3bfa4545f1e81f97ad1242c6fdf4
Alexander <dev@pavlovteam.ru>
2023-02-03 13:20:27 +0300

    Changed models for save receipts in three tables

--- receipts/models.py
+++ receipts/models.py
@@ -1,32 +1,33 @@
 from django.db import models
 
 
-class Customers(models.Model):
-    name_seller = models.CharField(max_length=255)
-    retail_place_address = models.CharField(max_length=256, null=True)
-    retail_place = models.CharField(null=True, blank=True, max_length=255)
+class Customer(models.Model):
+    name_seller = models.CharField(max_length=100)
+    retail_place_address = models.CharField(max_length=100)
+    retail_place = models.CharField(null=True, blank=True, max_length=100)
 
     def __str__(self):
         return self.name_seller
 
 
-class CashReceipts(models.Model):
+class Receipt(models.Model):
     receipt_date = models.DateTimeField()
     operation_type = models.IntegerField(null=True, blank=True)
-    total_sum = models.DecimalField(max_digits=20, decimal_places=2)
-    customer = models.ForeignKey(Customers, on_delete=models.CASCADE, related_name='cashreceipts')
+    total_sum = models.DecimalField(max_digits=10, decimal_places=2)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE,
+                                 verbose_name='customer',
+                                 related_name='customer')
+    product = models.ManyToManyField('Product', related_name='product')
 
     def __str__(self):
         return self.receipt_date
 
 
-class GoodsInReceipt(models.Model):
-    receipt = models.ForeignKey(CashReceipts, on_delete=models.CASCADE, related_name='goodsinreceipt')
-    product_name = models.CharField(max_length=255)
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.DecimalField(max_digits=10, decimal_places=2)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    customer = models.ForeignKey(Customers, on_delete=models.CASCADE, related_name='goodsinreceipts')
 
     def __str__(self):
         return self.product_name
