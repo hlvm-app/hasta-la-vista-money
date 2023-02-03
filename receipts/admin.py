1f729fd312ea330bf3564642e9f58e0de7f5b850
Alexander <dev@pavlovteam.ru>
2023-02-03 13:16:12 +0300

    Registration of three models

--- receipts/admin.py
+++ receipts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from receipts.models import Receipt
+from receipts.models import Customer, Receipt, Product
 
 
+admin.site.register(Customer)
 admin.site.register(Receipt)
+admin.site.register(Product)
