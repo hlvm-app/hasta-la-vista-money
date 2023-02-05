82420efcce7756efd89b898c8b1d3a72948c340a
Alexander <dev@pavlovteam.ru>
2023-02-05 14:32:14 +0300

    retail_place_address field became optional, added fields nds and nds sum at product

--- receipts/models.py
+++ receipts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Customer(models.Model):
     name_seller = models.CharField(max_length=100)
-    retail_place_address = models.CharField(max_length=100)
+    retail_place_address = models.CharField(null=True, max_length=100)
     retail_place = models.CharField(null=True, blank=True, max_length=100)
 
     def __str__(self):
@@ -28,6 +28,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.DecimalField(max_digits=10, decimal_places=2)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    nds_type = models.IntegerField(null=True, blank=True)
+    nds_sum = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.product_name
