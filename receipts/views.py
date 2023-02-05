9a0f0cebc075c0505374a4b20dafdcf1ef600aea
Alexander <dev@pavlovteam.ru>
2023-02-05 14:51:12 +0300

    changed the class to the function

--- receipts/views.py
+++ receipts/views.py
@@ -1,27 +1,29 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import redirect
+from django.db.models import Subquery
+from django.shortcuts import redirect, render, get_object_or_404
 from django.utils.translation import gettext_lazy
+from django.views import View
 from django_filters.views import FilterView
 from django_filters import rest_framework as filters
 
-from receipts.forms import ReceiptsFilter
-from receipts.models import Receipt
+# from receipts.forms import ReceiptsFilter
+
+from django.shortcuts import render
+from .models import Customer, Receipt, Product
+
+from django.shortcuts import render
 
 
-class ReceiptView(LoginRequiredMixin, SuccessMessageMixin, FilterView):
-    model = Receipt
-    template_name = 'receipts/receipts.html'
-    context_object_name = 'receipts'
-    ordering = ['-receipt_date']
-    filterset_class = ReceiptsFilter
-    filter_backends = (filters.DjangoFilterBackend,)
+def receipt_view(request):
+    receipts = Receipt.objects.all()
+    return render(request, 'receipts/receipts.html', {'receipts': receipts})
 
-    error_message = gettext_lazy('У вас нет прав на просмотр данной страницы! '
-                                 'Авторизуйтесь!')
-    no_permission_url = 'login'
 
-    def handle_no_permission(self):
-        messages.error(self.request, self.error_message)
-        return redirect(self.no_permission_url)
+
+
+
+
+
+
