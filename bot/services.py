aee3a4160f3ed6871d9d4fa71e7a653af85eb266
Alexander <dev@pavlovteam.ru>
2023-02-05 14:47:21 +0300

    simplified the function, transferred the information about the products to a file telegrambot.py

--- bot/services.py
+++ bot/services.py
@@ -1,10 +1,5 @@
 import datetime
 import os
-from collections import namedtuple
-
-ProductInformation = namedtuple('ProductInformation',
-                                ['name', 'price', 'quantity', 'amount',
-                                 'payment_type', 'product_type', 'nds_type'])
 
 
 # Выделяем дату из json
@@ -24,25 +19,12 @@ def remove_json_file(path):
 
 
 def parse_json_file(json_data):
-    date_time = convert_date_time(json_data["dateTime"])
-    seller = json_data['user']
-    total_sum = convert_price(json_data["totalSum"])
-    retail_place_address = json_data['retailPlaceAddress']
-    operation_type = json_data['operationType']
-    retail_place = json_data['retailPlace']
-
-    information_products = [
-        ProductInformation(
-            name=item["name"],
-            price=convert_price(item["price"]),
-            quantity=item["quantity"],
-            amount=convert_price(item["sum"]),
-            payment_type=item['paymentType'],
-            product_type=item['productType'],
-            nds_type=item['nds']
-        )
-        for item in json_data["items"]
-    ]
+    date_time = convert_date_time(json_data.get('dateTime', 'Нет данных'))
+    seller = json_data.get('user', 'Нет данных')
+    total_sum = convert_price(json_data.get('totalSum', 'Нет данных'))
+    retail_place_address = json_data.get('retailPlaceAddress', 'Нет данных')
+    operation_type = json_data.get('operationType', 'Нет данных')
+    retail_place = json_data.get('retailPlace', 'Нет данных')
 
     return date_time, seller, total_sum, retail_place_address, \
-        operation_type, retail_place, information_products
+        operation_type, retail_place
