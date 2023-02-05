69e68b95f5616e6019e0e287df0c02e19903c4ad
Alexander <dev@pavlovteam.ru>
2023-02-05 16:30:42 +0300

    added code lines for delete receipts

--- receipts/views.py
+++ receipts/views.py
@@ -18,10 +18,14 @@ from django.shortcuts import render
 
 def receipt_view(request):
     receipts = Receipt.objects.all()
+    if request.method == "POST" and 'delete_button' in request.POST:
+        receipt_id = request.POST.get('receipt_id')
+        receipt = get_object_or_404(Receipt, pk=receipt_id)
+        for product in receipt.product.all():
+            product.delete()
+        receipt.customer.delete()
+        receipt.delete()
     return render(request, 'receipts/receipts.html', {'receipts': receipts})
-
-
-
 
 
 
