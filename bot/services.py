fce0c65e2c1a579bec60702124bc4ffbbc44e758
Alexander Pavlov <dev@pavlovteam.ru>
2023-02-07 14:21:07 +0300

    In  function convert_date_time format changed on f-string

--- bot/services.py
+++ bot/services.py
@@ -4,8 +4,7 @@ import os
 
 # Выделяем дату из json
 def convert_date_time(date_time):
-    return datetime.datetime.fromtimestamp(date_time).strftime("%Y-%m-%d "
-                                                               "%H:%M")
+    return f"{datetime.datetime.fromtimestamp(date_time):%Y-%m-%d %H:%M}"
 
 
 def convert_price(price):
