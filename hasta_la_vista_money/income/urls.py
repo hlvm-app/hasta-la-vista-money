4c18950cea8b7de92c1436120a7e3c17d81cea55
Alexander <dev@pavlovteam.ru>
2023-02-09 21:33:03 +0300

    added url for link to a template create_income.html

--- hasta_la_vista_money/income/urls.py
+++ hasta_la_vista_money/income/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from hasta_la_vista_money.income.views import IncomeView
+from hasta_la_vista_money.income.views import IncomeView, AddIncome
 
 app_name = 'income'
 urlpatterns = [
     path('', IncomeView.as_view(), name='list'),
+    path('create/', AddIncome.as_view(), name='create_income')
 ]
