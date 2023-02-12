291231ee6f2e50d75b1edf39d94b4bd8afda7d16
Alexander <dev@pavlovteam.ru>
2023-02-12 16:39:37 +0300

    added link to create page for add receipt

--- hasta_la_vista_money/receipts/urls.py
+++ hasta_la_vista_money/receipts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from hasta_la_vista_money.receipts.views import ReceiptView
+from hasta_la_vista_money.receipts.views import ReceiptView, CreateReceipt
 
 app_name = 'receipts'
 urlpatterns = [
     path('', ReceiptView.as_view(), name='list'),
+    path('create/', CreateReceipt.as_view(), name='create')
 ]
