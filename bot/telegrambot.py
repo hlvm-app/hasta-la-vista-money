37bc0d9ad2b1616b751d0260ceb529369f217ea6
Alexander <dev@pavlovteam.ru>
2023-02-19 12:58:44 +0300

    removed unnecessary

--- bot/telegrambot.py
+++ bot/telegrambot.py
@@ -2,9 +2,6 @@ import json
 import logging
 import datetime
 
-# from bot.add_receipt import get_receipt_date, get_receipt_total, \
-#     get_receipt_products, get_receipt_seller, add_result_db
-from bot.markup import markup
 from bot.services import parse_json_file, convert_price
 from hasta_la_vista_money.receipts.models import Customer, Receipt, Product
 import os
@@ -108,74 +105,3 @@ def not_json_file(message):
     if message.document.mime_type != 'application/json':
         bot_admin.send_message(message.chat.id,
                                'Файл должен быть только в формате JSON!')
-
-
-@bot_admin.message_handler(commands=['add'])
-def add_receipt(message):
-    text = message.text
-    if text == '/add':
-        bot_admin.send_message(message.chat.id, 'Выберите действие:',
-                               reply_markup=markup())
-
-# ПЕРЕДЕЛАТЬ!
-# @bot_admin.callback_query_handler(func=lambda call: True) """ This function
-# is a callback handler for handling inline button clicks in a telegram bot.
-# It executes the corresponding function based on the value of the button
-# click data.
-#
-# Input: call: CallbackQuery object from the telegram bot API, containing
-# information on the button click event.
-#
-# Output: Sends a message to the chat asking for specific information (e.g.
-# date, seller, total, products), or confirms receipt addition to the database.
-#
-# Handlers: 'date': prompts the user to input the receipt date and calls
-# get_receipt_date function 'seller': prompts the user to input the receipt
-# seller and calls get_receipt_seller function 'total': prompts the user to
-# input the receipt total and calls get_receipt_total function 'products':
-# prompts the user to input the receipt products and calls
-# get_receipt_products function
-#
-# Result: Sends a message to the chat confirming receipt addition to the
-# database and calls add_result_db function.
-#
-# :param call: The callback query object containing the callback data,
-# message and other metadata. :type call: telegram.CallbackQuery
-#
-# :return: None
-# """
-# def callback_add_receipt(call):
-#     data = call.data
-#     message = call.message
-#     handlers = {
-#         'date': ('Введи дату', get_receipt_date),
-#         'seller': ('Введи имя продавца', get_receipt_seller),
-#         'total': ('Введи итоговую сумму чека', get_receipt_total),
-#         'products': ('Введи информацию о продуктах', get_receipt_products),
-#     }
-#     result = {
-#         'result': 'Чек будет добавлен в базу данных!',
-#     }
-#     if data in handlers:
-#         bot_admin.send_message(message.chat.id, handlers[data][0])
-#         bot_admin.register_next_step_handler(message, handlers[data][1])
-#     if data in result:
-#         bot_admin.send_message(message.chat.id, result[data])
-#         add_result_db(message)
-# def process_values(message):
-#     # Process the values filled in by the user
-#     while True:
-#         if message == 'stop':
-#             break
-#         else:
-#             values = message.text.split()
-#             print(values)
-#
-#
-# @bot_admin.callback_query_handler(func=lambda call:
-# call.data == 'fill_values')
-# def callback_fill_values(call):
-#     # Ask the user to fill in the values
-#     bot_admin.send_message(chat_id=call.message.chat.id,
-#                            text="Please fill in the values:")
-#     bot_admin.register_next_step_handler(call.message, process_values)
