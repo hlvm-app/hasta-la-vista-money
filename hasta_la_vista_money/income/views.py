a2c413c032e97e3ce9aae7f39d9785d78ca41072
Alexander <dev@pavlovteam.ru>
2023-02-19 20:36:39 +0300

    added get and post methods for AddIncome class

--- hasta_la_vista_money/income/views.py
+++ hasta_la_vista_money/income/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy
 from django.views.generic import ListView, CreateView, FormView
@@ -9,7 +10,7 @@ from .forms import IncomeForm
 
 class IncomeView(LoginRequiredMixin, ListView):
     model = Income
-    template_name = 'income/index.html'
+    template_name = 'income/income.html'
     context_object_name = 'incomes'
     error_message = gettext_lazy('У вас нет прав на просмотр данной страницы! '
                                  'Авторизуйтесь!')
@@ -20,6 +21,17 @@ class AddIncome(LoginRequiredMixin, CreateView, FormView):
     model = Income
     form_class = IncomeForm
     template_name = 'income/add_income.html'
-    success_url = reverse_lazy('income:create_income')
+    success_url = reverse_lazy('income:list')
 
+    def get(self, request, *args, **kwargs):
+        income_form = IncomeForm()
+        return self.render_to_response({'income_form': income_form})
+
+    def post(self, request, *args, **kwargs):
+        income_form = IncomeForm(request.POST)
+        if income_form.is_valid():
+            income_form.save()
+            return redirect(reverse_lazy('income:list'))
+        else:
+            return self.render_to_response({'income_form': income_form})
 
