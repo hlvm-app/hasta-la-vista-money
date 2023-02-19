afe88844dca67cea6661270073cce6e8f9fd7fa6
Alexander <dev@pavlovteam.ru>
2023-02-19 12:55:21 +0300

    Added a script to add another product

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,20 @@
-document.getElementById("close-button").addEventListener("click", function() {
-    window.close();
-  });
+let birdForm = document.querySelectorAll(".form-product")
+let container = document.querySelector("#form-create-receipt")
+let addButton = document.querySelector("#add-form")
+let totalForms = document.querySelector("#id_form-TOTAL_FORMS")
+
+let formNum = birdForm.length-1
+addButton.addEventListener('click', addForm)
+
+function addForm(e){
+    e.preventDefault()
+
+    let newForm = birdForm[0].cloneNode(true)
+    let formRegex = RegExp(`form-(\\d){1}-`,'g')
+
+    formNum++
+    newForm.innerHTML = newForm.innerHTML.replace(formRegex, `form-${formNum}-`)
+    container.insertBefore(newForm, addButton)
+
+    totalForms.setAttribute('value', `${formNum+1}`)
+}
