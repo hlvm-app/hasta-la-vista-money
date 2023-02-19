971eb92cb168faa4ef38d36a70f3e902774b3d99
Alexander <dev@pavlovteam.ru>
2023-02-19 20:35:11 +0300

    changed format date

--- hasta_la_vista_money/income/models.py
+++ hasta_la_vista_money/income/models.py
@@ -1,30 +1,33 @@
 from django.db import models
-
+from datetime import datetime
 
 salary = (
     ('Зарплата', 'Зарплата'),
 )
 
+current_year = datetime.now().year
+
+
 months = (
-    ('Январь', 'Январь'),
-    ('Февраль', 'Февраль'),
-    ('Март', 'Март'),
-    ('Апрель', 'Апрель'),
-    ('Май', 'Май'),
-    ('Июнь', 'Июнь'),
-    ('Июль', 'Июль'),
-    ('Август', 'Август'),
-    ('Сентябрь', 'Сентябрь'),
-    ('Октябрь', 'Месяц'),
-    ('Ноябрь', 'Ноябрь'),
-    ('Декабрь', 'Декабрь'),
+    (f'Январь {current_year}', f'Январь {current_year}'),
+    (f'Февраль {current_year}', f'Февраль {current_year}'),
+    (f'Март {current_year}', f'Март {current_year}'),
+    (f'Апрель {current_year}', f'Апрель {current_year}'),
+    (f'Май {current_year}', f'Май {current_year}'),
+    (f'Июнь {current_year}', f'Июнь {current_year}'),
+    (f'Июль {current_year}', f'Июль {current_year}'),
+    (f'Август {current_year}', f'Август {current_year}'),
+    (f'Сентябрь {current_year}', f'Сентябрь {current_year}'),
+    (f'Октябрь {current_year}', f'Октябрь {current_year}'),
+    (f'Ноябрь {current_year}', f'Ноябрь {current_year}'),
+    (f'Декабрь {current_year}', f'Декабрь {current_year}'),
 )
 
 
 class Income(models.Model):
     type_income = models.CharField(max_length=10, choices=salary)
-    month = models.CharField(max_length=10, choices=months)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    month = models.CharField(max_length=20, choices=months)
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
 
     def __str__(self):
         return self.type_income
