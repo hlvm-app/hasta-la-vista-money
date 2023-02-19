b56f35da728f4ea22694dc2f85550dcedc659247
Alexander <dev@pavlovteam.ru>
2023-02-19 12:45:59 +0300

    added three forms - CustomerForm, ProductForm, ReceiptForm

--- hasta_la_vista_money/receipts/forms.py
+++ hasta_la_vista_money/receipts/forms.py
@@ -1,6 +1,10 @@
 # import django_filters
-from django import forms
-from django.utils.translation import gettext_lazy
+from django.forms import ModelForm, formset_factory, DateTimeInput, DateInput
+from django.utils.translation import gettext_lazy as _
+
+from hasta_la_vista_money.receipts.models import Customer, Receipt, Product
+
+
 #
 # from receipts.models import Customers
 
@@ -14,3 +18,38 @@ from django.utils.translation import gettext_lazy
 #     class Meta:
 #         model = Customers
 #         fields = ('name_seller',)
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = ['name_seller']
+        labels = {
+            'name_seller': _('Продавец')
+        }
+
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = ['product_name', 'price', 'quantity', 'amount']
+        labels = {
+            'product_name': _('Наименование продукта'),
+            'price': _('Цена'),
+            'quantity': _('Количество'),
+            'amount': _('Сумма')
+        }
+
+
+ProductFormSet = formset_factory(ProductForm, extra=1)
+
+
+class ReceiptForm(ModelForm):
+    class Meta:
+        model = Receipt
+        fields = ['receipt_date', 'operation_type', 'total_sum']
+        labels = {
+            'receipt_date': _('Дата и время чека'),
+            'operation_type': _('Тип операции'),
+            'total_sum': _('Итоговая сумма по чеку')
+        }
+
+    products = ProductFormSet()
