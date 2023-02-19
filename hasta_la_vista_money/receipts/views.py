4e9b3fe5693820027d55a02ecae9f76e120e6ec3
Alexander <dev@pavlovteam.ru>
2023-02-19 12:59:49 +0300

    added reciptcreateview class for forms of adding receipts

--- hasta_la_vista_money/receipts/views.py
+++ hasta_la_vista_money/receipts/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views import View
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy
 from django.views.generic import CreateView
-from django.views.generic.edit import FormMixin
 
-from .forms import ReceiptForm
+from .forms import CustomerForm, ReceiptForm, ProductFormSet
 from .models import Receipt
 
 
@@ -34,11 +33,29 @@ class ReceiptView(LoginRequiredMixin, View, SuccessMessageMixin):
         return self.get(request)
 
 
-class CreateReceiptView(LoginRequiredMixin, CreateView, FormMixin):
+class ReceiptCreateView(CreateView):
     model = Receipt
-    template_name = 'receipts/create_customer.html'
-    form_class = ReceiptForm
-    success_url = reverse_lazy('receipts:list')
-    error_message = gettext_lazy('У вас нет прав на просмотр данной страницы! '
-                                 'Авторизуйтесь!')
-    no_permission_url = reverse_lazy('login')
+    template_name = 'receipts/create_receipt.html'
+    success_url = 'receipts:list'
+
+    def get(self, request, *args, **kwargs):
+        seller_form = CustomerForm()
+        receipt_form = ReceiptForm()
+        product_formset = ProductFormSet()
+        return self.render_to_response({'seller_form': seller_form, 'receipt_form': receipt_form, 'product_formset': product_formset})
+
+    def post(self, request, *args, **kwargs):
+        seller_form = CustomerForm(request.POST)
+        receipt_form = ReceiptForm(request.POST)
+        product_formset = ProductFormSet(request.POST)
+        if seller_form.is_valid() and receipt_form.is_valid() and product_formset.is_valid():
+            seller = seller_form.save()
+            receipt = receipt_form.save(commit=False)
+            receipt.customer = seller
+            receipt.save()
+            for product_form in product_formset:
+                product = product_form.save()
+                receipt.product.add(product)
+            return redirect(reverse_lazy("receipts:list"))
+        else:
+            return self.render_to_response({'seller_form': seller_form, 'receipt_form': receipt_form, 'product_formset': product_formset})
