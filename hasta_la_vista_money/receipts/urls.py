3480fbc97b26a76f5a15fc64d37437ed42dc77a7
Alexander <dev@pavlovteam.ru>
2023-02-19 12:58:59 +0300

    rename class

--- hasta_la_vista_money/receipts/urls.py
+++ hasta_la_vista_money/receipts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from hasta_la_vista_money.receipts.views import ReceiptView, CreateReceiptView
+from hasta_la_vista_money.receipts.views import ReceiptView, ReceiptCreateView
 
 app_name = 'receipts'
 urlpatterns = [
     path('', ReceiptView.as_view(), name='list'),
-    path('create/', CreateReceiptView.as_view(), name='create')
+    path('create/', ReceiptCreateView.as_view(), name='create')
 ]
