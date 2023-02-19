166e0324c59302b77e9fb9d7bbea98e327440b7b
Alexander <dev@pavlovteam.ru>
2023-02-19 20:34:06 +0300

    changed type fields

--- hasta_la_vista_money/income/forms.py
+++ hasta_la_vista_money/income/forms.py
@@ -7,7 +7,7 @@ from hasta_la_vista_money.income.models import Income
 class IncomeForm(ModelForm):
     class Meta:
         model = Income
-        fields = ('type_income', 'month', 'amount')
+        fields = ['type_income', 'month', 'amount']
         labels = {
             'type_income': gettext_lazy('Тип дохода'),
             'month': gettext_lazy('Месяц'),
