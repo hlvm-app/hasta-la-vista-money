d245d41acbe29a9a73f5cbec953a14a60709f59f
Alexander <dev@pavlovteam.ru>
2023-02-21 20:46:32 +0300

    added django.contrib.humanize

--- hasta_la_vista_money/settings.py
+++ hasta_la_vista_money/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.humanize',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',
