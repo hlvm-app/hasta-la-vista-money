8d937bcf37831bf4c0083b64a67f3ccb1a26e5c2
Alexander <dev@pavlovteam.ru>
2023-02-25 11:31:57 +0300

    remove line STATICFILES_STORAGE

--- hasta_la_vista_money/settings.py
+++ hasta_la_vista_money/settings.py
@@ -194,8 +194,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
