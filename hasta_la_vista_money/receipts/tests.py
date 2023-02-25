73112acfc5c9d1ba931eecebec919e17b2086ce7
Alexander <dev@pavlovteam.ru>
2023-02-25 10:41:23 +0300

    added tests for receipts

--- hasta_la_vista_money/receipts/tests.py
+++ hasta_la_vista_money/receipts/tests.py
@@ -1,22 +1,43 @@
 from django.test import TestCase
 from django.urls import reverse_lazy
 
-from hasta_la_vista_money.receipts.models import Receipt
+from hasta_la_vista_money.receipts.models import Receipt, Product, Customer
 from hasta_la_vista_money.users.models import User
 
 
 class TestReceipt(TestCase):
 
-    fixtures = ['receipts.yaml', 'users.yaml']
+    fixtures = ['receipt.yaml', 'users.yaml', 'customer.yaml', 'product.yaml']
 
     def setUp(self) -> None:
         self.user = User.objects.get(pk=1)
-        self.receipt1 = Receipt.objects.get(pk=4)
-        self.receipt2 = Receipt.objects.get(pk=5)
+        self.receipt = Receipt.objects.get(pk=57)
+        self.customer = Customer.objects.get(pk=66)
+        self.product1 = Product.objects.get(pk=40)
+        self.product2 = Product.objects.get(pk=41)
 
     def test_receipt_list(self):
         self.client.force_login(self.user)
         response = self.client.get(reverse_lazy('receipts:list'))
         self.assertEqual(response.status_code, 200)
-        receipts_list = list(response.context['receipts'])
-        self.assertQuerysetEqual(receipts_list, [self.receipt1, self.receipt2])
+
+    def test_product(self):
+        self.client.force_login(self.user)
+        self.assertEqual(self.receipt.product.count(), 2)
+        self.assertIn(self.product1, self.receipt.product.all())
+        self.assertIn(self.product2, self.receipt.product.all())
+
+    def test_customer_create(self):
+        self.client.force_login(self.user)
+        self.customer = Customer.objects.create(
+            name_seller='ООО "Пятерочка"',
+            retail_place_address='Test Address',
+            retail_place='Test place'
+        )
+        self.assertEqual(self.customer.pk, 68)
+
+    def test_delete_customer(self):
+        customer = Customer.objects.get(pk=67)
+        customer.delete()
+        with self.assertRaises(Customer.DoesNotExist):
+            Customer.objects.get(pk=67)
