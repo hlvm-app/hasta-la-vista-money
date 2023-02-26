70b5741d4b39bb3eeb98ea1d99da8ca19b0119e8
Alexander <dev@pavlovteam.ru>
2023-02-26 13:41:53 +0300

    Saving the json file was converted into a temporary folder by the tempfile library

--- bot/telegrambot.py
+++ bot/telegrambot.py
@@ -1,13 +1,13 @@
 import json
 import os
 import datetime
+import tempfile
 
 import telebot
 
 from bot.services import parse_json_file, convert_price
 from hasta_la_vista_money.receipts.models import Customer, Receipt, Product
 from .log_config import logger, TelegramLogsHandler
-
 
 token = os.environ.get('TOKEN_TELEGRAM_BOT')
 bot_admin = telebot.TeleBot(token, parse_mode='html')
@@ -16,21 +16,23 @@ id_group_user = os.environ.get('ID_GROUP_USER')
 logger.addHandler(TelegramLogsHandler(bot_admin, id_group_user))
 
 
-@bot_admin.message_handler(func=lambda message: message.document.mime_type ==
-                           'application/json',
-                           content_types=['document'])
+@bot_admin.message_handler(content_types=['document'])
 def get_receipt(message):
+    if message.document.mime_type != 'application/json':
+        bot_admin.send_message(message.chat.id,
+                               'Файл должен быть только в формате JSON!')
+        return
     try:
         file_info = bot_admin.get_file(message.document.file_id)
         file_downloaded = bot_admin.download_file(
             file_path=file_info.file_path
         )
-        src = f'bot/receipt/{message.document.file_name}'
-        with open(src, 'wb') as file:
-            file.write(file_downloaded)
 
-        with open(file.name, 'r') as json_file:
-            json_data = json.load(json_file)
+        with tempfile.NamedTemporaryFile(suffix='.json') as temp_json_file:
+            print(temp_json_file)
+            temp_json_file.write(file_downloaded)
+            temp_json_file.seek(0)
+            json_data = json.load(temp_json_file)
 
             customer = Customer.objects.create(
                 name_seller=parse_json_file(json_data)[1],
@@ -66,29 +68,13 @@ def get_receipt(message):
 
             bot_admin.send_message(message.chat.id, 'Чек принят!')
 
-        os.remove(src)
-
-    except FileNotFoundError as error:
+    except (FileNotFoundError, json.decoder.JSONDecodeError) as error:
         logger.error(
             f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S} произошла ошибка: '
-            f'{error}. Файл json не удалился'
-        )
-    except json.decoder.JSONDecodeError as error:
-        logger.error(
-            f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S} произошла ошибка '
-            f'JSON файла: {error}'
+            f'{error}.'
         )
     except Exception as error:
         logger.error(
             f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S} произошла ошибка: '
             f'{error}'
         )
-
-
-@bot_admin.message_handler(func=lambda message: message.document.mime_type !=
-                           'application/json',
-                           content_types=['document'])
-def not_json_file(message):
-    if message.document.mime_type != 'application/json':
-        bot_admin.send_message(message.chat.id,
-                               'Файл должен быть только в формате JSON!')
