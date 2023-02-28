3043796a2c76e86b72e765c824915c4b8acbf7c3
Alexander <dev@pavlovteam.ru>
2023-02-28 06:13:03 +0300

    write class for get receipt from qr-code

--- bot/services.py
+++ bot/services.py
@@ -1,4 +1,8 @@
 import datetime
+import os
+
+import requests
+from dotenv import load_dotenv
 
 
 # Выделяем дату из json
@@ -10,13 +14,86 @@ def convert_price(price):
     return round(price / 100, 2)
 
 
-def parse_json_file(json_data):
-    date_time = convert_date_time(json_data.get('dateTime', 'Нет данных'))
-    seller = json_data.get('user', 'Нет данных')
-    total_sum = convert_price(json_data.get('totalSum', 'Нет данных'))
-    retail_place_address = json_data.get('retailPlaceAddress', 'Нет данных')
-    operation_type = json_data.get('operationType', 'Нет данных')
-    retail_place = json_data.get('retailPlace', 'Нет данных')
+def get_value(dictionary, key):
+    if isinstance(dictionary, dict):
+        for k, v in dictionary.items():
+            if k == key:
+                return v
+            elif isinstance(v, dict):
+                result = get_value(v, key)
+                if result is not None:
+                    return result
+    return None
 
-    return date_time, seller, total_sum, retail_place_address, \
-        operation_type, retail_place
+
+class ReceiptApiReceiver:
+    def __init__(self) -> None:
+        load_dotenv()
+        self.__session_id = None
+        self.host = 'irkkt-mobile.nalog.ru:8888'
+        self.device_os = 'Android'
+        self.client_version = '2.9.0'
+        self.device_id = 'a5e1e72bf5b9966690e10f5ce03cd8e99e0b23dc4'
+        self.accept = '*/*'
+        self.user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) ' \
+                          'Gecko/20100101 Firefox/110.0'
+        self.accept_language = 'ru-RU;q=1, en-US;q=0.9'
+        self.set_session_id()
+
+    def set_session_id(self) -> None:
+        client_secret = [env for env in ['CLIENT_SECRET', 'INN', 'PASSWORD']
+                         if os.getenv(env) is None]
+        if client_secret:
+            raise ValueError(f'OS environments not content '
+                             f'{", ".join(client_secret)}')
+
+        url = f'https://{self.host}/v2/mobile/users/lkfl/auth'
+        payload = {
+            'inn': os.getenv('INN'),
+            'client_secret': os.getenv('CLIENT_SECRET'),
+            'password': os.getenv('PASSWORD')
+        }
+        headers = {
+            'Host': self.host,
+            'Accept': self.accept,
+            'Device-OS': self.device_os,
+            'Device-Id': self.device_id,
+            'clientVersion': self.client_version,
+            'Accept-Language': self.accept_language,
+            'User-Agent': self.user_agent,
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+        self.__session_id = response.json()['sessionId']
+
+    def _get_receipt_id(self, qr: str) -> str:
+        url = f'https://{self.host}/v2/ticket'
+        payload = {'qr': qr}
+        headers = {
+            'Host': self.host,
+            'Accept': self.accept,
+            'Device-OS': self.device_os,
+            'Device-Id': self.device_id,
+            'clientVersion': self.client_version,
+            'Accept-Language': self.accept_language,
+            'sessionId': self.__session_id,
+            'User-Agent': self.user_agent,
+        }
+        resp = requests.post(url, json=payload, headers=headers)
+        return resp.json()["id"]
+
+    def get_receipt(self, qr: str) -> dict:
+        ticket_id = self._get_receipt_id(qr)
+        url = f'https://{self.host}/v2/tickets/{ticket_id}'
+        headers = {
+            'Host': self.host,
+            'sessionId': self.__session_id,
+            'Device-OS': self.device_os,
+            'clientVersion': self.client_version,
+            'Device-Id': self.device_id,
+            'Accept': self.accept,
+            'User-Agent': self.user_agent,
+            'Accept-Language': self.accept_language,
+        }
+        resp = requests.get(url, headers=headers)
+        return resp.json()
