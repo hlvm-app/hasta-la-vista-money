b7ad878b683bb170493f5b95b94c0d3c876cf425
Alexander <dev@pavlovteam.ru>
2023-03-01 21:50:59 +0300

    Parse information from receipt taken out in a separate function

--- bot/telegrambot.py
+++ bot/telegrambot.py
@@ -6,8 +6,10 @@ import tempfile
 
 import telebot
 
-from bot.services import convert_price, convert_date_time, ReceiptApiReceiver, \
-    get_value
+from bot.services import convert_price, \
+    convert_date_time, \
+    ReceiptApiReceiver, \
+    ParseJson
 from hasta_la_vista_money.receipts.models import Customer, Receipt, Product
 from .log_config import logger, TelegramLogsHandler
 
@@ -19,10 +21,10 @@ logger.addHandler(TelegramLogsHandler(bot_admin, id_group_user))
 
 
 CONSTANT_RECEIPT = {
-    'seller': 'user',
+    'name_seller': 'user',
     'retail_place_address': 'retailPlaceAddress',
     'retail_place': 'retailPlace',
-    'date_time': 'dateTime',
+    'receipt_date': 'dateTime',
     'operation_type': 'operationType',
     'total_sum': 'totalSum',
     'product_name': 'name',
@@ -30,8 +32,85 @@ CONSTANT_RECEIPT = {
     'quantity': 'quantity',
     'amount': 'sum',
     'nds_type': 'nds',
-    'nds_sum': 'ndsSum'
+    'nds_sum': 'ndsSum',
+    'items': 'items'
 }
+
+
+def parse_receipt(json_data, chat_id):
+    parser = ParseJson(json_data)
+
+    # Getting of receipt items without products
+    name_seller = parser.parse_json(
+        json_data, CONSTANT_RECEIPT.get('name_seller')
+    )
+    retail_place_address = parser.parse_json(
+        json_data, CONSTANT_RECEIPT.get('retail_place_address')
+    )
+    retail_place = parser.parse_json(
+        json_data, CONSTANT_RECEIPT.get('retail_place')
+    )
+    customer = Customer.objects.create(
+        name_seller=name_seller,
+        retail_place_address=retail_place_address,
+        retail_place=retail_place
+    )
+
+    receipt_date = convert_date_time(parser.parse_json(
+        json_data, CONSTANT_RECEIPT.get('receipt_date')
+    ))
+    operation_type = parser.parse_json(
+        json_data, CONSTANT_RECEIPT.get('operation_type')
+    )
+    total_sum = convert_price(parser.parse_json(
+        json_data, CONSTANT_RECEIPT.get('total_sum')
+    ))
+    receipt = Receipt.objects.create(
+        receipt_date=receipt_date,
+        operation_type=operation_type,
+        total_sum=total_sum,
+        customer=customer
+    )
+
+    # Getting a list of products
+    products_list = parser.parse_json(
+        json_data, CONSTANT_RECEIPT.get('items')
+    )
+
+    # Getting an information of products
+    result_products_list = []
+    for product in products_list:
+        print(product, type(product))
+        product_name = parser.parse_json(
+            product, CONSTANT_RECEIPT.get('product_name')
+        )
+        price = convert_price(parser.parse_json(
+            product, CONSTANT_RECEIPT.get('price')
+        ))
+        quantity = parser.parse_json(
+            product, CONSTANT_RECEIPT.get('quantity')
+        )
+        amount = convert_price(parser.parse_json(
+            product, CONSTANT_RECEIPT.get('amount')
+        ))
+        nds_type = parser.parse_json(
+            product, CONSTANT_RECEIPT.get('nds_type')
+        )
+        nds_sum = parser.parse_json(
+            product, CONSTANT_RECEIPT.get('nds_sum')
+        )
+        products = Product.objects.create(
+            product_name=product_name,
+            price=price,
+            quantity=quantity,
+            amount=amount,
+            nds_type=nds_type,
+            nds_sum=nds_sum
+        )
+        result_products_list.append(products)
+    receipt.product.set(result_products_list)
+
+    bot_admin.send_message(chat_id, 'Чек принят!')
 
 
 @bot_admin.message_handler(content_types=['document'])
@@ -51,55 +130,12 @@ def get_receipt(message):
             temp_json_file.seek(0)
             json_data = json.load(temp_json_file)
 
-            customer = Customer.objects.create(
-                name_seller=json_data.get(CONSTANT_RECEIPT['seller']),
-                retail_place_address=json_data.get(
-                    CONSTANT_RECEIPT['retail_place_address']
-                ),
-                retail_place=json_data.get(CONSTANT_RECEIPT['retail_place']),
-            )
-
-            receipt = Receipt.objects.create(
-                receipt_date=json_data.get(CONSTANT_RECEIPT['date_time']),
-                operation_type=json_data.get(
-                    CONSTANT_RECEIPT['operation_type']
-                ),
-                total_sum=json_data.get(CONSTANT_RECEIPT['total_sum']),
-                customer=customer,
-            )
-
-            products = []
-            for item in json_data['items']:
-                product_name = item.get(
-                    CONSTANT_RECEIPT['product_name'], 'Нет данных'
-                )
-                price = convert_price(item.get(CONSTANT_RECEIPT['price'], 0))
-                quantity = item.get(CONSTANT_RECEIPT['quantity'], 0)
-                amount = convert_price(item.get(CONSTANT_RECEIPT['amount'], 0))
-                nds_type = item.get(CONSTANT_RECEIPT['nds_type'], -1)
-                nds_sum = item.get(CONSTANT_RECEIPT['nds_sum'], 0)
-                goods = Product.objects.create(
-                    product_name=product_name,
-                    price=price,
-                    quantity=quantity,
-                    amount=amount,
-                    nds_type=nds_type,
-                    nds_sum=nds_sum
-                )
-                products.append(goods)
-            receipt.product.set(products)
-
-            bot_admin.send_message(message.chat.id, 'Чек принят!')
+            parse_receipt(json_data, message.chat.id)
 
     except (FileNotFoundError, json.decoder.JSONDecodeError) as error:
         logger.error(
             f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S} произошла ошибка: '
             f'{error}.'
-        )
-    except Exception as error:
-        logger.error(
-            f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S} произошла ошибка: '
-            f'{error}'
         )
 
 
@@ -113,14 +149,9 @@ def get_receipt_text(message):
         try:
             client = ReceiptApiReceiver()
             qr_code = input_user
-            receipt = client.get_receipt(qr_code)
-            json_data_dumps = json.dumps(receipt, ensure_ascii=False)
-            json_data_load = json.loads(json_data_dumps)
+            json_data = client.get_receipt(qr_code)
 
-            date_time = convert_date_time(
-                get_value(json_data_load, CONSTANT_RECEIPT['date_time'])
-            )
-            print(date_time, 'date_time')
+            parse_receipt(json_data, message.chat.id)
 
         except ValueError as value_error:
             bot_admin.send_message(message.chat.id, str(value_error))
