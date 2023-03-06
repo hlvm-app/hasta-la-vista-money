05fe685c130da6d471bb86652c715430d6f5f682
Alexander <dev@pavlovteam.ru>
2023-03-06 20:54:59 +0300

    added form CustomerInputForm in the ReceiptCreateView

--- hasta_la_vista_money/receipts/views.py
+++ hasta_la_vista_money/receipts/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views import View
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy
 from django.views.generic import CreateView
 from django_filters import rest_framework as filters
 from django_filters.views import FilterView
 
-from .forms import CustomerForm, ReceiptForm, ProductFormSet, ReceiptFilter
+from .forms import CustomerForm, ReceiptForm, ProductFormSet, ReceiptFilter, \
+    CustomerInputForm
 from .models import Receipt
 
 
@@ -26,8 +26,10 @@ class ReceiptView(LoginRequiredMixin, SuccessMessageMixin, FilterView):
         if 'delete_button' in request.POST:
             receipt_id = request.POST.get('receipt_id')
             receipt = get_object_or_404(self.model, pk=receipt_id)
+
             for product in receipt.product.all():
                 product.delete()
+
             receipt.customer.delete()
             receipt.delete()
         return self.get(request)
@@ -40,23 +42,34 @@ class ReceiptCreateView(CreateView):
 
     def get(self, request, *args, **kwargs):
         seller_form = CustomerForm()
+        seller_input_form = CustomerInputForm()
         receipt_form = ReceiptForm()
         product_formset = ProductFormSet()
+
         return self.render_to_response({
             'seller_form': seller_form,
+            'seller_input_form': seller_input_form,
             'receipt_form': receipt_form,
             'product_formset': product_formset})
 
     def post(self, request, *args, **kwargs):
+
         seller_form = CustomerForm(request.POST)
+        seller_input_form = CustomerInputForm(request.POST)
         receipt_form = ReceiptForm(request.POST)
         product_formset = ProductFormSet(request.POST)
-        if seller_form.is_valid() and receipt_form.is_valid() and \
-                product_formset.is_valid():
+
+        if seller_form.is_valid() and seller_input_form.is_valid() \
+                and receipt_form.is_valid() and product_formset.is_valid():
             seller = seller_form.save()
+            seller_input = seller_input_form.save()
             receipt = receipt_form.save(commit=False)
+
+            if seller is None:
+                receipt.customer = seller_input
             receipt.customer = seller
             receipt.save()
+
             for product_form in product_formset:
                 product = product_form.save()
                 receipt.product.add(product)
@@ -64,6 +77,7 @@ class ReceiptCreateView(CreateView):
         else:
             return self.render_to_response({
                 'seller_form': seller_form,
+                'seller_input_form': seller_input_form,
                 'receipt_form': receipt_form,
                 'product_formset': product_formset
             })
