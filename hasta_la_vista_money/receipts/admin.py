5b485b70b925528f62b9ef1f7ff18efa74ad6388
Alexander <dev@pavlovteam.ru>
2023-03-08 15:21:37 +0300

    test lint

--- hasta_la_vista_money/receipts/admin.py
+++ hasta_la_vista_money/receipts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from hasta_la_vista_money.receipts.models import Product, Receipt, Customer
+from hasta_la_vista_money.receipts.models import Customer, Product, Receipt
 
 admin.site.register(Customer)
 admin.site.register(Product)
