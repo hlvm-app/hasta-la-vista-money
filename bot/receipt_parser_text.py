55c3405e075ed7f4524ada693d3400d74547c0ad
Alexander <dev@pavlovteam.ru>
2023-03-21 23:08:02 +0300

    get_receipt_text вынес в отдельный модуль и переименовал

---
+++ bot/receipt_parser_text.py
@@ -0,0 +1,35 @@
+import datetime
+import re
+
+from bot.config_bot import bot_admin
+from bot.json_parse import ReceiptParser
+from bot.log_config import logger
+from bot.services import ReceiptApiReceiver
+
+
+@bot_admin.message_handler(content_types=['text'])
+def handle_receipt_text(message):
+    input_user = message.text
+    pattern = (
+        r't=[0-9]+T[0-9]+'
+        r'&s=[0-9]+.[0-9]+&fn=[0-9]+'
+        r'&i=[0-9]+&fp=[0-9]+&n=[0-5]{1}'
+    )
+
+    text_pattern = re.match(pattern, input_user)
+    if text_pattern:
+        try:
+            client = ReceiptApiReceiver()
+            qr_code = input_user
+            json_data = client.get_receipt(qr_code)
+
+            parse = ReceiptParser(json_data, message.chat.id)
+            parse.parse(message.chat.id)
+
+        except Exception as error:
+            logger.error(
+                f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S}\n'
+                f'произошла ошибка: {error}.',
+            )
+    else:
+        bot_admin.send_message(message.chat.id, 'Недопустимый текст')
