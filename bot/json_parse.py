a6106f0789753b5ae4b1a98f289105240afa4e62
Alexander <dev@pavlovteam.ru>
2023-03-21 22:22:45 +0300

    Парсинг json и чека выделил в отдельный модуль и упаковал в разные классы

--- bot/json_parse.py
+++ bot/json_parse.py
@@ -1,7 +1,7 @@
 import types
 
 from bot.config_bot import bot_admin
-from bot.services import ParseJson, convert_date_time, convert_price
+from bot.services import convert_date_time, convert_price
 from hasta_la_vista_money.receipts.models import Customer, Product, Receipt
 
 CONSTANT_RECEIPT = types.MappingProxyType(
@@ -23,76 +23,109 @@ CONSTANT_RECEIPT = types.MappingProxyType(
 )
 
 
-def parse_receipt(json_data, chat_id):  # noqa: WPS210
-    parser = ParseJson(json_data)
+class ParserJson:
+    def __init__(self, json_data):
+        self.json_data = json_data
 
-    # Getting of receipt items without products
-    name_seller = parser.parse_json(
-        json_data, CONSTANT_RECEIPT.get('name_seller'),
-    )
-    retail_place_address = parser.parse_json(
-        json_data, CONSTANT_RECEIPT.get('retail_place_address'),
-    )
-    retail_place = parser.parse_json(
-        json_data, CONSTANT_RECEIPT.get('retail_place'),
-    )
-    customer = Customer.objects.create(
-        name_seller=name_seller,
-        retail_place_address=retail_place_address,
-        retail_place=retail_place,
-    )
+    def parse_json(self, json_data: dict, key: str):
+        return ParserJson.get_value(self, json_data, key)
 
-    receipt_date = convert_date_time(parser.parse_json(
-        json_data, CONSTANT_RECEIPT.get('receipt_date'),
-    ))
-    operation_type = parser.parse_json(
-        json_data, CONSTANT_RECEIPT.get('operation_type'),
-    )
-    total_sum = convert_price(parser.parse_json(
-        json_data, CONSTANT_RECEIPT.get('total_sum'),
-    ))
-    receipt = Receipt.objects.create(
-        receipt_date=receipt_date,
-        operation_type=operation_type,
-        total_sum=total_sum,
-        customer=customer,
-    )
+    def get_value(self, dictionary, key):
+        if not isinstance(dictionary, dict):
+            return None
 
-    # Getting a list of products
-    products_list = parser.parse_json(
-        json_data, CONSTANT_RECEIPT.get('items'),
-    )
+        dict_value = dictionary.get(key)
+        if dict_value is not None:
+            return dict_value
 
-    # Getting an information of products
-    result_products_list = []
-    for product in products_list:
-        product_name = parser.parse_json(
-            product, CONSTANT_RECEIPT.get('product_name'),
+        for nested_dict in dictionary.values():
+            dict_value = self.get_value(nested_dict, key)
+            if dict_value is not None:
+                return dict_value
+
+        return None
+
+
+class ParseReceipt:
+    def __init__(self, json_data, chat_id):
+        self.json_data = json_data
+        self.parser = ParserJson(self.json_data)
+        self.customer = None
+        self.receipt = None
+        self.product_list = []
+
+    def parse_customer(self):
+        name_seller = self.parser.parse_json(
+            self.json_data, CONSTANT_RECEIPT.get('name_seller')
         )
-        price = convert_price(parser.parse_json(
-            product, CONSTANT_RECEIPT.get('price'),
+        retail_place_address = self.parser.parse_json(
+            self.json_data, CONSTANT_RECEIPT.get('retailPlaceAddress')
+        )
+        retail_place = self.parser.parse_json(
+            self.json_data, CONSTANT_RECEIPT.get('retailPlace')
+        )
+        self.customer = Customer.objects.create(
+            name_seller=name_seller,
+            retail_place_address=retail_place_address,
+            retail_place=retail_place,
+        )
+
+    def parse_receipt(self):
+        receipt_date = convert_date_time(self.parser.parse_json(
+            self.json_data, CONSTANT_RECEIPT.get('receipt_date')
         ))
-        quantity = parser.parse_json(
-            product, CONSTANT_RECEIPT.get('quantity'),
+        operation_type = self.parser.parse_json(
+            self.json_data, CONSTANT_RECEIPT.get('operation_type')
         )
-        amount = convert_price(parser.parse_json(
-            product, CONSTANT_RECEIPT.get('amount'),
+        total_sum = convert_price(self.parser.parse_json(
+            self.json_data, CONSTANT_RECEIPT.get('total_sum')
         ))
-        nds_type = parser.parse_json(
-            product, CONSTANT_RECEIPT.get('nds_type'),
+        customer = self.customer
+        self.receipt = Receipt.objects.create(
+            receipt_date=receipt_date,
+            operation_type=operation_type,
+            total_sum=total_sum,
+            customer=customer,
         )
-        nds_sum = convert_price(parser.parse_json(
-            product, CONSTANT_RECEIPT.get('nds_sum'),
-        ))
-        products = Product.objects.create(
-            product_name=product_name,
-            price=price,
-            quantity=quantity,
-            amount=amount,
-            nds_type=nds_type,
-            nds_sum=nds_sum,
-        )
-        result_products_list.append(products)
-    receipt.product.set(result_products_list)
 
-    bot_admin.send_message(chat_id, 'Чек принят!')
+    def parse_products(self):
+        products_list = self.parser.parse_json(
+            self.json_data, CONSTANT_RECEIPT.get('items')
+        )
+        for product in products_list:
+            product_name = self.parser.parse_json(
+                product, CONSTANT_RECEIPT.get('product_name')
+            )
+            price = convert_price(
+                self.parser.parse_json(
+                    product, CONSTANT_RECEIPT.get('price')
+                )
+            )
+            quantity = self.parser.parse_json(
+                product, CONSTANT_RECEIPT.get('quantity')
+            )
+            amount = convert_price(self.parser.parse_json(
+                product, CONSTANT_RECEIPT.get('amount')
+            ))
+            nds_type = self.parser.parse_json(
+                product, CONSTANT_RECEIPT.get('nds_type')
+            )
+            nds_sum = convert_price(self.parser.parse_json(
+                product, CONSTANT_RECEIPT.get('nds_sum')
+            ))
+            products = Product.objects.create(
+                product_name=product_name,
+                price=price,
+                quantity=quantity,
+                amount=amount,
+                nds_type=nds_type,
+                nds_sum=nds_sum,
+            )
+            self.product_list.append(products)
+        self.receipt.product.set(self.product_list)
+
+    def parse(self, chat_id):
+        self.parse_customer()
+        self.parse_receipt()
+        self.parse_products()
+        bot_admin.send_message(chat_id, 'Чек принят!')
