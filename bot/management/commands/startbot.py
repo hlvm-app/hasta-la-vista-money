5c7f9a04b64930b48366d318ef4871f638dea7cb
Alexander <dev@pavlovteam.ru>
2023-03-21 23:08:22 +0300

    добавил обработчик

--- bot/management/commands/startbot.py
+++ bot/management/commands/startbot.py
@@ -1,8 +1,10 @@
-from bot.receipt_parser import bot_admin
+from bot.receipt_parser_json import bot_admin as bot_json
+from bot.receipt_parser_text import bot_admin as bot_text
 from django.core.management import BaseCommand
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        bot_admin.polling(none_stop=True, skip_pending=False)
+        bot_json.polling(none_stop=True, skip_pending=False)
+        bot_text.polling(none_stop=True, skip_pending=False)
