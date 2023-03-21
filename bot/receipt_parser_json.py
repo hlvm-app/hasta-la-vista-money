3cfbee0f33f642732eb5d5154822df9218f21878
Alexander <dev@pavlovteam.ru>
2023-03-21 23:07:58 +0300

    get_receipt_text вынес в отдельный модуль и переименовал

--- bot/receipt_parser_json.py
+++ bot/receipt_parser_json.py
@@ -1,15 +1,13 @@
 import datetime
 import json
-import re
 
 from bot.config_bot import bot_admin
 from bot.json_parse import ReceiptParser
 from bot.log_config import logger
-from bot.services import ReceiptApiReceiver
 
 
 @bot_admin.message_handler(content_types=['document'])
-def get_receipt(message):
+def handle_receipt_json(message):
     if message.document.mime_type != 'application/json':
         bot_admin.send_message(
             message.chat.id, 'Файл должен быть только в формате JSON!',
@@ -36,31 +34,3 @@ def get_receipt(message):
             f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S} произошла ошибка:'
             f' {error}.',
         )
-
-
-@bot_admin.message_handler(content_types=['text'])
-def get_receipt_text(message):
-    input_user = message.text
-    pattern = (
-        r't=[0-9]+T[0-9]+'
-        r'&s=[0-9]+.[0-9]+&fn=[0-9]+'
-        r'&i=[0-9]+&fp=[0-9]+&n=[0-5]{1}'
-    )
-
-    text_pattern = re.match(pattern, input_user)
-    if text_pattern:
-        try:
-            client = ReceiptApiReceiver()
-            qr_code = input_user
-            json_data = client.get_receipt(qr_code)
-
-            parse = ReceiptParser(json_data, message.chat.id)
-            parse.parse(message.chat.id)
-
-        except Exception as error:
-            logger.error(
-                f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S}\n'
-                f'произошла ошибка: {error}.',
-            )
-    else:
-        bot_admin.send_message(message.chat.id, 'Недопустимый текст')
