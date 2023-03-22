43688c8ba9db267d39e779fc9d50aa87dd0e419d
Alexander <dev@pavlovteam.ru>
2023-03-22 22:25:45 +0300

    Добавил форматирование в логгирование

--- bot/log_config.py
+++ bot/log_config.py
@@ -14,6 +14,11 @@ class TelegramLogsHandler(logging.Handler):
         self.chat_id = chat_id
         self.tg_bot = tg_bot
 
+    def format(self, record: logging.LogRecord) -> str:
+        return f"Появилось исключение!\n" \
+               f"В модуле {record.funcName} на строке {record.lineno} " \
+               f"есть ошибка: {record.getMessage()}"
+
     def emit(self, record):
         log_entry = self.format(record)
         self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
