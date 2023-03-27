910a075473fa6ccdd374723518bce543bf41014d
Alexander <dev@pavlovteam.ru>
2023-03-27 22:34:04 +0300

    make lint + fix tests

--- bot/tests.py
+++ bot/tests.py
@@ -1,31 +1,56 @@
 import json
 from os import environ
-from django.test import TestCase
-from bot.json_parse import ReceiptParser
 
+from bot.json_parse import ReceiptParser
+from django.test import TestCase
 from dotenv import load_dotenv
 
 load_dotenv()
 
+NUMBER_RECEIPT = 6887
+TOTAL_SUM = 217.6
+PRODUCT_LIST_LEN = 2
+PRICE_PRODUCT1 = 131.32
+PRICE_PRODUCT2 = 86.28
+QUANTITY_PRODUCT1 = 1
+QUANTITY_PRODUCT2 = 1
+AMOUNT_PRODUCT1 = PRICE_PRODUCT1 * QUANTITY_PRODUCT1
+AMOUNT_PRODUCT2 = PRICE_PRODUCT2 * QUANTITY_PRODUCT2
+
 
 class ReceiptParserTestCase(TestCase):
+    def setUp(self) -> None:
+        with open('fixtures/receipt.json', 'r') as receipt_json:
+            json_data = json.load(receipt_json)
+        self.parser = ReceiptParser(json_data)
+        self.parser.parse(environ.get('ID_GROUP_USER'))
 
     def test_parse_receipt(self):
-        with open('fixtures/receipt.json', 'r') as f:
-            json_data = json.load(f)
+        self.assertIsNotNone(self.parser.receipt)
+        self.assertEqual(self.parser.receipt.number_receipt, NUMBER_RECEIPT)
+        self.assertEqual(self.parser.receipt.total_sum, TOTAL_SUM)
+        self.assertEqual(len(self.parser.product_list), PRODUCT_LIST_LEN)
 
-        parser = ReceiptParser(json_data)
+    def test_product_name(self):
+        self.assertEqual(
+            self.parser.product_list[0].product_name, 'Product One',
+        )
+        self.assertEqual(
+            self.parser.product_list[1].product_name, 'Product Two',
+        )
 
-        parser.parse(environ.get('ID_GROUP_USER'))
-        self.assertIsNotNone(parser.receipt)
-        self.assertEqual(parser.receipt.number_receipt, 6887)
-        self.assertEqual(parser.receipt.total_sum, 217.60)
-        self.assertEqual(len(parser.product_list), 2)
-        self.assertEqual(parser.product_list[0].product_name, 'Product One')
-        self.assertEqual(parser.product_list[0].price, 131.32)
-        self.assertEqual(parser.product_list[0].quantity, 1)
-        self.assertEqual(parser.product_list[0].amount, 131.32)
-        self.assertEqual(parser.product_list[1].product_name, 'Product Two')
-        self.assertEqual(parser.product_list[1].price, 86.28)
-        self.assertEqual(parser.product_list[1].quantity, 1)
-        self.assertEqual(parser.product_list[1].amount, 86.28)
+    def test_price(self):
+        self.assertEqual(self.parser.product_list[0].price, PRICE_PRODUCT1)
+        self.assertEqual(self.parser.product_list[1].price, PRICE_PRODUCT2)
+
+    def test_quantity(self):
+        self.assertEqual(
+            self.parser.product_list[0].quantity, QUANTITY_PRODUCT1,
+        )
+        self.assertEqual(
+            self.parser.product_list[1].quantity, QUANTITY_PRODUCT2,
+        )
+
+    def test_amount(self):
+        self.assertEqual(self.parser.product_list[0].amount, AMOUNT_PRODUCT1)
+        self.assertEqual(self.parser.product_list[1].amount, AMOUNT_PRODUCT2)
