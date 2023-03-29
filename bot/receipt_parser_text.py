a559327fcc14a1706c3cd7fedc2a9981d2f0c484
Alexander <dev@pavlovteam.ru>
2023-03-29 21:46:15 +0300

    Поправил ошибки линтера

--- bot/receipt_parser_text.py
+++ bot/receipt_parser_text.py
@@ -1,7 +1,6 @@
 import datetime
+import json
 import re
-
-import requests
 
 from bot.config_bot import bot_admin
 from bot.json_parse import ReceiptParser
@@ -11,34 +10,34 @@ from bot.services import ReceiptApiReceiver
 
 @bot_admin.message_handler(content_types=['text'])
 def handle_receipt_text(message):
-    input_user = message.text
-    pattern = (
-        r't=[0-9]+T[0-9]+'
-        r'&s=[0-9]+.[0-9]+&fn=[0-9]+'
-        r'&i=[0-9]+&fp=[0-9]+&n=[0-5]{1}'
-    )
+    try:
+        input_user = message.text
+        pattern = (
+            r't=[0-9]+T[0-9]+'
+            r'&s=[0-9]+.[0-9]+&fn=[0-9]+'
+            r'&i=[0-9]+&fp=[0-9]+&n=[0-5]{1}'
+        )
 
-    text_pattern = re.match(pattern, input_user)
-    if text_pattern:
-        try:
+        text_pattern = re.match(pattern, input_user)
+        if text_pattern:
             client = ReceiptApiReceiver()
             qr_code = input_user
             json_data = client.get_receipt(qr_code)
 
             parse = ReceiptParser(json_data)
             parse.parse(message.chat.id)
+        else:
+            bot_admin.send_message(message.chat.id, 'Недопустимый текст')
 
-        except requests.exceptions.JSONDecodeError as json_error:
-            logger.error(
-                f'Ошибка обработки json: {json_error}\n'
-                f'Время возникновения исключения: '
-                f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S}'
-            )
+    except json.decoder.JSONDecodeError as json_error:
+        logger.error(
+            f'Ошибка обработки json: {json_error}\n'
+            f'Время возникновения исключения: '
+            f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S}',
+        )
 
-        except Exception as error:
-            logger.error(
-                f'{error}Время возникновения исключения: '
-                f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S}',
-            )
-    else:
-        bot_admin.send_message(message.chat.id, 'Недопустимый текст')
+    except Exception as error:
+        logger.error(
+            f'{error}Время возникновения исключения: '
+            f'{datetime.datetime.now():%Y-%m-%d %H:%M:%S}',
+        )
