5871f8e06655e89e36103385aa21219e06cc8b97
Alexander <dev@pavlovteam.ru>
2023-04-04 22:53:15 +0300

    change

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -6,16 +6,22 @@ from django.http import HttpResponse
 from .config_bot import bot_admin
 from .config_bot import bot_type
 from .log_config import logger
-
+from .receipt_parser_json import handle_receipt_json
+from .receipt_parser_text import handle_receipt_text
 
 
 @csrf_exempt
 def webhooks(request):
     if request.method == 'POST':
-        json_request = request.body.decode('utf8')
-        json_data = json.loads(json_request)
+        try:
+            json_request = request.body.decode('utf8')
+            json_data = json.loads(json_request)
 
-        update = bot_type.Update.de_json(json_data)
-        bot_admin.process_new_updates([update])
-        logger.error(update.message.text)
+            update = bot_type.Update.de_json(json_data)
+            bot_admin.process_new_updates([update])
+            if update.message and update.message.text:
+                handle_receipt_text(update.message.text)
+                handle_receipt_json(update.message.text)
+        except Exception as error:
+            logger.error(error)
         return HttpResponse('')
