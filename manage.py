d98f1d7fea98d29d5afc24a5066d28e3b38977c0
Alexander <dev@pavlovteam.ru>
2023-04-04 22:19:53 +0300

    change

--- manage.py
+++ manage.py
@@ -25,5 +25,4 @@ def main():
 if __name__ == '__main__':
     bot_admin.remove_webhook()
     bot_admin.set_webhook(url=settings.WEBHOOK_URL)
-    print(bot_admin.set_webhook(url=settings.WEBHOOK_URL))
     main()
