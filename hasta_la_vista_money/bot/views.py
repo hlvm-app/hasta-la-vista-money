6ed7cfa7ae43e8d85a1e940fe97ef09deafa572f
Alexander <dev@pavlovteam.ru>
2023-04-04 22:45:25 +0300

    change

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -8,12 +8,14 @@ from .config_bot import bot_type
 from .log_config import logger
 
 
+
 @csrf_exempt
 def webhooks(request):
     if request.method == 'POST':
         json_request = request.body.decode('utf8')
         json_data = json.loads(json_request)
-        logger.error(json_data)
+
         update = bot_type.Update.de_json(json_data)
         bot_admin.process_new_updates([update])
+        logger.error(update)
         return HttpResponse('')
