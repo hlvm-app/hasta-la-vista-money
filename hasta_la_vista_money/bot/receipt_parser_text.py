fb7ae17e0b24b6db04f1bc5a5332982e94c7e59d
Alexander <dev@pavlovteam.ru>
2023-04-04 23:10:37 +0300

    change

--- hasta_la_vista_money/bot/receipt_parser_text.py
+++ hasta_la_vista_money/bot/receipt_parser_text.py
@@ -2,13 +2,14 @@ import re
 
 from hasta_la_vista_money.bot.config_bot import bot_admin
 from hasta_la_vista_money.bot.json_parse import ReceiptParser
+from hasta_la_vista_money.bot.log_config import logger
 from hasta_la_vista_money.bot.services import ReceiptApiReceiver
 
 
 @bot_admin.message_handler(content_types=['text'])
 def handle_receipt_text(message):
     input_user = message.text
-    print(input_user)
+    logger.error(input_user)
     pattern = (
         r't=[0-9]+T[0-9]+'
         r'&s=[0-9]+.[0-9]+&fn=[0-9]+'
