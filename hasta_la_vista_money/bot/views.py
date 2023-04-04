24e7230bc31c8ca8a165214dfe0ed38d54ce4c72
Alexander <dev@pavlovteam.ru>
2023-04-04 21:07:17 +0300

    added logger

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -2,11 +2,13 @@ from django.http import HttpResponse
 
 from .config_bot import bot_admin
 from .config_bot import bot_type
+from .log_config import logger
 
 
 def webhooks(request):
     if request.method == 'POST':
         json_data = request.body.decode('utf-8')
+        logger.error(json_data)
         update = bot_type.Update.de_json(json_data)
         bot_admin.process_new_updates([update])
         return HttpResponse('')
