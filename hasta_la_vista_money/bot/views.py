7f5aecbebefe714d60d5c46428b60942b64be91b
Alexander <dev@pavlovteam.ru>
2023-04-09 19:15:44 +0300

    check

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, HttpResponseNotAllowed
+from django.middleware import csrf
 from hasta_la_vista_money.bot.config_bot import bot_admin, bot_type
 from hasta_la_vista_money.bot.receipt_parser_json import handle_receipt_json
 from hasta_la_vista_money.bot.receipt_parser_text import handle_receipt_text
@@ -7,10 +7,20 @@ from hasta_la_vista_money.bot.receipt_parser_text import handle_receipt_text
 
 def webhooks(request):
     if request.method == 'POST':
+        # Get the CSRF token from the request cookies
+        csrf_token = request.COOKIES.get('csrftoken')
+        # Create the headers with the CSRF token
+        headers = {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrf_token
+        }
+        # Get the JSON data from the request body
         json_data = request.body.decode('utf8')
         try:
             update = bot_type.Update.de_json(json_data)
             bot_admin.process_new_updates([update])
             return HttpResponse('Webhook processed successfully')
         except Exception as error:
-            return HttpResponse(error)
+            return HttpResponse(error, status=500)
+    else:
+        return HttpResponseNotAllowed(['POST'])
