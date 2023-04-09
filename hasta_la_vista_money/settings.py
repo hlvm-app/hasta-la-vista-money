9dc5f31bab8b70d5da44831c06a1e0af0da9aa05
Alexander <dev@pavlovteam.ru>
2023-04-09 13:39:51 +0300

    added csp.middleware.CSPMiddleware

--- hasta_la_vista_money/settings.py
+++ hasta_la_vista_money/settings.py
@@ -75,6 +75,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'csp.middleware.CSPMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
