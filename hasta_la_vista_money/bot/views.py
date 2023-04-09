025ea18f0647beb37741d96e4c4719f96b90972c
Alexander <dev@pavlovteam.ru>
2023-04-09 18:14:08 +0300

    check

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from hasta_la_vista_money.bot.config_bot import bot_admin, bot_type
+from hasta_la_vista_money.bot.receipt_parser_json import handle_receipt_json
+from hasta_la_vista_money.bot.receipt_parser_text import handle_receipt_text
 
 
 @csrf_exempt
@@ -11,5 +13,7 @@ def webhooks(request):
         print(json_data)
         update = bot_type.Update.de_json(json_data)
         print(update)
+        handle_receipt_json(update.message)
+        handle_receipt_text(update.message)
         bot_admin.process_new_updates([update])
         return HttpResponse('')
