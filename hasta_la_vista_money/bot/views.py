08a1c251285c9c38c8321dc68e68bc9eed07a4c4
Alexander <dev@pavlovteam.ru>
2023-04-09 14:51:52 +0300

    changed csp settings

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -7,8 +7,6 @@ from hasta_la_vista_money.bot.config_bot import bot_admin, bot_type
 def webhooks(request):
     if request.method == 'POST':
         json_data = request.body.decode('utf8')
-        print(json_data)
         update = bot_type.Update.de_json(json_data)
-        print(update)
         bot_admin.process_new_updates([update])
         return HttpResponse('')
