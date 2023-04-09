0ee330d16c9bf892125f417a7111051bcc863d40
Alexander <dev@pavlovteam.ru>
2023-04-09 23:31:20 +0300

    Added docstrings

--- hasta_la_vista_money/bot/apps.py
+++ hasta_la_vista_money/bot/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class BotConfig(AppConfig):
+    """
+    Класс для конфигурации приложения Bot в Django.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hasta_la_vista_money.bot'
