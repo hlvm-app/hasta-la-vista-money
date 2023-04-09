454675d68b4d9afd79798b736ea3cbade8e5c823
Alexander <dev@pavlovteam.ru>
2023-04-09 18:26:55 +0300

    added condition

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -8,11 +8,11 @@ from hasta_la_vista_money.bot.receipt_parser_text import handle_receipt_text
 @csrf_exempt
 def webhooks(request):
     if request.method == 'POST':
-        print(request.headers['Content-Type'])
         json_data = request.body.decode('utf8')
-        print(json_data)
         update = bot_type.Update.de_json(json_data)
         print(update)
+        if update.message.document.mime_type == 'application/json':
+            handle_receipt_json(update.message)
         handle_receipt_text(update.message)
         bot_admin.process_new_updates([update])
         return HttpResponse('')
