aec4170e8c67370c67b28e8fc321e42522ab9bec
Alexander <dev@pavlovteam.ru>
2023-04-10 22:33:24 +0300

    Поправил текст

--- hasta_la_vista_money/bot/management/commands/startbot.py
+++ hasta_la_vista_money/bot/management/commands/startbot.py
@@ -5,19 +5,12 @@ from hasta_la_vista_money.bot.receipt_parser_text import bot_admin as bot_text
 
 class Command(BaseCommand):
     """
-        Django команда для запуска ботов.
-        Разбирает чеки и взаимодействуют с пользователем через Telegram.
-
-
-
-        Методы:
-            - handle(self, *args, **options): Метод, который запускает ботов, которые разбирают чеки и взаимодействуют
-            с пользователем через Telegram. Использует bot_admin из hasta_la_vista_money.bot.receipt_parser_json для парсинга
-            JSON-файлов и bot_admin из hasta_la_vista_money.bot.receipt_parser_text для парсинга текстовых сообщений.
-        """
+    Django команда для запуска бота через polling.
+    Разбирает чеки и взаимодействуют с пользователем через Telegram.
+    """
     def handle(self, *args, **options):
         """
-        Метод, который запускает ботов.
+        Метод, который запускает бота через polling.
 
         Использует bot_admin из hasta_la_vista_money.bot.receipt_parser_json
         для парсинга JSON-файлов и bot_admin из
