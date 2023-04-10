34956013e4bf8191c7e32f5fed3900a78c6e205b
Alexander <dev@pavlovteam.ru>
2023-04-10 23:44:50 +0300

    test webhook

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from hasta_la_vista_money.bot.config_bot import bot_admin, bot_type
 from hasta_la_vista_money.bot.log_config import logger
+from hasta_la_vista_money.bot.receipt_parser_json import handle_receipt_json
+from hasta_la_vista_money.bot.receipt_parser_text import handle_receipt_text
 
 
 @csrf_exempt
@@ -10,7 +12,12 @@ def webhooks(request):
         json_data = request.body.decode('utf8')
         try:
             update = bot_type.Update.de_json(json_data)
-            bot_admin.process_new_updates([update])
+            if update.message.content_type == 'document':
+                handle_receipt_json(update.message)
+                bot_admin.process_new_updates([update])
+            else:
+                handle_receipt_text(update.message)
+                bot_admin.process_new_updates([update])
             return HttpResponse('Webhook processed successfully')
         except Exception as error:
             logger.error(error)
