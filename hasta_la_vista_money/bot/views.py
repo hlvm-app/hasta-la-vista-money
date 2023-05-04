b9c072c311d69c0b57204b6c5eb3aad69e5dae7d
Alexander <dev@pavlovteam.ru>
2023-05-04 21:28:26 +0300

    test

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -6,6 +6,7 @@ from hasta_la_vista_money.bot.log_config import logger
 
 @csrf_exempt
 def webhooks(request):
+    logger.error(request)
     if request.method == 'POST':
         json_data = request.body.decode('utf8')
         try:
