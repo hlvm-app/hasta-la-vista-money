acf136aa72bf7699ab7d8cfc9638e20d17c22c34
Alexander <dev@pavlovteam.ru>
2023-05-04 21:14:15 +0300

    Поправил загрузку env

--- hasta_la_vista_money/bot/config_bot.py
+++ hasta_la_vista_money/bot/config_bot.py
@@ -3,7 +3,9 @@
 import os
 
 import telebot
+from dotenv import load_dotenv
 
-token = os.environ.get('TOKEN_TELEGRAM_BOT')
+load_dotenv()
+token = os.getenv('TOKEN_TELEGRAM_BOT')
 bot_admin = telebot.TeleBot(token, parse_mode='html')
 bot_type = telebot.types
