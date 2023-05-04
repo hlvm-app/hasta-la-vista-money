0c12d687c694cb5148c21b3d87666848667fbf91
Alexander <dev@pavlovteam.ru>
2023-05-04 22:30:23 +0300

    test

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -8,12 +8,11 @@ from hasta_la_vista_money.bot.log_config import logger
 @csrf_exempt
 def webhooks(request):
     print(bot_type.Update.de_json(request.body.decode('utf8')))
-    print(bot_admin.process_new_updates([bot_type.Update.de_json(request.body.decode('utf8'))]))
     if request.method == 'POST':
         json_data = request.body.decode('utf8')
         try:
             update = bot_type.Update.de_json(json_data)
-            bot_admin.process_new_updates([update])
+            print(bot_admin.process_new_updates([update]))
             return HttpResponse('Webhook processed successfully')
         except Exception as error:
             logger.error(error)
