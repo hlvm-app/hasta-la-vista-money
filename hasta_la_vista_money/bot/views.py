c2400511c1a44077322871ee90387184e913f015
Alexander <dev@pavlovteam.ru>
2023-05-04 23:04:15 +0300

    fix

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -8,10 +8,8 @@ from hasta_la_vista_money.bot.log_config import logger
 def webhooks(request):
     if request.method == 'POST':
         json_data = request.body.decode('utf8')
-        print(json_data)
         try:
             updates = bot_type.Update.de_json(json_data)
-            print(updates, 'updated')
             bot_admin.process_new_updates([updates])
             return HttpResponse('Webhook processed successfully')
         except Exception as error:
