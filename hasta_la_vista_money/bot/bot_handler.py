1a47c9361d347d648dd36917528c7fd2f0dc1b35
Alexander <dev@pavlovteam.ru>
2023-05-05 05:56:28 +0300

    fix

--- hasta_la_vista_money/bot/bot_handler.py
+++ hasta_la_vista_money/bot/bot_handler.py
@@ -5,7 +5,9 @@ from hasta_la_vista_money.bot.receipt_parser_text_qrcode import \
     handle_receipt_text_qrcode
 
 
-@bot_admin.message_handler(content_types=['text', 'document', 'photo'])
+@bot_admin.message_handler(
+    func=lambda message: True, content_types=['text', 'document', 'photo']
+)
 def handler(message):
     if message.content_type == 'text':
         handle_receipt_text(message, bot_admin)
--- hasta_la_vista_money/bot/config_bot.py
+++ hasta_la_vista_money/bot/config_bot.py
@@ -7,5 +7,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('TOKEN_TELEGRAM_BOT')
-bot_admin = telebot.TeleBot(token, parse_mode='html', threaded=False)
+bot_admin = telebot.TeleBot(token, parse_mode='html')
 bot_type = telebot.types
