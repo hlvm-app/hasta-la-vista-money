8e894a686739b326099e2ed21df8890befe9df6d
Alexander <dev@pavlovteam.ru>
2023-05-08 11:35:26 +0300

    changed config bot

--- hasta_la_vista_money/bot/config_bot.py
+++ hasta_la_vista_money/bot/config_bot.py
@@ -6,5 +6,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('TOKEN_TELEGRAM_BOT')
-bot_admin = TeleBot(token, parse_mode='html', threaded=False)
+bot_admin = TeleBot(token, parse_mode='html')
 bot_type = types
