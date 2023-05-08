42e6fbd7158eba185b66f0ee7008fa9eb42834ef
Alexander <dev@pavlovteam.ru>
2023-05-08 18:47:37 +0300

    changed

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from hasta_la_vista_money.bot.config_bot import bot_admin
@@ -10,8 +12,10 @@ STATUS_BAD = 500
 @csrf_exempt
 def webhooks(request):
     if request.method == 'POST':
-        updates = types.Update.de_json(request.body.decode('utf8'))
-        bot_admin.process_new_updates([updates])
+        update = types.Update.de_json(
+            json.loads(request.body.decode('utf8'))
+        )
+        bot_admin.process_new_updates([update])
         return HttpResponse(
             'Webhook processed successfully', status=STATUS_SUCCESS,
         )
