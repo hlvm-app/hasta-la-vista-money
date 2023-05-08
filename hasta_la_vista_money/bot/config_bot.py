468e3f8401424ee1073ac266f802bbea7cf86b46
Alexander <dev@pavlovteam.ru>
2023-05-08 13:05:42 +0300

    added logging to telebot

--- hasta_la_vista_money/bot/config_bot.py
+++ hasta_la_vista_money/bot/config_bot.py
@@ -1,10 +1,14 @@
 """Модуль конфигурации бота."""
-
+import logging
 import os
+
+import telebot
 from telebot import TeleBot, types
 from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('TOKEN_TELEGRAM_BOT')
 bot_admin = TeleBot(token, parse_mode='html')
+
+telebot.logger.setLevel(logging.DEBUG)
 bot_type = types
