7fb5067550d2c37448b927fcfa240b54328f39bb
Alexander <dev@pavlovteam.ru>
2023-05-08 15:25:49 +0300

    test

--- hasta_la_vista_money/bot/bot_handler.py
+++ hasta_la_vista_money/bot/bot_handler.py
@@ -5,16 +5,17 @@ from hasta_la_vista_money.bot.receipt_parser_text_qrcode import \
     handle_receipt_text_qrcode
 
 
-@bot_admin.message_handler(content_types=['text', 'document', 'photo'])
+@bot_admin.message_handler()
 def handler(message):
-    if message.content_type == 'text':
-        handle_receipt_text(message, bot_admin)
+    print(message)
+    # if message.content_type == 'text':
+    #     handle_receipt_text(message, bot_admin)
     # elif message.content_type == 'photo':
     #     handle_receipt_text_qrcode(message, bot_admin)
-    elif message.content_type == 'document':
-        handle_receipt_json(message, bot_admin)
-    else:
-        bot_admin.send_message(
-            message.chat.id,
-            'Принимаются файлы JSON, текст по формату и фотографии QR-кодов'
-        )
+    # elif message.content_type == 'document':
+    #     handle_receipt_json(message, bot_admin)
+    # else:
+    #     bot_admin.send_message(
+    #         message.chat.id,
+    #         'Принимаются файлы JSON, текст по формату и фотографии QR-кодов'
+    #     )
