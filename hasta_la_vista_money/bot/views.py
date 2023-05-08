ead61c3845ce1e0efba156b644f5eaf8e2ef99d5
Alexander <dev@pavlovteam.ru>
2023-05-08 19:48:27 +0300

    override process_new_updates from TeleBot class

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -1,4 +1,5 @@
 import json
+import os
 from typing import List
 
 from django.http import HttpResponse
@@ -9,24 +10,7 @@ from telebot import types, TeleBot
 
 STATUS_SUCCESS = 200
 STATUS_BAD = 500
-
-
-@csrf_exempt
-def webhooks(request):
-    try:
-        if request.method == 'POST':
-            update = types.Update.de_json(
-                json.loads(request.body.decode('utf8'))
-            )
-            bot_admin.process_new_updates([update])
-            return HttpResponse(
-                'Webhook processed successfully', status=STATUS_SUCCESS,
-            )
-        return HttpResponse(
-            'Webhook URL for Telegram bot', status=STATUS_SUCCESS,
-            )
-    except Exception as error:
-        logger.error(error)
+token_bot = os.environ.get('TOKEN_TELEGRAM_BOT')
 
 
 class MyBot(TeleBot):
@@ -38,3 +22,27 @@ class MyBot(TeleBot):
             super(MyBot, self).process_new_updates(updates)
         except Exception as error:
             logger.error(error)
+
+
+my_bot = MyBot(token_bot)
+
+
+@csrf_exempt
+def webhooks(request):
+    try:
+        if request.method == 'POST':
+            update = types.Update.de_json(
+                json.loads(request.body.decode('utf8'))
+            )
+            my_bot.process_new_updates([update])
+            return HttpResponse(
+                'Webhook processed successfully', status=STATUS_SUCCESS,
+            )
+        return HttpResponse(
+            'Webhook URL for Telegram bot', status=STATUS_SUCCESS,
+            )
+    except Exception as error:
+        logger.error(error)
+
+
+
