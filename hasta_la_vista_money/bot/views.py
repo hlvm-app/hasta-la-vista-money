a18c4766b28b4be7317ba681130bdc0efb350b01
Alexander <dev@pavlovteam.ru>
2023-05-08 19:25:43 +0300

    override process_new_updates from TeleBot class

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -1,11 +1,11 @@
 import json
-import os
+from typing import List
 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from hasta_la_vista_money.bot.config_bot import bot_admin
 from hasta_la_vista_money.bot.log_config import logger
-from telebot import types
+from telebot import types, TeleBot
 
 STATUS_SUCCESS = 200
 STATUS_BAD = 500
@@ -29,4 +29,11 @@ def webhooks(request):
         logger.error(error)
 
 
-
+class MyBot(TeleBot):
+    def process_new_updates(self, updates: List[types.Update]):
+        try:
+            if not updates:
+                logger.error('Not Updates')
+            logger.error(updates)
+        except Exception as error:
+            logger.error(error)
