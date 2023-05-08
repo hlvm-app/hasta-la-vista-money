e184f91599f7fb7422c2ed5abfc54450221b1f67
Alexander <dev@pavlovteam.ru>
2023-05-08 15:11:20 +0300

    test

--- hasta_la_vista_money/bot/bot_handler.py
+++ hasta_la_vista_money/bot/bot_handler.py
@@ -5,14 +5,12 @@ from hasta_la_vista_money.bot.receipt_parser_text_qrcode import \
     handle_receipt_text_qrcode
 
 
-@bot_admin.message_handler(
-    func=lambda message: True, content_types=['text', 'document', 'photo']
-)
+@bot_admin.message_handler(content_types=['text', 'document', 'photo'])
 def handler(message):
     if message.content_type == 'text':
         handle_receipt_text(message, bot_admin)
-    elif message.content_type == 'photo':
-        handle_receipt_text_qrcode(message, bot_admin)
+    # elif message.content_type == 'photo':
+    #     handle_receipt_text_qrcode(message, bot_admin)
     elif message.content_type == 'document':
         handle_receipt_json(message, bot_admin)
     else:
