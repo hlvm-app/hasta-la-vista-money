17a88a96b25bffb45897e1b9624d3cbebb72ff25
Alexander <dev@pavlovteam.ru>
2023-05-08 18:50:00 +0300

    add try except

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -3,7 +3,9 @@ import json
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from hasta_la_vista_money.bot.config_bot import bot_admin
+from hasta_la_vista_money.bot.log_config import logger
 from telebot import types
+
 
 STATUS_SUCCESS = 200
 STATUS_BAD = 500
@@ -11,15 +13,18 @@ STATUS_BAD = 500
 
 @csrf_exempt
 def webhooks(request):
-    if request.method == 'POST':
-        update = types.Update.de_json(
-            json.loads(request.body.decode('utf8'))
-        )
-        bot_admin.process_new_updates([update])
-        return HttpResponse(
-            'Webhook processed successfully', status=STATUS_SUCCESS,
-        )
-    else:
-        return HttpResponse(
-            'Webhook URL for Telegram bot', status=STATUS_SUCCESS,
-        )
+    try:
+        if request.method == 'POST':
+            update = types.Update.de_json(
+                json.loads(request.body.decode('utf8'))
+            )
+            bot_admin.process_new_updates([update])
+            return HttpResponse(
+                'Webhook processed successfully', status=STATUS_SUCCESS,
+            )
+        else:
+            return HttpResponse(
+                'Webhook URL for Telegram bot', status=STATUS_SUCCESS,
+            )
+    except Exception as error:
+        logger.error(error)
