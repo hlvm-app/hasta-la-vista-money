9225c476fb71308fef23353c11d7df6358daeab6
Alexander <dev@pavlovteam.ru>
2023-05-08 13:59:06 +0300

    changed

--- hasta_la_vista_money/bot/config_bot.py
+++ hasta_la_vista_money/bot/config_bot.py
@@ -1,14 +1,10 @@
 """Модуль конфигурации бота."""
-import logging
 import os
 
-import telebot
 from telebot import TeleBot, types
 from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('TOKEN_TELEGRAM_BOT')
 bot_admin = TeleBot(token, parse_mode='html')
-
-telebot.logger.setLevel(logging.INFO)
 bot_type = types
