f332e6aae9c6092bcbc0884b7261ba00f85c0c4b
Alexander <dev@pavlovteam.ru>
2023-05-08 20:30:37 +0300

    override process_new_updates from TeleBot class

--- hasta_la_vista_money/bot/bot_handler.py
+++ hasta_la_vista_money/bot/bot_handler.py
@@ -1,9 +1,10 @@
 from hasta_la_vista_money.bot.config_bot import bot_admin
 from hasta_la_vista_money.bot.receipt_parser_json import handle_receipt_json
 from hasta_la_vista_money.bot.receipt_parser_text import handle_receipt_text
+from hasta_la_vista_money.bot.views import my_bot
 
 
-@bot_admin.message_handler(content_types=['text', 'document'])
+@my_bot.message_handler(content_types=['text', 'document'])
 def handle_receipt(message):
     if message.content_type == 'text':
         handle_receipt_text(message, bot_admin)
