698193a5757e09829f8dbd4fac6f283cb3608d4c
Alexander <dev@pavlovteam.ru>
2023-05-08 22:01:07 +0300

    refactoring

--- hasta_la_vista_money/bot/webhooks.py
+++ hasta_la_vista_money/bot/webhooks.py
@@ -2,11 +2,14 @@ import json
 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from hasta_la_vista_money.bot.bot_handler import handle_receipt
 from hasta_la_vista_money.bot.config_bot import bot_admin
 from hasta_la_vista_money.bot.log_config import logger
 from telebot import types
 
 STATUS_SUCCESS = 200
+
+bot_admin.add_message_handler(handle_receipt)
 
 
 @csrf_exempt
