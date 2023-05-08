0ebb1dd935b687b57706717fc818103e7ee524a9
Alexander <dev@pavlovteam.ru>
2023-05-08 13:35:21 +0300

    changed

--- hasta_la_vista_money/bot/views.py
+++ hasta_la_vista_money/bot/views.py
@@ -6,13 +6,14 @@ from hasta_la_vista_money.bot.log_config import logger
 
 @csrf_exempt
 def webhooks(request):
-    try:
-        if request.method == 'POST':
+    if request.method == 'POST':
+        try:
             json_data = request.body.decode('utf8')
-            print(json_data)
             updates = bot_type.Update.de_json(json_data)
-            print(updates)
             bot_admin.process_new_updates([updates])
             return HttpResponse('Webhook processed successfully')
-    except Exception as error:
-        logger.error(error)
+        except Exception as error:
+            logger.error(error)
+            return HttpResponse(status=500)
+    else:
+        return HttpResponse('Webhook URL for Telegram bot', status=200)
