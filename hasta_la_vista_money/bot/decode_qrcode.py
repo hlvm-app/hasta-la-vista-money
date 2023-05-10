5f1137edd55aa5371e1359a5c82c2b209ef5994c
Alexander <dev@pavlovteam.ru>
2023-05-10 22:47:23 +0300

    changed name variable + added exceptions

--- hasta_la_vista_money/bot/decode_qrcode.py
+++ hasta_la_vista_money/bot/decode_qrcode.py
@@ -1,8 +1,7 @@
 """Модуль декодирования изображения QR-кода в текст."""
+from hasta_la_vista_money.bot.log_config import logger
 from PIL import Image
 from pyzbar.pyzbar import decode
-
-from hasta_la_vista_money.bot.log_config import logger
 
 
 def decode_qrcode(image):
@@ -15,9 +14,18 @@ def decode_qrcode(image):
     :rtype: str
     """
     try:
-        result = decode(Image.open(image))
-        return result[0].data.decode()
+        decode_image = decode(Image.open(image))
+        return decode_image[0].data.decode()
 
         # Выводим текст из QR-кода
+    except IndexError:
+        logger.error(
+            'QR-код не считался, '
+            'попробуй ещё раз или '
+            'воспользуйся сторонним приложением '
+            'и передай текст из QR-кода боту'  # noqa: C812
+        )
+    except FileNotFoundError:
+        logger.error('Файл не загрузился, попробуйте ещё раз!')
     except Exception as error:
         logger.error(error)
