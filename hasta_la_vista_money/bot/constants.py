06c5e7727038b0017ff14908bccebbdef7b1437f
Alexander <dev@pavlovteam.ru>
2023-05-10 05:59:05 +0300

    Добавил переменные класса - константы для парсинга чека

--- hasta_la_vista_money/bot/constants.py
+++ hasta_la_vista_money/bot/constants.py
@@ -3,4 +3,17 @@ from constantly import ValueConstant, Values
 
 class ReceiptConstant(Values):
     NAME_SELLER = ValueConstant('user')
+    RETAIL_PLACE_ADDRESS = ValueConstant('retailPlaceAddress')
+    RETAIL_PLACE = ValueConstant('retailPlace')
+    RECEIPT_DATE = ValueConstant('dateTime')
+    NUMBER_RECEIPT = ValueConstant('fiscalDocumentNumber')
+    OPERATION_TYPE = ValueConstant('operationType')
+    TOTAL_SUM = ValueConstant('totalSum')
+    PRODUCT_NAME = ValueConstant('name')
+    PRICE = ValueConstant('price')
+    QUANTITY = ValueConstant('quantity')
+    AMOUNT = ValueConstant('sum')
+    NDS_TYPE = ValueConstant('nds')
+    NDS_SUM = ValueConstant('ndsSum')
+    ITEMS = ValueConstant('items')
 
