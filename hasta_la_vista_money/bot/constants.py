b5b74d4a8d801ab5bc700801f5bfac0a51f6356d
Alexander <dev@pavlovteam.ru>
2023-05-11 21:45:24 +0300

    отказался от constantly в пользу классики

--- hasta_la_vista_money/bot/constants.py
+++ hasta_la_vista_money/bot/constants.py
@@ -1,18 +1,15 @@
-from constantly import ValueConstant, Values
-
-
-class ReceiptConstant(Values):
-    NAME_SELLER = ValueConstant('user')
-    RETAIL_PLACE_ADDRESS = ValueConstant('retailPlaceAddress')
-    RETAIL_PLACE = ValueConstant('retailPlace')
-    RECEIPT_DATE = ValueConstant('dateTime')
-    NUMBER_RECEIPT = ValueConstant('fiscalDocumentNumber')
-    OPERATION_TYPE = ValueConstant('operationType')
-    TOTAL_SUM = ValueConstant('totalSum')
-    PRODUCT_NAME = ValueConstant('name')
-    PRICE = ValueConstant('price')
-    QUANTITY = ValueConstant('quantity')
-    AMOUNT = ValueConstant('sum')
-    NDS_TYPE = ValueConstant('nds')
-    NDS_SUM = ValueConstant('ndsSum')
-    ITEMS_PRODUCT = ValueConstant('items')
+# Константы полей чека при парсинге
+NAME_SELLER = 'user'
+RETAIL_PLACE_ADDRESS = 'retailPlaceAddress'
+RETAIL_PLACE = 'retailPlace'
+RECEIPT_DATE = 'dateTime'
+NUMBER_RECEIPT = 'fiscalDocumentNumber'
+OPERATION_TYPE = 'operationType'
+TOTAL_SUM = 'totalSum'
+PRODUCT_NAME = 'name'
+PRICE = 'price'
+QUANTITY = 'quantity'
+AMOUNT = 'sum'
+NDS_TYPE = 'nds'
+NDS_SUM = 'ndsSum'
+ITEMS_PRODUCT = 'items'
