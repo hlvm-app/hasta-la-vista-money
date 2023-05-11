66bcc5dd98eac9f8760428b139399aa5c28f7737
Alexander <dev@pavlovteam.ru>
2023-05-11 21:38:07 +0300

    fix lint

--- config/django/base.py
+++ config/django/base.py
@@ -38,9 +38,9 @@ ALLOWED_HOSTS = os.environ.get(
 
 
 CSRF_TRUSTED_ORIGINS = os.environ.get(
-    'CSRF_TRUSTED_ORIGINS'
+    'CSRF_TRUSTED_ORIGINS',
 ).split(',') if os.environ.get(
-    'CSRF_TRUSTED_ORIGINS'
+    'CSRF_TRUSTED_ORIGINS',
 ) else ['https://127.0.0.1']
 
 
