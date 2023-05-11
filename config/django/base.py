f216c49d901e77eda680b12c22f0db56151598b9
Alexander Pavlov <dev@pavlovteam.ru>
2023-05-11 09:04:12 +0300

    changed CSRF_TRUSTED_ORIGINS value

--- config/django/base.py
+++ config/django/base.py
@@ -37,7 +37,11 @@ ALLOWED_HOSTS = os.environ.get(
 ) else ['localhost', '127.0.0.1']
 
 
-CSRF_TRUSTED_ORIGINS = [os.environ.get('CSRF_TRUSTED_ORIGINS')]
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    'ALLOWED_HOSTS',
+).split(',') if os.environ.get(
+    'ALLOWED_HOSTS',
+) else ['localhost', '127.0.0.1']
 
 # Application definition
 
