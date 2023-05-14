2588256ade42cee875acdf2c09e560b14d2dd07e
Alexander <dev@pavlovteam.ru>
2023-05-14 15:19:08 +0300

    Для констант в проекте будет использована Enum библиотека

--- hasta_la_vista_money/constants.py
+++ hasta_la_vista_money/constants.py
@@ -1,15 +1,18 @@
-# Константы полей чека при парсинге
-NAME_SELLER = 'user'
-RETAIL_PLACE_ADDRESS = 'retailPlaceAddress'
-RETAIL_PLACE = 'retailPlace'
-RECEIPT_DATE = 'dateTime'
-NUMBER_RECEIPT = 'fiscalDocumentNumber'
-OPERATION_TYPE = 'operationType'
-TOTAL_SUM = 'totalSum'
-PRODUCT_NAME = 'name'
-PRICE = 'price'
-QUANTITY = 'quantity'
-AMOUNT = 'sum'
-NDS_TYPE = 'nds'
-NDS_SUM = 'ndsSum'
-ITEMS_PRODUCT = 'items'
+from enum import Enum
+
+
+class ReceiptConstant(Enum):
+    NAME_SELLER = 'user'
+    RETAIL_PLACE_ADDRESS = 'retailPlaceAddress'
+    RETAIL_PLACE = 'retailPlace'
+    RECEIPT_DATE = 'dateTime'
+    NUMBER_RECEIPT = 'fiscalDocumentNumber'
+    OPERATION_TYPE = 'operationType'
+    TOTAL_SUM = 'totalSum'
+    PRODUCT_NAME = 'name'
+    PRICE = 'price'
+    QUANTITY = 'quantity'
+    AMOUNT = 'sum'
+    NDS_TYPE = 'nds'
+    NDS_SUM = 'ndsSum'
+    ITEMS_PRODUCT = 'items'
