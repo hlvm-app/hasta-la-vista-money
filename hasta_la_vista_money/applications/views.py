535e01801fe7a859c9a94ad9c93e01590415ffd7
Alexander <dev@pavlovteam.ru>
2023-05-14 19:47:19 +0300

    сообщения перенёс в модуль констант

--- hasta_la_vista_money/applications/views.py
+++ hasta_la_vista_money/applications/views.py
@@ -1,10 +1,7 @@
-from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.utils.translation import gettext_lazy
 from django.views.generic import TemplateView
 
+from hasta_la_vista_money.constants import Messages
 from hasta_la_vista_money.custom_mixin import CustomNoPermissionMixin
 
 
@@ -13,7 +10,5 @@ class PageApplication(CustomNoPermissionMixin, TemplateView):
 
     template_name = 'applications/page_application.html'
     context_object_name = 'applications'
-    permission_denied_message = gettext_lazy(
-        'У вас нет прав на просмотр данной страницы! Авторизуйтесь!',
-    )
+    permission_denied_message = Messages.ACCESS_DENIED.value
     no_permission_url = reverse_lazy('users:login')
