75220f278d073b3c010037e1db04bd130e690a51
Alexander <dev@pavlovteam.ru>
2023-05-14 19:26:26 +0300

    рефакторинг

--- hasta_la_vista_money/applications/views.py
+++ hasta_la_vista_money/applications/views.py
@@ -1,12 +1,14 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy
 from django.views.generic import TemplateView
 
+from hasta_la_vista_money.custom_mixin import CustomNoPermissionMixin
 
-class PageApplication(LoginRequiredMixin, SuccessMessageMixin, TemplateView):
+
+class PageApplication(CustomNoPermissionMixin, TemplateView):
     """Отображает список приложений в проекте на сайте."""
 
     template_name = 'applications/page_application.html'
@@ -14,18 +16,4 @@ class PageApplication(LoginRequiredMixin, SuccessMessageMixin, TemplateView):
     permission_denied_message = gettext_lazy(
         'У вас нет прав на просмотр данной страницы! Авторизуйтесь!',
     )
-    no_permission_url = 'login'
-
-    def handle_no_permission(self):
-        """
-        Метод для управления редиректом на страницу авторизации.
-
-        Когда пользователь пытается зайти на страницу со списком приложений
-        и не был до этого авторизован, его переадресовывает на страницу
-        авторизации.
-
-        :return: Редирект на страницу авторизации.
-
-        """
-        messages.error(self.request, self.error_message)
-        return redirect(self.no_permission_url)
+    no_permission_url = reverse_lazy('users:login')
