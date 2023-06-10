893da1c1cc64b760e629902d0cf1c463bfa7ce02
Alexander <dev@pavlovteam.ru>
2023-06-10 15:10:10 +0300

    поменял имя класса констант на более ясное

--- hasta_la_vista_money/income/views.py
+++ hasta_la_vista_money/income/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, FormView
 from django_filters.views import FilterView
-from hasta_la_vista_money.constants import Messages
+from hasta_la_vista_money.constants import MessageOnSite
 from hasta_la_vista_money.custom_mixin import CustomNoPermissionMixin
 from hasta_la_vista_money.income.forms import IncomeForm
 from hasta_la_vista_money.income.models import Income
@@ -16,12 +16,12 @@ class IncomeView(CustomNoPermissionMixin, SuccessMessageMixin, FilterView):
     model = Income
     template_name = 'income/income.html'
     context_object_name = 'incomes'
-    permission_denied_message = Messages.ACCESS_DENIED.value
+    permission_denied_message = MessageOnSite.ACCESS_DENIED.value
     no_permission_url = reverse_lazy('login')
 
     def get(self, request, *args, **kwargs):
         sort_by_month = Income.objects.filter(
-            user=request.user
+            user=request.user,
         ).order_by('-date')
         return render(
             request, self.template_name, {'income_by_month': sort_by_month},
@@ -46,14 +46,14 @@ class AddIncome(
     model = Income
     form_class = IncomeForm
     template_name = 'income/add_income.html'
-    permission_denied_message = Messages.ACCESS_DENIED.value
+    permission_denied_message = MessageOnSite.ACCESS_DENIED.value
     success_url = reverse_lazy('income:list')
 
     def get(self, request, *args, **kwargs):
         if request.user:
             income_form = IncomeForm()
             income_form.fields['account'].queryset = Account.objects.filter(
-                user=request.user
+                user=request.user,
             )
             return self.render_to_response({'income_form': income_form})
 
