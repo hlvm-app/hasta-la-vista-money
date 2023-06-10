3bc2ff1a63838cd780660c78cc7f049d91c306f4
Alexander <dev@pavlovteam.ru>
2023-06-10 21:44:30 +0300

    Пересоздали бд

--- hasta_la_vista_money/account/migrations/0002_initial.py
+++ hasta_la_vista_money/account/migrations/0002_initial.py
@@ -11,12 +11,12 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('income', '0001_initial'),
+        ('account', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='income',
+            model_name='account',
             name='user',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
