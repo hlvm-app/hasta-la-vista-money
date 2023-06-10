e01a25695efa226114a291f065f9fc8b21d6b5d5
Alexander <dev@pavlovteam.ru>
2023-06-10 15:07:38 +0300

    Добавлена авторизация в боте телеграма посредством логина и пароля с сайта

--- hasta_la_vista_money/bot/bot_handler.py
+++ hasta_la_vista_money/bot/bot_handler.py
@@ -4,18 +4,69 @@ from hasta_la_vista_money.bot.receipt_parser_text import handle_receipt_text
 from hasta_la_vista_money.bot.receipt_parser_text_qrcode import (
     handle_receipt_text_qrcode,
 )
+from hasta_la_vista_money.constants import TelegramMessage
+from hasta_la_vista_money.users.models import TelegramUser, User
+
+
+@bot_admin.message_handler(commands=['auth'])
+def handle_start(message):
+    bot_admin.send_message(
+        message.chat.id, TelegramMessage.REQUIRED_AUTHORIZATION.value,
+    )
+    bot_admin.register_next_step_handler(message, handle_auth)
+
+
+def handle_auth(message):
+    auth_data = message.text.split(':')
+    if len(auth_data) == 2:
+        username = auth_data[0].strip()
+        password = auth_data[1].strip()
+        user = User.objects.filter(username=username).first()
+        if user and user.check_password(password):
+            existing_telegram_user = TelegramUser.objects.filter(
+                user=user,
+            ).first()
+            if existing_telegram_user:
+                if existing_telegram_user.telegram_id == message.from_user.id:
+                    bot_admin.reply_to(
+                        message,
+                        TelegramMessage.ALREADY_LOGGING_LINK_ACCOUNT.value,
+                    )
+                else:
+                    bot_admin.reply_to(
+                        message,
+                        TelegramMessage.ALREADY_LINK_ANOTHER_ACCOUNT.value,
+                    )
+            else:
+                TelegramUser.objects.create(
+                    user=user, telegram_id=message.from_user.id,
+                )
+                bot_admin.reply_to(
+                    message, TelegramMessage.AUTHORIZATION_SUCCESSFUL.value,
+                )
+        else:
+            bot_admin.reply_to(
+                message, TelegramMessage.INVALID_USERNAME_PASSWORD.value,
+            )
+    else:
+        bot_admin.reply_to(message, TelegramMessage.INCORRECT_FORMAT.value)
 
 
 @bot_admin.message_handler(content_types=['text', 'document', 'photo'])
 def handle_receipt(message):
-    if message.content_type == 'text':
-        handle_receipt_text(message, bot_admin)
-    elif message.content_type == 'photo':
-        handle_receipt_text_qrcode(message, bot_admin)
-    elif message.content_type == 'document':
-        handle_receipt_json(message, bot_admin)
-    else:
-        bot_admin.send_message(
-            message.chat.id,
-            'Принимаются файлы JSON, текст по формату и фотографии QR-кодов',
-        )
+    telegram_user = message.from_user.id
+
+    user = TelegramUser.objects.filter(telegram_id=telegram_user).first()
+
+    if user:
+        if message.content_type == 'text':
+            handle_receipt_text(message, bot_admin)
+        elif message.content_type == 'photo':
+            handle_receipt_text_qrcode(message, bot_admin)
+        elif message.content_type == 'document':
+            handle_receipt_json(message, bot_admin)
+        else:
+            bot_admin.send_message(
+                message.chat.id,
+                TelegramMessage.ACCEPTED_FORMAT_JSON.value,
+            )
