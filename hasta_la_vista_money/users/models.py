08fb437caab52ead6e834f0ac75d6a84f818b53f
Alexander <dev@pavlovteam.ru>
2023-06-10 15:09:39 +0300

    убрал магические числа в модуль констант и добавил новую модель Телеграм пользователя

--- hasta_la_vista_money/users/models.py
+++ hasta_la_vista_money/users/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from hasta_la_vista_money.constants import NumericParameter
 
 CURRENCY = (
     ('RU', 'Российский рубль'),
@@ -12,8 +12,26 @@ class User(AbstractUser):
         return self.username
 
 
+class TelegramUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.CharField(
+        max_length=NumericParameter.TWO_HUNDRED_FIFTY.value,
+    )
+    telegram_id = models.IntegerField(unique=False)
+
+
 class Account(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name_account = models.CharField(max_length=250, default='Основной счёт')
-    balance = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    name_account = models.CharField(
+        max_length=NumericParameter.TWO_HUNDRED_FIFTY.value,
+        default='Основной счёт',
+    )
+    balance = models.DecimalField(
+        max_digits=NumericParameter.TWENTY.value,
+        decimal_places=2,
+        default=0,
+    )
     currency = models.CharField(choices=CURRENCY)
+
+    def __str__(self):
+        return self.name_account
