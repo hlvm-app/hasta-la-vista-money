fc63e18648ccc793d95856779d277fd01fc3cfc1
Alexander <dev@pavlovteam.ru>
2023-06-10 15:10:02 +0300

    поменял имя класса констант на более ясное

--- hasta_la_vista_money/applications/views.py
+++ hasta_la_vista_money/applications/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
-from hasta_la_vista_money.constants import Messages
+from hasta_la_vista_money.constants import MessageOnSite
 from hasta_la_vista_money.custom_mixin import CustomNoPermissionMixin
 from hasta_la_vista_money.users.models import Account
 
@@ -11,7 +11,7 @@ class PageApplication(CustomNoPermissionMixin, TemplateView):
 
     template_name = 'applications/page_application.html'
     context_object_name = 'applications'
-    permission_denied_message = Messages.ACCESS_DENIED.value
+    permission_denied_message = MessageOnSite.ACCESS_DENIED.value
     no_permission_url = reverse_lazy('login')
 
     def get(self, request, *args, **kwargs):
