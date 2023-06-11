3af2db6069d8a02b7522982e08f1a9ca982474fc
Alexander <dev@pavlovteam.ru>
2023-06-11 16:09:58 +0300

    make lint

--- hasta_la_vista_money/expense/models.py
+++ hasta_la_vista_money/expense/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from hasta_la_vista_money.constants import NumericParameter
 from hasta_la_vista_money.account.models import Account
+from hasta_la_vista_money.constants import NumericParameter
 from hasta_la_vista_money.users.models import User
 
 CATEGORIES = (
