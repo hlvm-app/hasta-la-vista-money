6dbb9f86c72ddae1390d0636badc88c4bf512da3
Alexander <dev@pavlovteam.ru>
2023-06-13 15:14:04 +0300

    убрал уже ненужный путь

--- hasta_la_vista_money/income/urls.py
+++ hasta_la_vista_money/income/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from hasta_la_vista_money.income.views import AddIncome, IncomeView
+from hasta_la_vista_money.income.views import IncomeView
 
 app_name = 'income'
 urlpatterns = [
-    path('', IncomeView.as_view(), name='list'),
-    path('create/', AddIncome.as_view(), name='create'),
+    path('', IncomeView.as_view(), name='list')
 ]
