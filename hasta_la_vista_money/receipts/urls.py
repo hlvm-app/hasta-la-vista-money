c998c50e8af2dfd2643c25fe3cab6aab61f280d5
Alexander <dev@pavlovteam.ru>
2023-06-13 11:23:30 +0300

    удалить путь до create/, так как появилось модальное окно

--- hasta_la_vista_money/receipts/urls.py
+++ hasta_la_vista_money/receipts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from hasta_la_vista_money.receipts.views import ReceiptCreateView, ReceiptView
+from hasta_la_vista_money.receipts.views import ReceiptView
 
 app_name = 'receipts'
 urlpatterns = [
     path('', ReceiptView.as_view(), name='list'),
-    path('create/', ReceiptCreateView.as_view(), name='create'),
 ]
