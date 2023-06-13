9138e51831f4721b760f2d0556814cdb1d4d5a4d
Alexander <dev@pavlovteam.ru>
2023-06-13 17:58:00 +0300

    Добавил календарь в поле ввода даты

--- hasta_la_vista_money/income/forms.py
+++ hasta_la_vista_money/income/forms.py
@@ -1,6 +1,13 @@
+from django.contrib.humanize.templatetags.humanize import intcomma
+
 from config.django.forms import BaseForm
+from django import forms
 from django.utils.translation import gettext_lazy as _
 from hasta_la_vista_money.income.models import Income
+
+
+class DateTimeInput(forms.DateTimeInput):
+    input_type = 'datetime-local'
 
 
 class IncomeForm(BaseForm):
@@ -16,3 +23,6 @@ class IncomeForm(BaseForm):
     class Meta:
         model = Income
         fields = ['type_income', 'date', 'amount', 'account']
+        widgets = {
+            'date': DateTimeInput(),
+        }
