43c40b6664ea12a8cf64687f0f178b8612764a3c
Alexander <dev@pavlovteam.ru>
2023-06-19 17:39:36 +0300

    создал форму добавление расходов

---
+++ hasta_la_vista_money/expense/forms.py
@@ -0,0 +1,26 @@
+from bootstrap_datepicker_plus.widgets import DateTimePickerInput
+
+from config.django.forms import BaseForm
+from django.utils.translation import gettext_lazy as _
+
+from hasta_la_vista_money.expense.models import Expense
+
+
+class AddExpenseForm(BaseForm):
+    labels = {
+        'account': _('Счёт'),
+        'category': _('Категория'),
+        'date': _('Дата'),
+        'amount': _('Сумма'),
+    }
+
+    class Meta:
+        model = Expense
+        fields = ['account', 'category', 'date', 'amount']
+        widgets = {
+            'date': DateTimePickerInput(
+                options={
+                    "format": "DD/MM/YYYY HH:ss",
+                    "showTodayButton": True,
+                })
+        }
