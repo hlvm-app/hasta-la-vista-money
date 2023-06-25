71f9545f3cc10e8f9b58917d720e5e0f7b1b2aea
Alexander <dev@pavlovteam.ru>
2023-06-25 12:23:11 +0300

    линтер

--- hasta_la_vista_money/expense/views.py
+++ hasta_la_vista_money/expense/views.py
@@ -5,19 +5,17 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from hasta_la_vista_money.account.models import Account
-from hasta_la_vista_money.constants import MessageOnSite
+from hasta_la_vista_money.buttons_delete import button_delete_expenses
 from hasta_la_vista_money.custom_mixin import CustomNoPermissionMixin
 from hasta_la_vista_money.expense.forms import AddExpenseForm
 from hasta_la_vista_money.expense.models import Expense
 from hasta_la_vista_money.receipts.models import Receipt
-from hasta_la_vista_money.utils import button_delete_expenses
 
 
 class ExpenseView(CustomNoPermissionMixin, SuccessMessageMixin, TemplateView):
     model = Expense
     template_name = 'expense/expense.html'
     context_object_name = 'expense'
-    permission_denied_message = MessageOnSite.ACCESS_DENIED.value
     no_permission_url = reverse_lazy('login')
     success_url = 'expense:list'
 
@@ -54,7 +52,7 @@ class ExpenseView(CustomNoPermissionMixin, SuccessMessageMixin, TemplateView):
                 'category__name',
                 'amount',
             ).order_by('-date')
-            print(expenses.values('id'))
+
             return render(
                 request,
                 self.template_name,
@@ -62,10 +60,10 @@ class ExpenseView(CustomNoPermissionMixin, SuccessMessageMixin, TemplateView):
                     'receipt_info_by_month': receipt_info_by_month,
                     'expenses': expenses,
                     'add_expense_form': add_expense_form,
-                }
+                },
             )
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):  # noqa: WPS210
         if 'delete_expense_button' in request.POST:
             expense_id = request.POST.get('expense_id')
             button_delete_expenses(
