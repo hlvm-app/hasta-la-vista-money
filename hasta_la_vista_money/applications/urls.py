2979d641e28d54dbabb112dc999f50503c11b44a
Alexander <dev@pavlovteam.ru>
2023-06-27 17:23:23 +0300

    Переделал метод удаления счёта

--- hasta_la_vista_money/applications/urls.py
+++ hasta_la_vista_money/applications/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from hasta_la_vista_money.applications.views import PageApplication
+from hasta_la_vista_money.applications.views import (
+    DeleteAccountView,
+    PageApplication,
+)
 
 app_name = 'applications'
 urlpatterns = [
     path('', PageApplication.as_view(), name='list'),
+    path(
+        'account/<int:pk>',
+        DeleteAccountView.as_view(),
+        name='delete_account'
+    ),
 ]
