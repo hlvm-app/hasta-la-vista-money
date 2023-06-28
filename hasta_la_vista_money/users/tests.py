a5d6cb1aa3ac6a0ac5415ca3d325eb033906e779
Alexander <dev@pavlovteam.ru>
2023-06-28 22:02:06 +0300

    Добавил тесты по пользователям

---
+++ hasta_la_vista_money/users/tests.py
@@ -0,0 +1,38 @@
+from django.test import TestCase, Client
+from django.urls import reverse_lazy
+from faker import Faker
+
+from hasta_la_vista_money.constants import HTTPStatus
+from hasta_la_vista_money.users.models import User
+
+
+class TestUser(TestCase):
+
+    fixtures = ['users.yaml']
+
+    def setUp(self) -> None:
+        self.user1 = User.objects.get(pk=1)
+        self.user2 = User.objects.get(pk=2)
+        self.client: Client = Client()
+        self.faker = Faker()
+
+    def test_create_user(self):
+        url = reverse_lazy('users:registration')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.SUCCESS_CODE.value)
+
+        Faker.seed(0)
+        username = self.faker.user_name()
+        first_name = self.faker.first_name()
+        last_name = self.faker.last_name()
+        set_password = self.faker.password(length=12)
+        new_user = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'username': username,
+            'password1': set_password,
+            'password2': set_password,
+        }
+
+        response = self.client.post(url, new_user, follow=True)
+        self.assertRedirects(response, '/login/')
