e5af813096d6e3a5ffb3f54bf2b236f6edf8f30d
Alexander <dev@pavlovteam.ru>
2023-06-29 21:17:41 +0300

    Поправил отображение подписей к полям

--- hasta_la_vista_money/account/forms.py
+++ hasta_la_vista_money/account/forms.py
@@ -4,12 +4,11 @@ from hasta_la_vista_money.account.models import Account
 
 class AddAccountForm(ModelForm):
 
-    labels = {
-        'name_account': 'Наименование счёта',
-        'balance': 'Начальный баланс',
-        'currency': 'Валюта счёта',
-    }
-
     class Meta:
         model = Account
         fields = ['name_account', 'balance', 'currency']
+        labels = {
+            'name_account': 'Наименование счёта',
+            'balance': 'Начальный баланс',
+            'currency': 'Валюта счёта',
+        }
