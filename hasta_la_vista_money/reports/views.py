194300bb5a6e220c8310dfec10b1a0af18c2edf4
Alexander <dev@pavlovteam.ru>
2023-06-29 13:19:49 +0300

    Поправил линтер

--- hasta_la_vista_money/reports/views.py
+++ hasta_la_vista_money/reports/views.py
@@ -5,12 +5,6 @@ from django.db.models import Sum
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
-from hasta_la_vista_money.constants import (
-    CURRENT_YEAR,
-    MONTH_NAMES,
-    MONTH_NUMBERS,
-    NumberMonthOfYear,
-)
 from hasta_la_vista_money.custom_mixin import CustomNoPermissionMixin
 from hasta_la_vista_money.expense.models import Expense
 from hasta_la_vista_money.income.models import Income
@@ -21,60 +15,68 @@ class ReportView(CustomNoPermissionMixin, SuccessMessageMixin, TemplateView):
     no_permission_url = reverse_lazy('login')
     success_url = reverse_lazy('reports:list')
 
-    def get(self, request, *args, **kwargs):
-        dataset = Expense.objects.filter(user=request.user).values(
-            'date').annotate(total_amount=Sum('amount')).order_by('date')
+    def get(self, request, *args, **kwargs):  # noqa: WPS210 C901
+        dataset = Expense.objects.filter(
+            user=request.user,
+        ).values(
+            'date',
+        ).annotate(
+            total_amount=Sum('amount'),
+        ).order_by('date')
 
-        income_dataset = Income.objects.filter(user=request.user).values(
-            'date').annotate(total_amount=Sum('amount')).order_by('date')
-        print(income_dataset)
+        income_dataset = Income.objects.filter(
+            user=request.user,
+        ).values(
+            'date',
+        ).annotate(
+            total_amount=Sum('amount'),
+        ).order_by('date')
+
         dates = []
         amounts = []
         income_dates = []
         income_amounts = []
 
-        for data in dataset:
-            dates.append(data['date'].strftime('%Y-%m-%d'))
-            amounts.append(float(data['total_amount']))
+        for expense_date_amount in dataset:
+            dates.append(expense_date_amount['date'].strftime('%Y-%m-%d'))
+            amounts.append(float(expense_date_amount['total_amount']))
 
-        for data in income_dataset:
-            income_dates.append(data['date'].strftime('%Y-%m-%d'))
-            income_amounts.append(float(data['total_amount']))
+        for income_date_amount in income_dataset:
+            income_dates.append(income_date_amount['date'].strftime('%Y-%m-%d'))
+            income_amounts.append(float(income_date_amount['total_amount']))
 
         unique_dates = []
         unique_amounts = []
 
-
-        # Объединение сумм по уникальным датам
-        for i, date in enumerate(dates):
-            if date not in unique_dates:
-                unique_dates.append(date)
-                unique_amounts.append(amounts[i])
+        for expense_index, expense_date in enumerate(dates):
+            if expense_date not in unique_dates:
+                unique_dates.append(expense_date)
+                unique_amounts.append(amounts[expense_index])
             else:
-                index = unique_dates.index(date)
-                unique_amounts[index] += amounts[i]
+                index = unique_dates.index(expense_date)
+                unique_amounts[index] += amounts[expense_index]
 
         unique_income_dates = []
         unique_income_amounts = []
-        print(unique_income_amounts)
-        print(income_amounts)
 
-        for i, date in enumerate(income_dates):
-            if date not in unique_income_dates:
-                unique_income_dates.append(date)
-                unique_income_amounts.append(income_amounts[i])
+        for income_index, income_date in enumerate(income_dates):
+            if income_date not in unique_income_dates:
+                unique_income_dates.append(income_date)
+                unique_income_amounts.append(income_amounts[income_index])
             else:
-                index = unique_income_dates.index(date)
-                unique_income_amounts[index] += income_amounts[i]
+                index = unique_income_dates.index(income_date)
+                unique_income_amounts[index] += income_amounts[income_index]
 
         chart = {
             'chart': {'type': 'line'},
             'title': {'text': 'Статистика по расходам и доходам'},
             'xAxis': [
                 {'categories': unique_dates,
-                 'title': {'text': 'Дата (расходы)'}},
+                 'title': {'text': 'Дата (расходы)'},
+                 },
                 {'categories': unique_income_dates,
-                 'title': {'text': 'Дата (доходы)'}}
+                 'title': {'text': 'Дата (доходы)'},
+                 },
             ],
             'yAxis': {'title': {'text': 'Сумма'}},
             'series': [
@@ -82,22 +84,20 @@ class ReportView(CustomNoPermissionMixin, SuccessMessageMixin, TemplateView):
                     'name': 'Расходы',
                     'data': unique_amounts,
                     'color': 'red',
-                    'xAxis': 0
-                    # Указываем, что данные для этой серии относятся к первой оси X
+                    'xAxis': 0,
                 },
                 {
                     'name': 'Доходы',
                     'data': unique_income_amounts,
                     'color': 'green',
-                    'xAxis': 1
-                    # Указываем, что данные для этой серии относятся ко второй оси X
-                }
+                    'xAxis': 1,
+                },
             ],
             'credits': {
-                'enabled': False
+                'enabled': False,
             },
             'exporting': {
-                'enabled': False
+                'enabled': False,
             },
         }
 
