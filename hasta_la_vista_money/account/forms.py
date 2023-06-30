3d0a96e27540707690db414e03a6285cfd90ef4c
Alexander <dev@pavlovteam.ru>
2023-06-30 10:06:11 +0300

    линтер + мелкие правки рефакторинга

--- hasta_la_vista_money/account/forms.py
+++ hasta_la_vista_money/account/forms.py
@@ -1,6 +1,12 @@
 from bootstrap_datepicker_plus.widgets import DateTimePickerInput
-from django import forms
-from django.forms import ModelForm
+from django.forms import (
+    DateTimeField,
+    DecimalField,
+    Form,
+    ModelChoiceField,
+    ModelForm,
+    ValidationError,
+)
 from hasta_la_vista_money.account.models import Account, TransferMoneyLog
 from hasta_la_vista_money.constants import NumericParameter
 
@@ -17,29 +23,33 @@ class AddAccountForm(ModelForm):
         }
 
 
-class TransferMoneyAccountForm(forms.Form):
-    from_account = forms.ModelChoiceField(queryset=None)
-    to_account = forms.ModelChoiceField(queryset=None)
-    amount = forms.DecimalField(
+class TransferMoneyAccountForm(Form):
+    from_account = ModelChoiceField(queryset=None)
+    to_account = ModelChoiceField(queryset=None)
+    amount = DecimalField(
         max_digits=NumericParameter.TWENTY.value,
         decimal_places=NumericParameter.TWO.value,
     )
-    exchange_date = forms.DateTimeField(widget=DateTimePickerInput())
+    exchange_date = DateTimeField(widget=DateTimePickerInput())
 
     def __init__(self, user, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['from_account'].queryset = Account.objects.filter(user=user)
         self.fields['to_account'].queryset = Account.objects.filter(user=user)
 
-    def clean(self):
-        cleanded_data = super().clean()
-        from_account = cleanded_data['from_account']
-        to_account = cleanded_data['to_account']
+    def clean_to_account(self):
+        cleaned_data = super().clean_to_account()
+        from_account = cleaned_data['from_account']
+        to_account = cleaned_data['to_account']
 
         if from_account == to_account:
-            raise forms.ValidationError(
-                'Вы не можете переводить с одинаково счёта'
+            raise ValidationError(
+                'Вы не можете переводить с одинакового счёта',
+                code='invalid',
             )
+
+        return cleaned_data
+
     def save(self, commit=True):
         from_account = self.cleaned_data['from_account']
         to_account = self.cleaned_data['to_account']
@@ -47,13 +57,12 @@ class TransferMoneyAccountForm(forms.Form):
         exchange_date = self.cleaned_data['exchange_date']
 
         if from_account.transfer_money(to_account, amount):
-            transfer_log = TransferMoneyLog.objects.create(
+            return TransferMoneyLog.objects.create(
                 user=from_account.user,
                 from_account=from_account,
                 to_account=to_account,
                 amount=amount,
                 exchange_date=exchange_date,
             )
-            return transfer_log
 
         return None
