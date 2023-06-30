07637467d74949f595384cea43e84596d60ebb9a
Alexander <dev@pavlovteam.ru>
2023-06-30 10:04:32 +0300

    дополнил код, чтобы данные со страницы не исчезали пи пост запросе

--- hasta_la_vista_money/applications/views.py
+++ hasta_la_vista_money/applications/views.py
@@ -7,8 +7,10 @@ from django.db.models.functions import TruncMonth
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import DeleteView, TemplateView
-from hasta_la_vista_money.account.forms import AddAccountForm, \
-    TransferMoneyAccountForm
+from hasta_la_vista_money.account.forms import (
+    AddAccountForm,
+    TransferMoneyAccountForm,
+)
 from hasta_la_vista_money.account.models import Account
 from hasta_la_vista_money.custom_mixin import CustomNoPermissionMixin
 from hasta_la_vista_money.expense.models import Expense
@@ -71,7 +73,7 @@ class PageApplication(
 
             initial_form_data = {
                 'from_account': accounts.first(),
-                'to_account': accounts.first()
+                'to_account': accounts.first(),
             }
 
             context['accounts'] = accounts
@@ -86,11 +88,48 @@ class PageApplication(
         return context
 
     def post(self, request, *args, **kwargs):
+
+        receipt_info_by_month = Receipt.objects.filter(
+            user=self.request.user,
+        ).annotate(
+            month=TruncMonth('receipt_date'),
+        ).values(
+            'month',
+            'account__name_account',
+        ).annotate(
+            count=Count('id'),
+            total_amount=Sum('total_sum'),
+        ).order_by('-month')
+
+        expenses = Expense.objects.filter(user=self.request.user).values(
+            'id',
+            'date',
+            'account__name_account',
+            'category__name',
+            'amount',
+        ).order_by('-date')
+
+        income = Income.objects.filter(
+            user=self.request.user,
+        ).values(
+            'id',
+            'date',
+            'account__name_account',
+            'category__name',
+            'amount',
+        ).order_by('-date')
+
+        income_expense = sorted(
+            list(expenses) + list(income),
+            key=itemgetter('date'),
+            reverse=True,
+        )
         accounts = Account.objects.filter(user=self.request.user).all()
         account_form = AddAccountForm(request.POST)
         transfer_money_form = TransferMoneyAccountForm(
-            user=request.user, data=request.POST
+            user=request.user, data=request.POST,
         )
+
         if account_form.is_valid():
             add_account = account_form.save(commit=False)
             if request.user.is_authenticated:
@@ -104,16 +143,17 @@ class PageApplication(
                 return redirect(self.success_url)
             transfer_money_form.add_error(None, 'Недостаточно средств!')
 
-        else:
-            return render(
-                request,
-                self.template_name,
-                {
-                    'accounts': accounts,
-                    'add_account_form': account_form,
-                    'transfer_money_form': transfer_money_form,
-                },
-            )
+        return render(
+            request,
+            self.template_name,
+            {
+                'accounts': accounts,
+                'income_expense': income_expense,
+                'add_account_form': account_form,
+                'receipt_info_by_month': receipt_info_by_month,
+                'transfer_money_form': transfer_money_form,
+            },
+        )
 
 
 class DeleteAccountView(DeleteView):
