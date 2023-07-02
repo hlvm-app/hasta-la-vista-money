d3d65b1e1202e22b5ffc77602a5fb2218738c096
Alexander <dev@pavlovteam.ru>
2023-07-02 18:59:39 +0300

    убрал пути для account

--- hasta_la_vista_money/applications/urls.py
+++ hasta_la_vista_money/applications/urls.py
@@ -1,15 +1,7 @@
 from django.urls import path
-from hasta_la_vista_money.applications.views import (
-    DeleteAccountView,
-    PageApplication,
-)
+from hasta_la_vista_money.applications.views import PageApplication
 
 app_name = 'applications'
 urlpatterns = [
     path('', PageApplication.as_view(), name='list'),
-    path(
-        'account/<int:pk>',
-        DeleteAccountView.as_view(),
-        name='delete_account',
-    ),
 ]
