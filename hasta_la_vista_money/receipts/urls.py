4f1c7f92855c5807b13cdffedf35d46740b497b6
Alexander <dev@pavlovteam.ru>
2023-07-02 12:29:01 +0300

    добавил путь для ссылки удаления

--- hasta_la_vista_money/receipts/urls.py
+++ hasta_la_vista_money/receipts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from hasta_la_vista_money.receipts.views import ReceiptView
+from hasta_la_vista_money.receipts.views import ReceiptView, ReceiptDeleteView
 
 app_name = 'receipts'
 urlpatterns = [
     path('', ReceiptView.as_view(), name='list'),
+    path('<int:pk>/', ReceiptDeleteView.as_view(), name='delete'),
 ]
