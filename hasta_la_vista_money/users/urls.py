85f5b817d6c1fb65feb0abd786256542fb36a0b2
Alexander <dev@pavlovteam.ru>
2023-07-11 21:30:20 +0300

    Добавил линки по изменению данных и установки нового пароля

--- hasta_la_vista_money/users/urls.py
+++ hasta_la_vista_money/users/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
-from hasta_la_vista_money.users.views import CreateUser, ListUsers
+from hasta_la_vista_money.users.views import (
+    CreateUser,
+    ListUsers,
+    UpdateUserPasswordView,
+    UpdateUserView,
+)
 
 app_name = 'users'
 urlpatterns = [
     path('', ListUsers.as_view(), name='list'),
     path('registration/', CreateUser.as_view(), name='registration'),
+    path('update_user/<int:pk>', UpdateUserView.as_view(), name='update_user'),
+    path(
+        'change-password/<int:pk>/',
+        UpdateUserPasswordView.as_view(),
+        name='change_password',
+    ),
 ]
