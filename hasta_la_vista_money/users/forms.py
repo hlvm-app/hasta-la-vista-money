4d6ec3af0b036b3b834e94afff2ddafee8d044e1
Alexander <dev@pavlovteam.ru>
2023-07-11 21:29:00 +0300

    Добавил формы по изменению данных и установки нового пароля

--- hasta_la_vista_money/users/forms.py
+++ hasta_la_vista_money/users/forms.py
@@ -1,4 +1,11 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    ReadOnlyPasswordHashField,
+    SetPasswordForm,
+    UserChangeForm,
+    UserCreationForm,
+)
+from hasta_la_vista_money.constants import MessageOnSite
 from hasta_la_vista_money.users.models import User
 
 
@@ -14,3 +21,21 @@ class RegisterUserForm(UserCreationForm):
         fields = [
             'first_name', 'last_name', 'username', 'password1', 'password2',
         ]
+
+
+class UpdateUserForm(UserChangeForm):
+    password = ReadOnlyPasswordHashField(
+        label='',
+        help_text=MessageOnSite.SUCCESS_MESSAGE_SET_PASSWORD.value,
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            'first_name', 'last_name', 'username',
+        ]
+
+
+class UpdateUserPasswordForm(SetPasswordForm):
+    class Meta:
+        model = User
