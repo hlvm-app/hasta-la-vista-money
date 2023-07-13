5e61d268654575ace1ebfccca6b245e5dc8102a5
Alexander <dev@pavlovteam.ru>
2023-07-13 22:17:39 +0300

    Добавил путь до профиля пользователя

--- hasta_la_vista_money/users/urls.py
+++ hasta_la_vista_money/users/urls.py
@@ -8,8 +8,8 @@ from hasta_la_vista_money.users.views import (
 
 app_name = 'users'
 urlpatterns = [
-    path('', ListUsers.as_view(), name='list'),
     path('registration/', CreateUser.as_view(), name='registration'),
+    path('profile/<int:pk>/', ListUsers.as_view(), name='profile'),
     path('update_user/<int:pk>', UpdateUserView.as_view(), name='update_user'),
     path(
         'change-password/<int:pk>/',
