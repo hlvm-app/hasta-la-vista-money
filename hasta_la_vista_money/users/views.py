dd8e1b316dfa37849251c9354a5cd6ff3771ceae
Alexander <dev@pavlovteam.ru>
2023-07-13 22:21:41 +0300

    Сделал формы для смены пароля и информации пользователя

--- hasta_la_vista_money/users/views.py
+++ hasta_la_vista_money/users/views.py
@@ -4,7 +4,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import CreateView, ListView, TemplateView, UpdateView
+from django.views.generic import CreateView, TemplateView, UpdateView
 from hasta_la_vista_money.constants import MessageOnSite
 from hasta_la_vista_money.users.forms import (
     RegisterUserForm,
@@ -22,10 +22,21 @@ class IndexView(TemplateView):
         return redirect('login')
 
 
-class ListUsers(ListView):
+class ListUsers(TemplateView):
     model = User
-    template_name = 'header.html'
+    template_name = 'users/profile.html'
     context_object_name = 'users'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            user_update = UpdateUserForm(instance=self.request.user)
+            user_update_pass_form = UpdateUserPasswordForm(
+                user=self.request.user,
+            )
+            context['user_update'] = user_update
+            context['user_update_pass_form'] = user_update_pass_form
+        return context
 
 
 class LoginUser(SuccessMessageMixin, LoginView):
@@ -68,28 +79,48 @@ class CreateUser(SuccessMessageMixin, CreateView):
 
 class UpdateUserView(SuccessMessageMixin, UpdateView):
     model = User
-    template_name = 'header.html'
+    template_name = 'users/profile.html'
     form_class = UpdateUserForm
     success_message = MessageOnSite.SUCCESS_MESSAGE_CHANGED_PROFILE.value
 
     def get_success_url(self):
-        return reverse_lazy('applications:list')
+        user = self.kwargs['pk']
+        return reverse_lazy('users:profile', kwargs={'pk': user})
+
+    def post(self, request, *args, **kwargs):
+        user_update = UpdateUserForm(request.POST)
+        if user_update.is_valid():
+            user_update.save()
+            return redirect(self.get_success_url())
+        return render(
+            request,
+            self.template_name,
+            {
+                'user_update': user_update,
+            },
+        )
 
 
 class UpdateUserPasswordView(SuccessMessageMixin, PasswordChangeView):
     model = User
-    template_name = 'header.html'
+    template_name = 'users/profile.html'
     form_class = UpdateUserPasswordForm
     success_message = MessageOnSite.SUCCESS_MESSAGE_CHANGED_PASSWORD.value
 
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            update_pass_user_form = UpdateUserPasswordForm(user=request.user)
-            return render(
-                request,
-                self.template_name,
-                {'update_pass_user_form': update_pass_user_form},
-            )
-
     def get_success_url(self):
-        return reverse_lazy('applications:list')
+        user = self.kwargs['pk']
+        return reverse_lazy('users:profile', kwargs={'pk': user})
+
+    def post(self, request, *args, **kwargs):
+        user_update_pass_form = UpdateUserPasswordForm(request.POST)
+
+        if user_update_pass_form.is_valid():
+            user_update_pass_form.save()
+            return redirect(self.get_success_url())
+        return render(
+            request,
+            self.template_name,
+            {
+                'user_update_pass_form': user_update_pass_form,
+            },
+        )
