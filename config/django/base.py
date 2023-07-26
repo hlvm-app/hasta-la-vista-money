5a06a0b7bcd3eb57575e22aee32fe3557127bf58
Alexander Pavlov <alexander.pavlov@pavlovteam.ru>
2023-07-26 09:35:48 +0300

    разделил config django

--- config/django/base.py
+++ config/django/base.py
@@ -263,31 +263,6 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-INTERNAL_IPS = [
-    os.environ.get(
-        'LOCAL_IPS',
-    ) if os.environ.get(
-        'LOCAL_IPS',
-    ) else '127.0.0.1',
-]
-
-
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.history.HistoryPanel',
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel',
-]
-
 if os.environ.get('ACCESS_TOKEN') is not None:
     ROLLBAR = {
         'access_token': os.environ.get('ACCESS_TOKEN'),
