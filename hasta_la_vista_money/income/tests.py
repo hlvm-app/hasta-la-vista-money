5c9e6391719170f3a01104ac5769fa01b988e1d6
Alexander <dev@pavlovteam.ru>
2023-07-28 06:29:00 +0300

    Добавил тесты по изменению доходов

--- hasta_la_vista_money/income/tests.py
+++ hasta_la_vista_money/income/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.urls import reverse_lazy
 from hasta_la_vista_money.account.models import Account
 from hasta_la_vista_money.constants import HTTPStatus
@@ -6,6 +6,7 @@ from hasta_la_vista_money.income.models import Income, IncomeType
 from hasta_la_vista_money.users.models import User
 
 TEST_AMOUNT = 15000
+NEW_TEST_AMOUNT = 25000
 
 
 class TestIncome(TestCase):
@@ -15,6 +16,7 @@ class TestIncome(TestCase):
     ]
 
     def setUp(self) -> None:
+        self.client = Client()
         self.user = User.objects.get(pk=1)
         self.account = Account.objects.get(pk=1)
         self.income = Income.objects.get(pk=1)
@@ -39,6 +41,24 @@ class TestIncome(TestCase):
         }
 
         response = self.client.post(url, data=new_income, follow=True)
+        self.assertEqual(response.status_code, HTTPStatus.SUCCESS_CODE.value)
+
+    def test_income_update(self):
+        self.client.force_login(self.user)
+        url = reverse_lazy('income:change', args=(self.income.pk,))
+        update_expense = {
+            'user': self.user,
+            'category': self.income_type,
+            'account': self.account,
+            'date': '15/02/2023 15:30',
+            'amount': NEW_TEST_AMOUNT,
+        }
+
+        response = self.client.post(url, update_expense)
+
+        self.assertEqual(
+            Income.objects.get(pk=self.income.pk), self.income,
+        )
         self.assertEqual(response.status_code, HTTPStatus.SUCCESS_CODE.value)
 
     def test_income_delete(self):
