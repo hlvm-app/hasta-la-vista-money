0ea164a4ddd2db25b5ebd4cd589adcd89bbf8994
Alexander <dev@pavlovteam.ru>
2023-08-01 06:07:04 +0300

    добавил миксин для удаления категорий

--- hasta_la_vista_money/custom_mixin.py
+++ hasta_la_vista_money/custom_mixin.py
@@ -1,8 +1,10 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import ProtectedError
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import DeleteView, DetailView
+from django.views.generic import CreateView, DeleteView
+from hasta_la_vista_money.constants import MessageOnSite
 
 
 class CustomNoPermissionMixin(LoginRequiredMixin):
@@ -16,34 +18,44 @@ class CustomNoPermissionMixin(LoginRequiredMixin):
         return redirect(self.no_permission_url)
 
 
-class DeleteCategoryMixin(DetailView, DeleteView):
+class DeleteCategoryMixin(DeleteView):
+    model = None
     success_url = None
 
     def form_valid(self, form):
-        success = self.delete_category()
-        if success:
-            success_message = self.get_success_message()
-            if success_message:
-                messages.success(self.request, success_message)
+        try:
+            category = self.object
+            category.delete()
+            messages.success(
+                self.request, MessageOnSite.SUCCESS_CATEGORY_DELETED.value,
+            )
             return super().form_valid(form)
-        error_message = self.get_error_message()
-        messages.error(self.request, error_message)
-        return redirect(self.get_success_url())
-
-    def get_error_message(self):
-        return ''
-
-    def get_success_message(self):
-        return ''
-
-    def delete_category(self):
-        raise NotImplementedError('Subclasses must implement delete_category()')
+        except ProtectedError:
+            messages.error(
+                self.request, MessageOnSite.ACCESS_DENIED_DELETE_CATEGORY.value,
+            )
+            return redirect(self.success_url)
 
 
-class CustomSuccessMessageUserMixin:
+class CustomSuccessURLUserMixin:
     def __init__(self):
         self.kwargs = None
 
     def get_success_url(self):
         user = self.kwargs['pk']
         return reverse_lazy('users:profile', kwargs={'pk': user})
+
+
+class ExpenseIncomeFormValidCreateMixin(CreateView):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request = None
+
+    def form_valid(self, form):
+        category_form = form.save(commit=False)
+        category_form.user = self.request.user
+        category_form.save()
+        messages.success(
+            self.request, MessageOnSite.SUCCESS_CATEGORY_ADDED.value,
+        )
+        return super().form_valid(form)
