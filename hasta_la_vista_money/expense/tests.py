36bed29e68fb180615dafbf6f58045923c17949c
Alexander <dev@pavlovteam.ru>
2023-08-01 21:31:50 +0300

    рефакторинг

--- hasta_la_vista_money/expense/tests.py
+++ hasta_la_vista_money/expense/tests.py
@@ -77,7 +77,7 @@ class TestExpense(TestCase):
 
         new_category = {
             'user': self.user,
-            'name': 'Аванс'
+            'name': 'ЖКХ'
         }
 
         response = self.client.post(url, data=new_category, follow=True)
