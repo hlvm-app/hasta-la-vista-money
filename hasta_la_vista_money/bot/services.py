106a0a9f605cc8204e30cdb42a39b207c3af9ac6
Alexander <dev@pavlovteam.ru>
2023-08-03 22:08:05 +0300

    Добавил условия на проверку формата дат

--- hasta_la_vista_money/bot/services.py
+++ hasta_la_vista_money/bot/services.py
@@ -18,17 +18,18 @@ def convert_date_time(date_time: Union[str, int]) -> Union[str, None]:
 
     """
     try:
-        if date_time is None or date_time == '':
+        if date_time is None:
             return None
-        if ':' in date_time and 'T' in date_time:
-            dt = datetime.datetime.strptime(date_time, '%Y-%m-%dT%H:%M:%S')
-            return dt.strftime('%Y-%m-%d %H:%M')
-        elif ':' not in date_time:
-            dt = datetime.datetime.strptime(date_time, '%Y%m%dT%H%M%S')
-            return dt.strftime('%Y-%m-%d %H:%M')
-        elif 'T' not in date_time:
-            dt = datetime.datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')
-            return dt.strftime('%Y%m%dT%H%M%S')
+        if isinstance(date_time, str):
+            if ':' in date_time and 'T' in date_time:
+                dt = datetime.datetime.strptime(date_time, '%Y-%m-%dT%H:%M:%S')
+                return dt.strftime('%Y-%m-%d %H:%M')
+            elif ':' not in date_time:
+                dt = datetime.datetime.strptime(date_time, '%Y%m%dT%H%M%S')
+                return dt.strftime('%Y-%m-%d %H:%M')
+            elif 'T' not in date_time:
+                dt = datetime.datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S')
+                return dt.strftime('%Y%m%dT%H%M%S')
         dt = datetime.datetime.fromtimestamp(int(date_time))
         return f'{dt:%Y-%m-%d %H:%M}'
     except TypeError as error:
