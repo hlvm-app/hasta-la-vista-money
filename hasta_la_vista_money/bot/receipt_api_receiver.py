e827d1dae4f82e0d57ed42bc20a41b1cc5b2aaff
Alexander <dev@pavlovteam.ru>
2023-08-03 22:06:24 +0300

    добавил проверку на исключение

--- hasta_la_vista_money/bot/receipt_api_receiver.py
+++ hasta_la_vista_money/bot/receipt_api_receiver.py
@@ -77,34 +77,45 @@ class ReceiptApiReceiver:
         Raises:
             ValueError: Ошибка переданного значения
         """
-        client_secret = [
-            env
-            for env in ('CLIENT_SECRET', 'INN', 'PASSWORD')
-            if os.getenv(env) is None
-        ]
-        if client_secret:
-            raise ValueError(
-                f'OS environments not content {", ".join(client_secret)}',
+        try:
+            client_secret = [
+                env
+                for env in ('CLIENT_SECRET', 'INN', 'PASSWORD')
+                if os.getenv(env) is None
+            ]
+            if client_secret:
+                raise ValueError(
+                    f'OS environments not content {", ".join(client_secret)}',
+                )
+
+            url = f'https://{self.host}/v2/mobile/users/lkfl/auth'
+            payload = {
+                'inn': os.getenv('INN'),
+                'client_secret': os.getenv('CLIENT_SECRET'),
+                'password': os.getenv('PASSWORD'),
+            }
+            headers = {
+                'Host': self.host,
+                'Accept': self.accept,
+                'Device-OS': self.device_os,
+                'Device-Id': self.device_id,
+                'clientVersion': self.client_version,
+                'Accept-Language': self.accept_language,
+                'User-Agent': self.user_agent,
+            }
+
+            response = requests.post(
+                url,
+                json=payload,
+                headers=headers,
+                timeout=10
             )
-
-        url = f'https://{self.host}/v2/mobile/users/lkfl/auth'
-        payload = {
-            'inn': os.getenv('INN'),
-            'client_secret': os.getenv('CLIENT_SECRET'),
-            'password': os.getenv('PASSWORD'),
-        }
-        headers = {
-            'Host': self.host,
-            'Accept': self.accept,
-            'Device-OS': self.device_os,
-            'Device-Id': self.device_id,
-            'clientVersion': self.client_version,
-            'Accept-Language': self.accept_language,
-            'User-Agent': self.user_agent,
-        }
-
-        response = requests.post(url, json=payload, headers=headers, timeout=10)
-        self._session_id = response.json()['sessionId']
+            self._session_id = response.json()['sessionId']
+        except (
+            requests.exceptions.ConnectionError,
+            json.decoder.JSONDecodeError,
+        ):
+            logger.error('Недоступен сервис авторизации.')
 
     def get_receipt(self, qr: str) -> dict | None:
         """
