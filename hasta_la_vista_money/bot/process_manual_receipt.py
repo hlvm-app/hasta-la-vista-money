273ba8fc4761f4f8c18f347d3c6fea2881b46add
Alexander <dev@pavlovteam.ru>
2023-08-08 22:23:38 +0300

    добавил функцию обработчик

--- hasta_la_vista_money/bot/process_manual_receipt.py
+++ hasta_la_vista_money/bot/process_manual_receipt.py
@@ -1,13 +1,20 @@
 from hasta_la_vista_money.bot.config_bot import bot_admin
 from hasta_la_vista_money.users.models import TelegramUser
 
-class HandleManualReceipt:
+dictionary_string_from_qrcode = {}
 
-    self.dictionary_string_from_qrcode = {}
-    @bot_admin.message_handler(commands=['manual'])
-    def start_process_add_manual_receipt(message):
-        telegram_username = message.from_user.username
-        existing_telegram_user = TelegramUser.objects.filter(username=telegram_username).first()
-        if existing_telegram_user:
-            user_text = message.text
-            
+
+@bot_admin.message_handler(commands=['manual'])
+def start_process_add_manual_receipt(message):
+    """
+    Функция обработчик команды manual.
+
+    :param message:
+    :return:
+    """
+    telegram_username = message.from_user.username
+    existing_telegram_user = TelegramUser.objects.filter(
+        username=telegram_username,
+    ).first()
+    if existing_telegram_user:
+        bot_admin.send_message(message.chat.id, '')
