b9e2635068b3e3234e87d4deeea3acd35dbcc93e
Alexander <dev@pavlovteam.ru>
2023-08-10 21:38:23 +0300

    поправил код

--- hasta_la_vista_money/bot/middleware.py
+++ hasta_la_vista_money/bot/middleware.py
@@ -1,23 +1,31 @@
 from hasta_la_vista_money.bot.config_bot import bot_admin
+from hasta_la_vista_money.constants import TelegramMessage
 from hasta_la_vista_money.users.models import TelegramUser
 from telebot.handler_backends import BaseMiddleware, CancelUpdate
 
 
 class AccessMiddleware(BaseMiddleware):
-
     def __init__(self):
         super().__init__()
         self.telegram_username = None
         self.update_types = ['message']
 
-    def pre_process(self, message, data):
+    def pre_process(self, message, data):  # noqa: WPS110
         self.telegram_username = TelegramUser.objects.filter(
             telegram_id=message.from_user.id,
         ).first()
+
         if message.text.startswith('/auth'):
-            return super().pre_process(message, data)
+            return None
 
         if not self.telegram_username:
-            bot_admin.send_message(message.chat.id, 'У вас нет доступа к использованию бота, сначала надо авторизоваться - /auth')
+            bot_admin.send_message(
+                message.chat.id,
+                TelegramMessage.ACCESS_DENIED.value,
+            )
             return CancelUpdate()
-        return super().pre_process(message, data)
+        return data
+
+    def post_process(self, message, data, exception):
+        if exception:
+            bot_admin.send_message(message.chat.id, exception)
