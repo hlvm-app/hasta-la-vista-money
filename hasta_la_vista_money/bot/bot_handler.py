0f67306b222bd2697c75cdf9c9753f464d792f64
Alexander <dev@pavlovteam.ru>
2023-08-10 21:42:26 +0300

    Поправил название функций, добавил обработчик удаления телеграм аккаунта из базы и оптимизировал код

--- hasta_la_vista_money/bot/bot_handler.py
+++ hasta_la_vista_money/bot/bot_handler.py
@@ -11,26 +11,88 @@ from hasta_la_vista_money.bot.receipt_parser_text_qrcode import (
     handle_receipt_text_qrcode,
 )
 from hasta_la_vista_money.constants import TelegramMessage
-from hasta_la_vista_money.users.models import TelegramUser
+from hasta_la_vista_money.users.models import TelegramUser, User
 from telebot import types
-
 
 bot_admin.setup_middleware(AccessMiddleware())
 
 
-
-
-
-def check_telegram_user(message):
+@bot_admin.message_handler(commands=['auth'])
+def handle_start(message):
     """
-    Проверка существования телеграм пользователя в базе.
+    Обработка команды /auth.
 
-    :param telegram_user_id:
+    :param message:
     :return:
     """
-    telegram_user_id = message.from_user.id
+    telegram_user = get_telegram_user(message)
+    if telegram_user:
+        bot_admin.send_message(
+            message.chat.id,
+            TelegramMessage.ALREADY_LOGGED.value,
+        )
+    else:
+        bot_admin.send_message(
+            message.chat.id,
+            TelegramMessage.REQUIRED_AUTHORIZATION.value,
+        )
+        bot_admin.register_next_step_handler(message, handle_auth)
+
+
+def handle_auth(message):
+    """
+    Обработка логина и пароля от сайта для авторизации в боте.
+
+    :param message:
+    :return:
+    """
+    auth_data = message.text.split(':')
+    if len(auth_data) != 2:
+        bot_admin.reply_to(message, TelegramMessage.INCORRECT_FORMAT.value)
+        return
+
+    username, password = map(str, auth_data)
+    user = User.objects.filter(username=username).first()
+
+    if user and user.check_password(password):
+        create_telegram_user(message, user)
+    else:
+        bot_admin.reply_to(
+            message,
+            TelegramMessage.INVALID_USERNAME_PASSWORD.value,
+        )
+        bot_admin.delete_message(message.from_user.id, message.message_id)
+
+
+def create_telegram_user(message, user):
+    """
+    Создание записи в базе данных с данными о телеграм пользователе.
+
+    :param message:
+    :param user:
+    :return:
+    """
+    TelegramUser.objects.create(
+        user=user,
+        username=message.from_user.username,
+        telegram_id=message.from_user.id,
+    )
+    bot_admin.reply_to(
+        message,
+        TelegramMessage.AUTHORIZATION_SUCCESSFUL.value,
+    )
+    bot_admin.delete_message(message.from_user.id, message.message_id)
+
+
+def get_telegram_user(message):
+    """
+    Функция получения о наличии телеграм пользователя в базе данных.
+
+    :param message:
+    :return:
+    """
     return TelegramUser.objects.filter(
-        telegram_id=telegram_user_id,
+        telegram_id=message.from_user.id,
     ).first()
 
 
@@ -51,7 +113,7 @@ def create_buttons_with_account(user):
     :param user:
     :return:
     """
-    accounts = check_account_exist(user)
+    accounts = Account.objects.filter(user=user)
     markup = types.InlineKeyboardMarkup()
     for account in accounts:
         button = types.InlineKeyboardButton(
@@ -90,9 +152,9 @@ def select_account(message):
     :param message:
     :return:
     """
-    telegram_user = check_telegram_user(message)
+    telegram_user = get_telegram_user(message)
     user = telegram_user.user
-    if telegram_user and check_account_exist(user):
+    if check_account_exist(user):
         markup = create_buttons_with_account(user)
         bot_admin.reply_to(message, 'Выберете счёт:', reply_markup=markup)
     else:
@@ -111,7 +173,7 @@ def handle_select_account(call):
     :param call:
     :return:
     """
-    telegram_user = check_telegram_user(call)
+    telegram_user = get_telegram_user(call)
     account_id = int(call.data.split('_')[2])
     telegram_user.selected_account_id = account_id
     telegram_user.save()
@@ -140,15 +202,21 @@ def start_process_add_manual_receipt(message):
             ),
         ),
     )
-    bot_admin.register_next_step_handler(message, get_date_receipt)
+    bot_admin.register_next_step_handler(message, process_date_receipt)
 
 
-def get_date_receipt(message):
+def process_date_receipt(message):
+    """
+    Получение даты от пользователя.
+
+    :param message:
+    :return:
+    """
     try:
         date = parse(message.text)
         dictionary_string_from_qrcode['date'] = f'{date:%Y%m%dT%H%M%S}'
         bot_admin.send_message(message.chat.id, 'Введите сумму чека')
-        bot_admin.register_next_step_handler(message, get_amount_receipt)
+        bot_admin.register_next_step_handler(message, process_amount_receipt)
     except ParserError:
         bot_admin.send_message(
             message.chat.id,
@@ -156,45 +224,75 @@ def get_date_receipt(message):
         )
 
 
-def get_amount_receipt(message):
+def process_amount_receipt(message):
+    """
+    Получение суммы от пользователя.
+
+    :param message:
+    :return:
+    """
     try:
         amount_receipt = message.text
         dictionary_string_from_qrcode['amount'] = decimal.Decimal(
             amount_receipt,
         )
         bot_admin.send_message(message.chat.id, 'Введите номер ФН')
-        bot_admin.register_next_step_handler(message, get_fiscal_number_receipt)
+        bot_admin.register_next_step_handler(
+            message,
+            process_fiscal_number_receipt,
+        )
     except ValueError:
         bot_admin.send_message(message.chat.id, 'Введите сумму!')
 
 
-def get_fiscal_number_receipt(message):
+def process_fiscal_number_receipt(message):
+    """
+    Получение ФН от пользователя.
+
+    :param message:
+    :return:
+    """
     try:
         fn_receipt = message.text
         dictionary_string_from_qrcode['fn'] = int(fn_receipt)
         bot_admin.send_message(message.chat.id, 'Введите номер ФД')
-        bot_admin.register_next_step_handler(message, get_fiscal_doc_receipt)
+        bot_admin.register_next_step_handler(
+            message,
+            process_fiscal_doc_receipt,
+        )
     except ValueError:
         bot_admin.send_message(message.chat.id, 'Введите корректный номер ФН!')
 
 
-def get_fiscal_doc_receipt(message):
+def process_fiscal_doc_receipt(message):
+    """
+    Получение ФД от пользователя.
+
+    :param message:
+    :return:
+    """
     try:
         fd_receipt = message.text
         dictionary_string_from_qrcode['fd'] = int(fd_receipt)
         bot_admin.send_message(message.chat.id, 'Введите номер ФП')
-        bot_admin.register_next_step_handler(message, get_fp_receipt)
+        bot_admin.register_next_step_handler(message, process_fp_receipt)
     except ValueError:
         bot_admin.send_message(message.chat.id, 'Введите корректный номер ФД!')
 
 
-def get_fp_receipt(message):
+def process_fp_receipt(message):
+    """
+    Получение ФП от пользователя.
+
+    :param message:
+    :return:
+    """
     try:
         fp_receipt = message.text
         dictionary_string_from_qrcode['fp'] = int(fp_receipt)
         telegram_user_id = message.from_user.id
 
-        telegram_user = check_telegram_user(telegram_user_id)
+        telegram_user = get_telegram_user(telegram_user_id)
 
         if telegram_user:
             user = telegram_user.user
@@ -217,6 +315,18 @@ def get_fp_receipt(message):
         bot_admin.send_message(message.chat.id, 'Введите корректный номер ФП!')
 
 
+@bot_admin.message_handler(commands=['deauthorize'])
+def process_deauthorize(message):
+    """
+    Функция отвязки телеграм аккаунта от аккаунта на сайте.
+
+    :param message:
+    :return:
+    """
+    TelegramUser.objects.get(telegram_id=message.from_user.id).delete()
+    bot_admin.send_message(message.chat.id, 'Телеграм аккаунт отвязан!')
+
+
 @bot_admin.message_handler(content_types=['text', 'document', 'photo'])
 def handle_receipt(message):
     """
@@ -225,7 +335,7 @@ def handle_receipt(message):
     :param message:
     :return:
     """
-    telegram_user = check_telegram_user(message)
+    telegram_user = get_telegram_user(message)
     if telegram_user:
         user = telegram_user.user
         account = telegram_user.selected_account_id
