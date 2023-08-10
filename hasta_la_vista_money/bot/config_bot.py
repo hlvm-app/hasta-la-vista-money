f9ae78eecc8546fc08923093d9d95ad286e87c81
Alexander <dev@pavlovteam.ru>
2023-08-10 21:41:26 +0300

    линтер

--- hasta_la_vista_money/bot/config_bot.py
+++ hasta_la_vista_money/bot/config_bot.py
@@ -4,7 +4,6 @@ import os
 from dotenv import load_dotenv
 from telebot import TeleBot, types
 
-
 load_dotenv()
 token = os.getenv('TOKEN_TELEGRAM_BOT')
 bot_admin = TeleBot(token, threaded=False, use_class_middlewares=True)
