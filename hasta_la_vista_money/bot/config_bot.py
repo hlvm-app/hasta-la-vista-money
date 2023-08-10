543a1c34983053e0cdea85a5e4053d42f81e913f
Alexander Pavlov <dev@pavlovteam.ru>
2023-08-10 08:32:11 +0000

    добавил middleware для проверки авторизации пользователя

--- hasta_la_vista_money/bot/config_bot.py
+++ hasta_la_vista_money/bot/config_bot.py
@@ -3,8 +3,11 @@ import os
 
 from dotenv import load_dotenv
 from telebot import TeleBot, types
+from hasta_la_vista_money.bot.middleware import Middleware
+
 
 load_dotenv()
 token = os.getenv('TOKEN_TELEGRAM_BOT')
 bot_admin = TeleBot(token, threaded=False)
+bot_admin.setup_middleware(Middleware)
 bot_type = types
---
+++ hasta_la_vista_money/bot/middleware.py
@@ -0,0 +1,19 @@
+from telebot.handler_backends import BaseMiddleware, CancelUpdate
+from hasta_la_vista_money.bot.config_bot import bot_admin
+from hasta_la_vista_money.users.models import TelegramUser
+
+
+class Middleware(BaseMiddleware):
+    def __init__(self):
+        super().__init__()
+        self.telegram_username = None
+
+
+    def pre_process(self, message, data):
+        self.telegram_username = TelegramUser.objects.filter(
+            telegram_id=message.from_user.id,
+        ).first()
+        if not self.telegram_username:
+            bot_admin.send_message(message.chat.id, 'Access Denied')
+            return CancelUpdate()
+        return super().pre_process(message, data)
