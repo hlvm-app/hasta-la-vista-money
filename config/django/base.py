9c957725c196e6225db6e32ffc33c82e4ba92347
Alexander <dev@pavlovteam.ru>
2023-08-13 12:14:06 +0300

    refactoring by pre-commit

--- config/django/base.py
+++ config/django/base.py
@@ -266,7 +266,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
