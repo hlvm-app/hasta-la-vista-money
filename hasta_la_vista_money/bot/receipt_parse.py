014c1425124f174c16240cc40f94778d3860d42d
Alexander <dev@pavlovteam.ru>
2023-08-17 20:46:27 +0300

    Вынес вспомогательные методы в отдельный модуль как функции

--- hasta_la_vista_money/bot/receipt_parse.py
+++ hasta_la_vista_money/bot/receipt_parse.py
@@ -13,9 +13,14 @@ from hasta_la_vista_money.bot.receipt_data_classes import (
     ReceiptData,
     ReceiptDataWriter,
 )
+from hasta_la_vista_money.bot.receipt_parse_handler import (
+    check_exists_number_receipt,
+    check_operation_type,
+    get_string_result_receipt,
+    handle_integrity_error,
+)
 from hasta_la_vista_money.bot.services import convert_date_time, convert_number
 from hasta_la_vista_money.constants import ReceiptConstants, TelegramMessage
-from hasta_la_vista_money.receipts.models import Receipt
 
 
 class ReceiptParser:
@@ -194,22 +199,22 @@ class ReceiptParser:
                 total_sum,
             ) = self.extract_receipt_info()
 
-            total_sum = self.check_operation_type(operation_type, total_sum)
-
-            check_number_receipt = Receipt.objects.filter(
-                user=self.user,
-                number_receipt=number_receipt,
-            ).first()
+            total_sum = check_operation_type(operation_type, total_sum)
 
             if 'query' in self.json_data and number_receipt is None:
                 logger.error(ReceiptConstants.RECEIPT_NOT_ACCEPTED.value)
                 return
 
+            check_number_receipt = check_exists_number_receipt(
+                user=self.user,
+                number_receipt=number_receipt,
+            )
             if check_number_receipt:
                 bot_admin.send_message(
                     chat_id,
                     ReceiptConstants.RECEIPT_ALREADY_EXISTS.value,
                 )
+                return
             else:
                 self.process_receipt_data(
                     receipt_date,
@@ -219,10 +224,15 @@ class ReceiptParser:
                 )
                 bot_admin.send_message(
                     chat_id,
-                    self.get_string_result_receipt(),
+                    get_string_result_receipt(
+                        account=self.account,
+                        product_list=self.product_list,
+                        receipt_date=self.receipt.receipt_date,
+                        customer=self.customer,
+                    ),
                 )
         except IntegrityError as integrity_error:
-            self.handle_integrity_error(
+            handle_integrity_error(
                 chat_id=chat_id,
                 integrity_error=integrity_error,
             )
@@ -231,49 +241,6 @@ class ReceiptParser:
                 chat_id,
                 TelegramMessage.NOT_CREATE_ACCOUNT.value,
             )
-
-    def get_string_result_receipt(self):
-        account_balance = get_object_or_404(Account, id=self.account)
-        products = [product.product_name for product in self.product_list]
-        newline_char = '\n'
-        return ''.join(
-            (
-                'Чек успешно добавлен.\n\n',
-                'Параметры чека:\n',
-                f'<b>Счёт списания:</b> {account_balance.name_account}\n',
-                f'<b>Дата чека:</b> {self.receipt.receipt_date}\n',
-                f'<b>Продавец:</b> {self.customer}\n\n',
-                f"<b>Товары:</b>\n{f', {newline_char}'.join(products)}\n",
-            ),
-        )
-
-    @classmethod
-    def check_operation_type(cls, operation_type, total_sum):
-        return -total_sum if operation_type in {2, 3} else total_sum
-
-    def extract_receipt_info(self):
-        receipt_date = convert_date_time(
-            self.parser.parse_json(
-                self.json_data,
-                ReceiptConstants.RECEIPT_DATE_TIME.value,
-            ),
-        )
-        number_receipt = self.parser.parse_json(
-            self.json_data,
-            ReceiptConstants.NUMBER_RECEIPT.value,
-        )
-
-        operation_type = self.parser.parse_json(
-            self.json_data,
-            ReceiptConstants.OPERATION_TYPE.value,
-        )
-        total_sum = convert_number(
-            self.parser.parse_json(
-                self.json_data,
-                ReceiptConstants.TOTAL_SUM.value,
-            ),
-        )
-        return receipt_date, number_receipt, operation_type, total_sum
 
     def process_receipt_data(
         self,
@@ -299,18 +266,29 @@ class ReceiptParser:
         self.receipt = ReceiptDataWriter.create_receipt(receipt_data)
         self.parse_products()
 
-    @classmethod
-    def handle_integrity_error(cls, chat_id, integrity_error):
-        if 'account' in str(integrity_error):
-            bot_admin.send_message(
-                chat_id,
-                TelegramMessage.NOT_CREATE_ACCOUNT.value,
-            )
-        else:
-            bot_admin.send_message(
-                chat_id,
-                TelegramMessage.ERROR_DATABASE_RECORD.value,
-            )
+    def extract_receipt_info(self):
+        receipt_date = convert_date_time(
+            self.parser.parse_json(
+                self.json_data,
+                ReceiptConstants.RECEIPT_DATE_TIME.value,
+            ),
+        )
+        number_receipt = self.parser.parse_json(
+            self.json_data,
+            ReceiptConstants.NUMBER_RECEIPT.value,
+        )
+
+        operation_type = self.parser.parse_json(
+            self.json_data,
+            ReceiptConstants.OPERATION_TYPE.value,
+        )
+        total_sum = convert_number(
+            self.parser.parse_json(
+                self.json_data,
+                ReceiptConstants.TOTAL_SUM.value,
+            ),
+        )
+        return receipt_date, number_receipt, operation_type, total_sum
 
     def parse(self, chat_id: int) -> None:
         """
---
+++ hasta_la_vista_money/bot/receipt_parse_handler.py
@@ -0,0 +1,77 @@
+from django.shortcuts import get_object_or_404
+from hasta_la_vista_money.account.models import Account
+from hasta_la_vista_money.bot.config_bot import bot_admin
+from hasta_la_vista_money.constants import TelegramMessage
+from hasta_la_vista_money.receipts.models import Receipt
+
+
+def get_string_result_receipt(account, product_list, receipt_date, customer):
+    """
+    Формирование строки с краткой информацией о добавленном чеке.
+
+    :param account:
+    :param product_list:
+    :param receipt_date:
+    :param customer:
+    :return:
+    """
+    account_balance = get_object_or_404(Account, id=account)
+    products = [product.product_name for product in product_list]
+    newline_char = '\n'
+    return ''.join(
+        (
+            'Чек успешно добавлен.\n\n',
+            'Параметры чека:\n',
+            f'<b>Счёт списания:</b> {account_balance.name_account}\n',
+            f'<b>Дата чека:</b> {receipt_date}\n',
+            f'<b>Продавец:</b> {customer}\n\n',
+            f"<b>Товары:</b>\n{f', {newline_char}'.join(products)}\n",
+        ),
+    )
+
+
+def check_exists_number_receipt(user, number_receipt):
+    """
+    Проверка на существование номера чека в базе данных.
+
+    :param user:
+    :param number_receipt:
+    :return:
+    """
+    return Receipt.objects.filter(
+        user=user,
+        number_receipt=number_receipt,
+    ).first()
+
+
+def check_operation_type(operation_type, total_sum):
+    """
+    Проверка на тип операции.
+
+    Если это возврат покупки или возврат выигрыша, то это минус со счёта.
+
+    :param operation_type:
+    :param total_sum:
+    :return:
+    """
+    return -total_sum if operation_type in {2, 3} else total_sum
+
+
+def handle_integrity_error(chat_id, integrity_error):
+    """
+    Обработка исключений.
+
+    :param chat_id:
+    :param integrity_error:
+    :return:
+    """
+    if 'account' in str(integrity_error):
+        bot_admin.send_message(
+            chat_id,
+            TelegramMessage.NOT_CREATE_ACCOUNT.value,
+        )
+    else:
+        bot_admin.send_message(
+            chat_id,
+            TelegramMessage.ERROR_DATABASE_RECORD.value,
+        )
