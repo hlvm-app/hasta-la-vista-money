cdc7468d74a9983080514dd3b26fc965dd999e44
Alexander Pavlov <dev@pavlovteam.ru>
2023-08-24 06:54:03 +0000

    Добавил в задачу обработаку json файла

--- hasta_la_vista_money/bot/tasks.py
+++ hasta_la_vista_money/bot/tasks.py
@@ -1,4 +1,5 @@
 """Модуль задач для пакета bot."""
+import json
 from celery import shared_task
 from hasta_la_vista_money.bot.receipt_api_receiver import ReceiptApiReceiver
 from hasta_la_vista_money.bot.receipt_parse import ReceiptParser
@@ -28,3 +29,25 @@ def async_handle_receipt_text_qrcode(
     json_data = client.get_receipt(input_user)
     parse = ReceiptParser(json_data, user, account)
     parse.parse_receipt(chat_id)
+
+
+@shared_task
+def async_handle_receipt_json(
+    chat_id,
+    user_id,
+    account,
+    json_data,
+):
+    """
+    Функция-задача celery для обработки json файлов от пользователя.
+
+    :param chat_id:
+    :param user_id:
+    :param account:
+    :param json_data:
+    :return:
+    """
+    user = User.objects.get(id=user_id)
+    parse = ReceiptParser(json_data, user, account)
+    parse.parse_receipt(chat_id)
+    
