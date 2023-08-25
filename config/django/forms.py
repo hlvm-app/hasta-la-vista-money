2ebb2e86a83b08761aea0ad358dea9c3766efd0c
Alexander Pavlov <dev@pavlovteam.ru>
2023-08-25 14:43:02 +0300

    Update forms.py

--- config/django/forms.py
+++ config/django/forms.py
@@ -76,10 +76,13 @@ class DateTimePickerWidgetForm(DateTimePickerInput):
         )
         options.setdefault(
             'maxDate',
-            (TODAY + datetime.timedelta(days=1)).replace(
-        hour=NumericParameter.DAY_MINUS_HOUR.value,
-        minute=NumericParameter.MINUTE_MINUS_ONE.value,
-        second=NumericParameter.SECOND_MINUS_ONE.value,
-    ).strftime('%d/%m/%Y %H:%M'),
+            (
+                TODAY.replace(
+                    hour=NumericParameter.DAY_MINUS_HOUR.value,
+                    minute=NumericParameter.MINUTE_MINUS_ONE.value,
+                    second=NumericParameter.SECOND_MINUS_ONE.value,
+                )
+                + datetime.timedelta(days=1)
+            ).strftime('%d/%m/%Y %H:%M'),
         )
         super().__init__(*args, **kwargs, options=options)
