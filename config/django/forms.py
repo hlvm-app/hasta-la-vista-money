1c2af76f0d046c94fd89bdf2477abc7114c3d6a2
Alexander <dev@pavlovteam.ru>
2023-08-31 21:52:26 +0300

    Поправил линтер

--- config/django/forms.py
+++ config/django/forms.py
@@ -76,6 +76,8 @@ class DateTimePickerWidgetForm(DateTimePickerInput):
         )
         options.setdefault(
             'maxDate',
-            (datetime.datetime.today() + datetime.timedelta(days=2)).strftime('%Y-%m-%d 23:59:59'),
+            (datetime.datetime.today() + datetime.timedelta(days=2)).strftime(
+                '%Y-%m-%d 23:59:59',
+            ),
         )
         super().__init__(*args, **kwargs, options=options)
