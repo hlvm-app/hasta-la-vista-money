c9ec214e2e6a0ad43618e2959813db883ef734c6
Alexander <dev@pavlovteam.ru>
2023-09-04 22:26:26 +0300

    Создал модели для кредитов

--- hasta_la_vista_money/loan/models.py
+++ hasta_la_vista_money/loan/models.py
@@ -1,5 +1,29 @@
 from django.db import models
+from hasta_la_vista_money.account.models import Account
+from hasta_la_vista_money.constants import NumericParameter
+from hasta_la_vista_money.users.models import User
+
+
+class Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    name = models.CharField(max_length=NumericParameter.TWO_HUNDRED_FIFTY.value)
+
+    def __str__(self):
+        return str(self.name)
 
 
 class Loan(models.Model):
-    ...
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    account = models.ForeignKey(Account, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    loan_amount = models.FloatField(
+        max_length=NumericParameter.TWO_HUNDRED_FIFTY.value,
+    )
+    annual_interest_rate = models.DecimalField(
+        max_digits=NumericParameter.TWO_HUNDRED_FIFTY.value,
+        decimal_places=NumericParameter.TWO.value,
+    )
+    monthly_interest_rate = models.DecimalField(
+        max_digits=NumericParameter.TWO_HUNDRED_FIFTY.value,
+        decimal_places=NumericParameter.TWO.value,
+    )
