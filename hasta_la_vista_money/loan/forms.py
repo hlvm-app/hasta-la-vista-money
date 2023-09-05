683c64de1951a6524771370727ea1b7550e325c6
Alexander <dev@pavlovteam.ru>
2023-09-05 20:53:30 +0300

    Добавил форму

--- hasta_la_vista_money/loan/forms.py
+++ hasta_la_vista_money/loan/forms.py
@@ -1,1 +1,20 @@
 """Модуль форм по кредитам."""
+from config.django.forms import BaseForm, DateTimePickerWidgetForm
+from django.utils.translation import gettext_lazy as _
+from hasta_la_vista_money.loan.models import Loan
+
+
+class LoanForm(BaseForm):
+    labels = {
+        'date': _('Начальная дата кредита'),
+        'loan_amount': _('Сумма кредита'),
+        'annual_interest_rate': _('Годовая ставка в %'),
+        'period_loan': _('Срок кредита в месяцах'),
+    }
+
+    class Meta:
+        model = Loan
+        fields = ['date', 'loan_amount', 'annual_interest_rate', 'period_loan']
+        widgets = {
+            'date': DateTimePickerWidgetForm,
+        }
