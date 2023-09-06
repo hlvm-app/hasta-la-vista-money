e223f954780de406feba1c10b10d9658f55f0ce2
Alexander <dev@pavlovteam.ru>
2023-09-06 21:28:14 +0300

    Убрал поля с ФИ и почтой

--- hasta_la_vista_money/users/forms.py
+++ hasta_la_vista_money/users/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import (
     ReadOnlyPasswordHashField,
     UserCreationForm,
 )
-from django.forms import BooleanField, CharField, ModelForm, forms
+from django.forms import CharField, ModelForm, forms
 from hasta_la_vista_money.constants import MessageOnSite
 from hasta_la_vista_money.users.models import User
 
@@ -15,15 +15,9 @@ class UserLoginForm(AuthenticationForm):
 
 
 class RegisterUserForm(UserCreationForm):
-    policy = BooleanField(label='Политика конфиденциальности', required=True)
-
     class Meta:
         model = User
         fields = [
-            'first_name',
-            'last_name',
-            'email',
-            'policy',
             'username',
             'password1',
             'password2',
