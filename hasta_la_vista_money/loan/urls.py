d9278d8af7bd4a8c4e6339d62acf0afc4f16151b
Alexander <dev@pavlovteam.ru>
2023-09-14 07:14:31 +0300

    Добавил url для создания кредита

--- hasta_la_vista_money/loan/urls.py
+++ hasta_la_vista_money/loan/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from hasta_la_vista_money.loan.views import LoanView
+from hasta_la_vista_money.loan.views import LoanCreateView, LoanView
 
 app_name = 'loan'
 urlpatterns = [
     path('', LoanView.as_view(), name='list'),
+    path('create/', LoanCreateView.as_view(), name='create'),
 ]
