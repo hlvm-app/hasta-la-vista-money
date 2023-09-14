b96e7547618676a19a45f45f1b56db92c9cec54f
Alexander <dev@pavlovteam.ru>
2023-09-14 07:25:15 +0300

    Добавил новую модель по погашению платежа

--- hasta_la_vista_money/loan/models.py
+++ hasta_la_vista_money/loan/models.py
@@ -4,18 +4,9 @@ from hasta_la_vista_money.constants import NumericParameter
 from hasta_la_vista_money.users.models import User
 
 
-class Category(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-    name = models.CharField(max_length=NumericParameter.TWO_HUNDRED_FIFTY.value)
-
-    def __str__(self):
-        return str(self.name)
-
-
 class Loan(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     account = models.ForeignKey(Account, on_delete=models.PROTECT)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     date = models.DateTimeField()
     loan_amount = models.FloatField(
         max_length=NumericParameter.TWO_HUNDRED_FIFTY.value,
@@ -24,7 +15,16 @@ class Loan(models.Model):
         max_digits=NumericParameter.TWO_HUNDRED_FIFTY.value,
         decimal_places=NumericParameter.TWO.value,
     )
-    period_loan = models.DecimalField(
+    period_loan = models.IntegerField(
+        max_length=NumericParameter.TWO_HUNDRED_FIFTY.value,
+    )
+
+
+class PaymentMakeLoan(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    account = models.ForeignKey(Account, on_delete=models.PROTECT)
+    loan = models.ForeignKey(Loan, on_delete=models.PROTECT)
+    amount = models.DecimalField(
         max_digits=NumericParameter.TWO_HUNDRED_FIFTY.value,
         decimal_places=NumericParameter.TWO.value,
     )
