e5c16982c71017f8c2dc017f4f0cfa971c3384b9
Alexander <dev@pavlovteam.ru>
2023-09-17 21:07:26 +0300

    Добавил ссылку на внесения платежа по кредиту

--- hasta_la_vista_money/loan/urls.py
+++ hasta_la_vista_money/loan/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from hasta_la_vista_money.loan.views import LoanCreateView, LoanView
+from hasta_la_vista_money.loan.views import (
+    LoanCreateView,
+    LoanView,
+    PaymentMakeView,
+)
 
 app_name = 'loan'
 urlpatterns = [
     path('', LoanView.as_view(), name='list'),
     path('create/', LoanCreateView.as_view(), name='create'),
+    path('payment/create/', PaymentMakeView.as_view(), name='payment_create'),
 ]
