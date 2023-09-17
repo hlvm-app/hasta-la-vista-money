380dc03c83d005c7c3139a52cd88bfca65c552d1
Alexander <dev@pavlovteam.ru>
2023-09-17 21:08:15 +0300

    Добавил представление для обработки внесения платежа по кредиту

--- hasta_la_vista_money/loan/views.py
+++ hasta_la_vista_money/loan/views.py
@@ -1,12 +1,14 @@
+from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
 from hasta_la_vista_money.account.models import Account
+from hasta_la_vista_money.constants import MessageOnSite
 from hasta_la_vista_money.custom_mixin import CustomNoPermissionMixin
 from hasta_la_vista_money.loan.forms import LoanForm, PaymentMakeLoanForm
-from hasta_la_vista_money.loan.models import Loan
+from hasta_la_vista_money.loan.models import Loan, PaymentMakeLoan
 from hasta_la_vista_money.loan.tasks import async_calculate_annuity_loan
 from hasta_la_vista_money.users.models import User
 
@@ -73,5 +75,38 @@ class LoanCreateView(SuccessMessageMixin, CreateView):
             response_data = {
                 'success': False,
                 'errors': loan_form.errors,
+            }
+        return JsonResponse(response_data)
+
+
+class PaymentMakeView(CreateView):
+    template_name = 'loan/loan.html'
+    model = PaymentMakeLoan
+    form_class = PaymentMakeLoanForm
+    success_url = reverse_lazy('loan:list')
+
+    def post(self, request, *args, **kwargs):
+        payment_make_form = self.form_class(request.POST)
+        response_data = {}
+        if payment_make_form.is_valid():
+            payment_make = payment_make_form.save(commit=False)
+            amount = payment_make_form.cleaned_data.get('amount')
+            account = payment_make_form.cleaned_data.get('account')
+            account_balance = get_object_or_404(Account, id=account.id)
+            if account_balance.user == request.user:
+                account_balance.balance -= amount
+                account_balance.save()
+                payment_make.user = request.user
+                payment_make.save()
+                messages.success(
+                    request,
+                    MessageOnSite.SUCCESS_MESSAGE_PAYMENT_MAKE.value,
+                )
+                response_data = {'success': True}
+
+        else:
+            response_data = {
+                'success': False,
+                'errors': payment_make_form.errors,
             }
         return JsonResponse(response_data)
