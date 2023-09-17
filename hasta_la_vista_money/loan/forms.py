67f9f18c6ecc32e4026993f230b880b166294d84
Alexander <dev@pavlovteam.ru>
2023-09-17 21:06:11 +0300

    добавил форму добавления платежа

--- hasta_la_vista_money/loan/forms.py
+++ hasta_la_vista_money/loan/forms.py
@@ -1,7 +1,7 @@
 """Модуль форм по кредитам."""
 from config.django.forms import BaseForm, DateTimePickerWidgetForm
 from django.utils.translation import gettext_lazy as _
-from hasta_la_vista_money.loan.models import Loan
+from hasta_la_vista_money.loan.models import Loan, PaymentMakeLoan
 
 
 class LoanForm(BaseForm):
@@ -15,6 +15,22 @@ class LoanForm(BaseForm):
     class Meta:
         model = Loan
         fields = ['date', 'loan_amount', 'annual_interest_rate', 'period_loan']
+        widgets = {
+            'date': DateTimePickerWidgetForm,
+        }
+
+
+class PaymentMakeLoanForm(BaseForm):
+    labels = {
+        'date': _('Дата платежа'),
+        'account': _('Счёт списания'),
+        'loan': _('Кредит'),
+        'amount': _('Сумма платежа'),
+    }
+
+    class Meta:
+        model = PaymentMakeLoan
+        fields = ['date', 'account', 'loan', 'amount']
         widgets = {
             'date': DateTimePickerWidgetForm,
         }
