6ba9e703b1a36d485184df63a5a4d1204f1a1ad5
TurtleOld <dev@pavlovteam.ru>
2023-10-01 18:54:08 +0300

    поправил класс удаления

--- hasta_la_vista_money/loan/urls.py
+++ hasta_la_vista_money/loan/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from hasta_la_vista_money.loan.views import (
     LoanCreateView,
+    LoanDeleteView,
     LoanView,
     PaymentMakeView,
 )
@@ -9,6 +10,6 @@ app_name = 'loan'
 urlpatterns = [
     path('', LoanView.as_view(), name='list'),
     path('create/', LoanCreateView.as_view(), name='create'),
-    path('delete/<int:pk>', LoanCreateView.as_view(), name='delete'),
+    path('delete/<int:pk>', LoanDeleteView.as_view(), name='delete'),
     path('payment/create/', PaymentMakeView.as_view(), name='payment_create'),
 ]
