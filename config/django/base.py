3f796abeb2505f54feeff5da86c3c3ed642be02e
TurtleOld <dev@pavlovteam.ru>
2023-10-06 17:51:45 +0300

    registered commonlogic

--- config/django/base.py
+++ config/django/base.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
     'hasta_la_vista_money.account',
     'hasta_la_vista_money.applications',
     'hasta_la_vista_money.bot',
+    'hasta_la_vista_money.commonlogic',
     'hasta_la_vista_money.expense',
     'hasta_la_vista_money.income',
     'hasta_la_vista_money.loan',
