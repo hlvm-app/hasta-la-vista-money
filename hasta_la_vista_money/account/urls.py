d1d0f1ef87f2785c5ff77c7754ec4be467da1ab6
TurtleOld <dev@pavlovteam.ru>
2023-10-07 10:51:01 +0300

    добавил новый url для добавления счёта

--- hasta_la_vista_money/account/urls.py
+++ hasta_la_vista_money/account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from hasta_la_vista_money.applications.views import (
+    AccountCreateView,
     ChangeAccountView,
     DeleteAccountView,
     TransferMoneyAccountView,
@@ -7,16 +8,9 @@ from hasta_la_vista_money.applications.views import (
 
 app_name = 'account'
 urlpatterns = [
-    path(
-        '<int:pk>',
-        DeleteAccountView.as_view(),
-        name='delete_account',
-    ),
-    path(
-        'change/<int:pk>/',
-        ChangeAccountView.as_view(),
-        name='change',
-    ),
+    path('create/', AccountCreateView.as_view(), name='create'),
+    path('change/<int:pk>/', ChangeAccountView.as_view(), name='change'),
+    path('delete/<int:pk>', DeleteAccountView.as_view(), name='delete_account'),
     path(
         'transfer-money/',
         TransferMoneyAccountView.as_view(),
