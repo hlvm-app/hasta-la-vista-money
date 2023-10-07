af9c18fc63e9df12e115a26aa03a281454f6ddf9
TurtleOld <dev@pavlovteam.ru>
2023-10-07 10:15:06 +0300

    Добавил разделение миллионных

--- hasta_la_vista_money/templatags/thousand_comma.py
+++ hasta_la_vista_money/templatags/thousand_comma.py
@@ -5,23 +5,20 @@ from django import template
 register = template.Library()
 
 THOUSAND_MINUS_ONE = 999
+MILLION_MINUS_ONE = 999999
 
 
 @register.filter
 def comma(number: float) -> Union[float, str]:
     """
-    Функция разделения тысячных.
+    Функция разделения тысячных и миллионных.
 
     :param number:
     :type number: float
     :return: Union[float, str]
     """
-    if number > THOUSAND_MINUS_ONE:
-        return (
-            str(
-                number // 1000,
-            )
-            + ' '
-            + str(number)[len(str(number // 1000)) :]
-        )
+    if number > MILLION_MINUS_ONE:
+        return f'{number:,.0f}'.replace(',', ' ')
+    elif number > THOUSAND_MINUS_ONE:
+        return f'{number:,.0f}'.replace(',', ' ')
     return number
