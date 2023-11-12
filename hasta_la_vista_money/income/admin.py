d698a41811aa030b463ea694d21790bef3f26d4c
TurtleOld <dev@pavlovteam.ru>
2023-11-12 20:48:56 +0300

    Добавил в админку модель с категориями

--- hasta_la_vista_money/income/admin.py
+++ hasta_la_vista_money/income/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from hasta_la_vista_money.income.models import Income
+from hasta_la_vista_money.income.models import Income, IncomeCategory
 
 admin.site.register(Income)
+admin.site.register(IncomeCategory)
