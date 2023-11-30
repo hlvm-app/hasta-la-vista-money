bd50a89c7e1526da56f9ed85f9478ac193d5e8bd
TurtleOld <dev@pavlovteam.ru>
2023-11-30 20:50:22 +0300

    отображение новой формы для выбора даты

--- hasta_la_vista_money/budget/views.py
+++ hasta_la_vista_money/budget/views.py
@@ -1,5 +1,19 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from hasta_la_vista_money.budget.forms import SelectDateForm
+from hasta_la_vista_money.budget.models import Planning
 
 
-class BudgetView(TemplateView):
+class BaseView(ListView):
     template_name = 'budget.html'
+
+
+class BudgetView(BaseView):
+    model = Planning
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        select_date_form = SelectDateForm(initial={'select_date': '----------'})
+
+        context = super().get_context_data(**kwargs)
+        context['select_date_form'] = select_date_form
+
+        return context
