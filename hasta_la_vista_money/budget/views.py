318eb1669ef0a8b20f312677bd02eb4785cf375c
TurtleOld <dev@pavlovteam.ru>
2023-12-03 22:56:29 +0300

    Сделал единую таблицу

--- hasta_la_vista_money/budget/views.py
+++ hasta_la_vista_money/budget/views.py
@@ -1,6 +1,8 @@
+from django.db.models import Sum
+from django.db.models.functions import TruncMonth
 from django.views.generic import ListView
-from hasta_la_vista_money.budget.forms import SelectDateForm
-from hasta_la_vista_money.budget.models import Planning
+from hasta_la_vista_money.budget.models import DateList, Planning
+from hasta_la_vista_money.expense.models import Expense
 
 
 class BaseView(ListView):
@@ -11,9 +13,18 @@ class BudgetView(BaseView):
     model = Planning
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        select_date_form = SelectDateForm(initial={'select_date': '----------'})
+        list_date = DateList.objects.filter(user=self.request.user).order_by(
+            'date',
+        )
+        queryset_expense = (
+            Expense.objects.filter(user=self.request.user)
+            .annotate(month=TruncMonth('date'))
+            .values('month')
+            .annotate(total_amount=Sum('amount'))
+        )
 
         context = super().get_context_data(**kwargs)
-        context['select_date_form'] = select_date_form
+        context['queryset_expense'] = queryset_expense
+        context['list_date'] = list_date
 
         return context
