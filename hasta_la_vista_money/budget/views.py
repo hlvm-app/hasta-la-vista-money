89937dcc373663de38ac7824e3f10f7d32398ac3
TurtleOld <dev@pavlovteam.ru>
2023-12-05 21:21:15 +0300

    Добавил строчку с итогами по месяцам

--- hasta_la_vista_money/budget/views.py
+++ hasta_la_vista_money/budget/views.py
@@ -12,7 +12,7 @@ class BaseView(ListView):
 class BudgetView(BaseView):
     model = Planning
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs):  # noqa: WPS210
         context = super().get_context_data(**kwargs)
 
         list_dates = DateList.objects.filter(user=self.request.user).order_by(
@@ -45,19 +45,23 @@ class BudgetView(BaseView):
                 expenses_dict[category.name][month_key] = total_amount
 
         category_amount = []
+        total_sums = [0] * len(list_dates)  # noqa: WPS435
         for category in parent_categories:
             row = {'category': category.name, 'amounts': []}
 
-            for date in list_dates:
+            for date_index, date in enumerate(list_dates):
                 month_key = date.date.strftime('%Y-%m')
                 amount = expenses_dict.get(category.name, {}).get(
                     month_key,
                     '0,00',
                 )
                 row['amounts'].append(amount)
+                amount_str = str(amount)
+                total_sums[date_index] += float(amount_str.replace(',', '.'))
 
             category_amount.append(row)
         context['list_dates'] = list_dates
         context['category_amount'] = category_amount
+        context['total_sums'] = total_sums
 
         return context
