2c0d3dda9ab3040fc29a13f9feae46139a19e659
TurtleOld <dev@pavlovteam.ru>
2024-01-14 14:49:37 +0300

    added url planning

--- hasta_la_vista_money/budget/urls.py
+++ hasta_la_vista_money/budget/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from hasta_la_vista_money.budget.views import (
     BudgetView,
+    change_planning,
     generate_date_list_view,
 )
 
@@ -8,4 +9,9 @@ app_name = 'budget'
 urlpatterns = [
     path('', BudgetView.as_view(), name='list'),
     path('generate-date/', generate_date_list_view, name='generate_date'),
+    path(
+        'change_planning/',
+        change_planning,
+        name='change_planning',
+    ),
 ]
