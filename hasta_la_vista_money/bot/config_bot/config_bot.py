4f36e00ac27c944614040b9cd12367f93a4f1808
TurtleOld <dev@pavlovteam.ru>
2024-02-18 20:52:53 +0300

    #65 added logger and StateMemoryStorage

--- hasta_la_vista_money/bot/config_bot/config_bot.py
+++ hasta_la_vista_money/bot/config_bot/config_bot.py
@@ -1,15 +1,23 @@
 """Модуль конфигурации бота."""
+import logging
 import os
 
 from dotenv import load_dotenv
-from telebot import TeleBot, types
+from telebot import TeleBot, logger, types
+from telebot.storage import StateMemoryStorage
 
 load_dotenv()
 token = os.getenv('TOKEN_TELEGRAM_BOT')
+if os.getenv('DEBUG'):
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
+state_storage = StateMemoryStorage()
 bot_admin = TeleBot(
     token,
     threaded=False,
     use_class_middlewares=True,
     parse_mode='html',
+    state_storage=state_storage,
 )
 bot_type = types
