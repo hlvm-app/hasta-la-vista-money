e0c03aaab06fd0f3770edb11d4b19c0db38504a7
TurtleOld <dev@pavlovteam.ru>
2024-04-09 06:45:50 +0300

    refactoring

--- hasta_la_vista_money/account/serializers.py
+++ hasta_la_vista_money/account/serializers.py
@@ -1,7 +1,6 @@
 from hasta_la_vista_money.account.models import Account
-from rest_framework import serializers
-
 from hasta_la_vista_money.users.models import SelectedAccount
+from rest_framework import serializers
 
 
 class AccountSerializer(serializers.ModelSerializer):
