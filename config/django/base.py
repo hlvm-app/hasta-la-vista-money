138e49cd49ec15486591106559f80ac5d05cb377
Alexander Pavlov <dev@pavlovteam.ru>
2024-04-30 10:46:26 +0300

    Update base.py

--- config/django/base.py
+++ config/django/base.py
@@ -113,8 +113,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'axes.middleware.AxesMiddleware',
     'hasta_la_vista_money.users.middleware.CheckAdminMiddleware',
+    'axes.middleware.AxesMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
