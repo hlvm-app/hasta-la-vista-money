dbf32e31f60e7cb6befbcc722a62e0857be40b5e
Alexander Pavlov <dev@pavlovteam.ru>
2024-10-28 22:21:31 +0300

    ✨ feat: change modal window
    change the modal window to regular individual pages

--- hasta_la_vista_money/expense/urls.py
+++ hasta_la_vista_money/expense/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from hasta_la_vista_money.expense.views import (
     ExpenseCategoryCreateView,
     ExpenseCategoryDeleteView,
+    ExpenseCategoryView,
     ExpenseCopyView,
     ExpenseCreateView,
     ExpenseDeleteView,
@@ -15,6 +16,11 @@ urlpatterns = [
     path('create/', ExpenseCreateView.as_view(), name='create'),
     path('change/<int:pk>/', ExpenseUpdateView.as_view(), name='change'),
     path('delete/<int:pk>/', ExpenseDeleteView.as_view(), name='delete'),
+    path(
+        'category/list',
+        ExpenseCategoryView.as_view(),
+        name='category_list',
+    ),
     path(
         'category/<int:pk>/',
         ExpenseCategoryDeleteView.as_view(),
